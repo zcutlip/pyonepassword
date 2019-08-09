a9eba9c160bfbf5e0986da9ada815ef67fee3da8
Zachary Cutlip <uid000@gmail.com>
2019-08-09 12:08:59 -0700

    example of initial 1password sign-in

---
+++ examples/example-initial-signin.py
@@ -0,0 +1,36 @@
+import getpass
+import os
+import sys
+parent_path = os.path.dirname(
+    os.path.dirname(
+        os.path.abspath(__file__)
+    )
+)
+if parent_path not in sys.path:
+    sys.path.append(parent_path)
+
+
+from pyonepassword import (
+    OP,
+    OPSigninException
+)
+
+
+def do_initial_signin():
+    my_signin_address = input("1Password sign-in address:")
+    my_email_address = input("1Password email address:\n")
+    my_secret_key = getpass.getpass(prompt="1Password secret key:\n")
+    my_password = getpass.getpass(prompt="1Password master password:\n")
+    try:
+        op = OP(signin_address=my_signin_address,
+                email_address=my_email_address,
+                secret_key=my_secret_key,
+                password=my_password)
+    except OPSigninException as ope:
+        print("1Password initial signin failed: {}".format(ope))
+        print(ope.err_output)
+        exit(1)
+
+
+if __name__ == "__main__":
+    do_initial_signin()

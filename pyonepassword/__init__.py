2c837cd101362590960d25604572ad9b35d6afe8
Zachary Cutlip <uid000@gmail.com>
2019-08-09 12:09:46 -0700

    export pyonepassword classes in __all__

--- pyonepassword/__init__.py
+++ pyonepassword/__init__.py
@@ -4,8 +4,17 @@ from .__about__ import (
     __summary__
 )
 
+from .pyonepassword import (
+    OP,
+    OPLookupException,
+    OPSigninException
+)
+
 __all__ = [
     "__version__",
     "__title__",
-    "__summary__"
+    "__summary__",
+    OP,
+    OPLookupException,
+    OPSigninException
 ]

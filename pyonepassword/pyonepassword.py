ca1dac73af74ecf4ce262a3aa7af11d068ebd3f4
Zachary Cutlip <uid000@gmail.com>
2019-08-09 09:26:19 -0700

    optionally take a password to sign in with

--- pyonepassword/pyonepassword.py
+++ pyonepassword/pyonepassword.py
@@ -19,38 +19,39 @@ class OPLookupException(OPSigninException):
 class OP:
     OP_PATH = "/usr/local/bin/op"
 
-    def __init__(self, op_path=OP_PATH, signin_address=None, email_address=None, secret_key=None, logger=None):
+    def __init__(self, op_path=OP_PATH, signin_address=None, email_address=None,
+                 secret_key=None, password=None, logger=None):
         self.op_path = op_path
         if not logger:
             logging.basicConfig(format="%(message)s", level=logging.DEBUG)
             logger = logging.getLogger()
 
         self.logger = logger
-        initial_signin_args = [signin_address, email_address, secret_key]
+        initial_signin_args = [signin_address, email_address, secret_key, password]
         initial_signin = (None not in initial_signin_args)
 
         if initial_signin:
             self.token = self._do_initial_signin(*initial_signin_args)
         else:
-            self.token = self._do_normal_signin()
+            self.token = self._do_normal_signin(password)
 
-    def _do_normal_signin(self):
+    def _do_normal_signin(self, password):
         self.logger.info("Doing normal (non-initial) 1Password sign-in")
         signin_argv = [self.op_path, "signin", "--output=raw"]
         print("")
-        token = self._run_signin(signin_argv).rstrip()
+        token = self._run_signin(signin_argv, password=password).rstrip()
         return token
 
-    def _do_initial_signin(self, signin_address, email_address, secret_key):
+    def _do_initial_signin(self, signin_address, email_address, secret_key, password):
         self.logger.info("Performing initial 1Password sign-in to {} as {}".format(signin_address, email_address))
         signin_argv = [self.op_path, "signin", signin_address, email_address, secret_key, "--output=raw"]
         print("")
-        token = self._run_signin(signin_argv).rstrip()
+        token = self._run_signin(signin_argv, password=password).rstrip()
 
         return token
 
-    def _run_signin(self, argv):
-        return self._run(argv, OPSigninException, capture_stdout=True, input_string=None)
+    def _run_signin(self, argv, password=None):
+        return self._run(argv, OPSigninException, capture_stdout=True, input_string=password)
 
     def _run_lookup(self, argv, input_string, decode=None):
         return self._run(argv, OPLookupException, capture_stdout=True, input_string=input_string, decode=None)
@@ -59,6 +60,8 @@ class OP:
         _ran = None
         stdout = subprocess.PIPE if capture_stdout else None
         if input_string:
+            if isinstance(input_string, str):
+                input_string = input_string.encode("utf-8")
             _ran = subprocess.run(argv, input=input_string, stderr=subprocess.PIPE, stdout=stdout)
         else:
             _ran = subprocess.run(argv, stderr=subprocess.PIPE, stdout=stdout)

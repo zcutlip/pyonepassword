aee2c077e22cbcffd80fbca091e179ef5e7dac9f
Zachary Cutlip <uid000@gmail.com>
2019-08-09 12:45:01 -0700

    add additional metadata to setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,28 @@
 from setuptools import setup
 about = {}
+
 with open("pyonepassword/__about__.py") as fp:
     exec(fp.read(), about)
 
-setup(name=about["__title__"],
-      version=about["__version__"],
-      description=about["__summary__"],
-      url="https://github.com/zcutlip/pyonepassword",
-      packages=['pyonepassword'],
-      python_requires='>=3.7',
-      install_requires=[],
-      package_data={'pyonepassword': ['config/*']},
-      )
+with open("README.md", "r") as fp:
+    long_description = fp.read()
+
+setup(
+    name=about["__title__"],
+    version=about["__version__"],
+    description=about["__summary__"],
+    license="MIT",
+    author="Zachary Cutlip",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/zcutlip/pyonepassword",
+    packages=['pyonepassword'],
+    python_requires='>=3.7',
+    install_requires=[],
+    package_data={'pyonepassword': ['config/*']},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)

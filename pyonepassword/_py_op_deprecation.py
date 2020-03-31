2327fd05ae06289567c0c924bf6093c075e1404c
Zachary Cutlip <uid000@gmail.com>
2020-03-30 21:10:50 -0700

    deprecation decorator class

---
+++ pyonepassword/_py_op_deprecation.py
@@ -0,0 +1,65 @@
+
+import warnings
+import functools
+
+"""
+Adapted from:
+https://github.com/scikit-learn/scikit-learn/blob/master/sklearn/utils/deprecation.py
+"""
+
+
+class deprecated:
+
+    def __init__(self, extra=""):
+        self.extra = extra
+
+    def __call__(self, obj):
+        if isinstance(obj, type):
+            pass
+        else:
+            pass
+
+    def _decorate_class(self, cls):
+        msg = "Class {} is deprecated".format(cls.__name__)
+        if self.extra:
+            msg += ": {}".format(self.extra)
+
+        init = cls.__init__
+
+        def wrapped(*args, **kwargs):
+            warnings.warn(msg, category=FutureWarning)
+            return init(*args, **kwargs)
+        cls.__init__ = wrapped
+
+        wrapped.__name__ = '__init__'
+        wrapped.__doc__ = self._update_doc(init.__doc__)
+        wrapped.deprecated_original = init
+
+        return cls
+
+    def _decorate_fun(self, fun):
+        """Decorate function fun"""
+
+        msg = "Function %s is deprecated" % fun.__name__
+        if self.extra:
+            msg += "; %s" % self.extra
+
+        @functools.wraps(fun)
+        def wrapped(*args, **kwargs):
+            warnings.warn(msg, category=FutureWarning)
+            return fun(*args, **kwargs)
+
+        wrapped.__doc__ = self._update_doc(wrapped.__doc__)
+        # Add a reference to the wrapped function so that we can introspect
+        # on function arguments in Python 2 (already works in Python 3)
+        wrapped.__wrapped__ = fun
+
+        return wrapped
+
+    def _update_doc(self, olddoc):
+        newdoc = "DEPRECATED"
+        if self.extra:
+            newdoc = "%s: %s" % (newdoc, self.extra)
+        if olddoc:
+            newdoc = "%s\n\n    %s" % (newdoc, olddoc)
+        return newdoc

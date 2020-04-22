9c744f137710d6429b0bca3a3a454f2ebc418dfc
Zachary Cutlip <uid000@gmail.com>
2020-04-22 12:43:40 -0700

    rearrange imports

--- examples/example-signin-get-document.py
+++ examples/example-signin-get-document.py
@@ -1,3 +1,9 @@
+from pyonepassword import (
+    OP,
+    OPSigninException,
+    OPGetDocumentException,
+    OPNotFoundException
+)
 import getpass
 import os
 import sys
@@ -8,13 +14,6 @@ parent_path = os.path.dirname(
 )
 if parent_path not in sys.path:
     sys.path.append(parent_path)
-
-from pyonepassword import (  # noqa: E402
-    OP,
-    OPSigninException,
-    OPLookupException,
-    OPNotFoundException
-)
 
 
 def do_signin():

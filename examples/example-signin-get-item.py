69cbbe7f9219a25afbe7d91fc16330a794863749
Zachary Cutlip <uid000@gmail.com>
2020-06-14 13:36:39 -0700

    more tweaks to examples

--- examples/example-signin-get-item.py
+++ examples/example-signin-get-item.py
@@ -19,10 +19,13 @@ from pyonepassword import (  # noqa: E402
 
 
 def do_signin():
-    # account_shorthand = "arbitrary_account_shorthand"
     # If you've already signed in at least once, you don't need to provide all
     # account details on future sign-ins. Just master password
     my_password = getpass.getpass(prompt="1Password master password:\n")
+    # You may optionally provide an account shorthand if you used a custom one during initial sign-in
+    # shorthand = "arbitrary_account_shorthand"
+    # return OP(account_shorthand=shorthand, password=my_password)
+    # Or we'll try to look up account shorthand from your latest sign-in in op's config file
     return OP(password=my_password)
 
 

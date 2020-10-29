e0e59ed93c1b7529e370c8b925ce619772e5f3f7
Zachary Cutlip <uid000@gmail.com>
2020-10-29 16:56:11 -0700

    add a signin/forget example

---
+++ examples/example-signin-forget.py
@@ -0,0 +1,70 @@
+import getpass
+import os
+import sys
+parent_path = os.path.dirname(
+    os.path.dirname(
+        os.path.abspath(__file__)
+    )
+)
+if parent_path not in sys.path:
+    sys.path.append(parent_path)
+
+from pyonepassword import (  # noqa: E402
+    OP,
+    OPSigninException,
+    OPGetItemException,
+    OPSignoutException,
+    OPForgetException
+)
+
+
+def do_signin():
+    # If you've already signed in at least once, you don't need to provide all
+    # account details on future sign-ins. Just master password
+    my_password = getpass.getpass(prompt="1Password master password:\n")
+    # You may optionally provide an account shorthand if you used a custom one during initial sign-in
+    # shorthand = "arbitrary_account_shorthand"
+    # return OP(account_shorthand=shorthand, password=my_password)
+    # Or we'll try to look up account shorthand from your latest sign-in in op's config file
+    return OP(vault="Archive", password=my_password)
+
+
+def do_lookup():
+    try:
+        print(op.get_item_password("Example Login"))
+    except OPGetItemException as opge:
+        print("Get item failed.")
+        print(opge.err_output)
+        return opge.returncode
+
+
+if __name__ == "__main__":
+    try:
+        op = do_signin()
+    except OPSigninException as opse:
+        print("1Password sign-in failed.")
+        print(opse.err_output)
+        exit(opse.returncode)
+    account_shorthand = op.account_shorthand
+    print("Doing signout.")
+    try:
+        op.signout()
+    except OPSignoutException as e:
+        print("Signout failed.")
+        print(e.err_output)
+        exit(e.returncode)
+
+    print("Forgetting account: {}.".format(account_shorthand))
+    try:
+        OP.forget(account_shorthand)
+    except OPForgetException as e:
+        print(e.err_output)
+        exit(e.returncode)
+    print("Done.")
+
+    print("Trying to re-sign-in")
+    try:
+        do_signin()
+    except OPSigninException as opse:
+        print("1Password sign-in failed.")
+        print(opse.err_output)

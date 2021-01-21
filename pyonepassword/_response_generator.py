f4c536b8d3e2e6689e23be7ea409066ae9290369
Zachary Cutlip <uid000@gmail.com>
2021-01-21 12:45:43 -0800

    handle encoding & exit status:
    - separate stdout & stderr encoding
    - record exit status in response_dict

--- pyonepassword/_response_generator.py
+++ pyonepassword/_response_generator.py
@@ -5,10 +5,11 @@ from ._py_op_cli import _OPArgv
 
 
 class OPQueryResponse:
-    def __init__(self, query_dict, output, error_output, returncode, encoding="utf-8"):
+    def __init__(self, query_dict, output, error_output, returncode, stdout_encoding="utf-8", stderr_encoding="utf-8"):
         self.query_dict = query_dict
-        self.encoding = encoding
+        self.stdout_encoding = stdout_encoding
         self.output = output
+        self.stderr_encoding = stderr_encoding
         self.error_output = error_output
         self.returncode = returncode
 
@@ -20,27 +21,30 @@ class OPQueryResponse:
             resp_path = response_dir
 
         resp_path.mkdir(parents=True, exist_ok=True)
-        if self.encoding == "binary":
+        # TODO: stderr binary output doesn't really make sense
+        # should we check for it and raise an exception?
+        if self.stdout_encoding == "binary":
             binary = True
-            output_ext = ".bin"
+            output_ext = "bin"
         else:
             binary = False
-            output_ext = ".txt"
+            output_ext = "txt"
 
         output_path = Path(resp_path, f"output.{output_ext}")
-        error_output_path = Path(resp_path, f"error_output.{output_ext}")
+        error_output_path = Path(resp_path, "error_output.txt")
 
+        error_output_path.write_text(self.error_output)
         if binary:
             output_path.write_bytes(self.output)
-            error_output_path.write_bytes(self.error_output)
         else:
             output_path.write_text(self.output)
-            error_output_path.write_text(self.error_output)
         response_dict = {
             "query": self.query_dict,
             "response": {
-                "encoding": self.encoding,
-                "response_dir": os.path.basename(resp_path),
+                "stdout_encoding": self.stdout_encoding,
+                "stderr_encoding": self.stderr_encoding,
+                "exit_status": self.returncode,
+                "response_dir": str(resp_path),
                 "stdout": os.path.basename(output_path),
                 "stderr": os.path.basename(error_output_path)
             }
@@ -51,32 +55,33 @@ class OPQueryResponse:
 
 class OPResponseGenerator(_OPCommandInterface):
 
-    def _generate_response_dict(self, argv_obj: _OPArgv, stdout, stderr, returncode, decode=None):
-        if decode:
-            binary = False
-        else:
-            binary = True
+    def _generate_response_dict(self, argv_obj: _OPArgv, stdout, stderr, returncode, stdout_encoding, stderr_encoding):
         query_dict = argv_obj.query_dict()
         query_response = OPQueryResponse(
-            query_dict, stdout, stderr, returncode, binary=binary)
+            query_dict, stdout, stderr, returncode, stdout_encoding=stdout_encoding, stderr_encoding=stderr_encoding)
 
         return query_response
 
     def get_item_generate_response(self, item_name_or_uuid, vault=None, fields=None, decode="utf-8"):
+        out_encoding = err_encoding = decode
         get_item_argv: _OPArgv = self._get_item_argv(
             item_name_or_uuid, vault=vault, fields=fields)
-        output = super().get_item(item_name_or_uuid, vault=vault, fields=fields, decode=decode)
-        resp_dict = self._generate_response_dict(
-            get_item_argv, output, decode=decode)
+        stdout, stderr, returncode = self._run_raw(get_item_argv, capture_stdout=True, ignore_error=True)
+        stdout = stdout.decode(decode)
+        stderr = stderr.decode(decode)
+        resp_dict = self._generate_response_dict(get_item_argv, stdout, stderr, returncode, out_encoding, err_encoding)
 
         return resp_dict
 
     def get_document_generate_response(self, document_name_or_uuid: str, vault: str = None):
+        out_encoding = "binary"
+        err_encoding = "utf-8"
         get_doc_argv: _OPArgv = self._get_document_argv(
             document_name_or_uuid, vault=vault)
         stdout, stderr, returncode = self._run_raw(
             get_doc_argv, capture_stdout=True, ignore_error=True)
+        stderr = stderr.decode(err_encoding)
         resp_dict = self._generate_response_dict(
-            get_doc_argv, stdout, stderr, returncode)
+            get_doc_argv, stdout, stderr, returncode, out_encoding, err_encoding)
 
         return resp_dict

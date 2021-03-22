ae43a4c5aa9389db2f681b74e5461303dc9a3ed7
Zachary Cutlip <uid000@gmail.com>
2021-03-22 15:29:35 -0700

    add a class for Server entries in 1Password

--- pyonepassword/_py_op_items.py
+++ pyonepassword/_py_op_items.py
@@ -340,3 +340,40 @@ class OPDocumentItem(OPAbstractItem):
         document_attributes = details["documentAttributes"]
         file_name = document_attributes["fileName"]
         return file_name
+
+@op_register_item_type
+class OPServerItem(OPAbstractItem):
+    TEMPLATE_ID = "110"
+
+    def __init__(self, item_dict):
+        super().__init__(item_dict)
+
+    @property
+    def password(self):
+        password = self.primary_section_field_value("password")
+        return password
+
+    @property
+    def username(self):
+        username = self.primary_section_field_value("username")
+        return username
+
+    @property
+    def url(self):
+        url = self.primary_section_field_value("URL")
+        return url
+
+    @property
+    def admin_console_password(self):
+        password = self.field_value_by_section_title("Admin Console", "console password")
+        return password
+
+    @property
+    def admin_console_username(self):
+        username = self.field_value_by_section_title("admin console username")
+        return username
+
+    @property
+    def admin_console_url(self):
+        url = self.field_value_by_section_title("Admin Console", "admin console URL")
+        return url

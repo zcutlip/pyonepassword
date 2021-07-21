2596ccfcacb84d1351a47eb49dd74f722592ef31
Zachary Cutlip <uid000@gmail.com>
2021-07-20 22:16:29 -0700

    give login item a category name

--- pyonepassword/op_items/login.py
+++ pyonepassword/op_items/login.py
@@ -4,6 +4,7 @@ from ._op_items_base import OPAbstractItem
 @op_register_item_type
 class OPLoginItem(OPAbstractItem):
     TEMPLATE_ID = "001"
+    ITEM_CATEGORY = "Login"
 
     def __init__(self, item_dict):
         super().__init__(item_dict)

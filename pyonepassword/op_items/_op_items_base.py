162425cfc24e4aa70c180ff56832d084e740a943
Zachary Cutlip <uid000@gmail.com>
2021-07-29 16:58:51 -0700

    make OPItemOverview a fully functional class
    - Should be no need to extend by another class
    - process url entries if the overview has them

--- pyonepassword/op_items/_op_items_base.py
+++ pyonepassword/op_items/_op_items_base.py
@@ -3,7 +3,7 @@ import copy
 import json
 
 from abc import ABC, abstractmethod
-from typing import Dict, List
+from typing import List
 from .item_section import OPSection, OPSectionField, OPSectionCollisionException
 from .templates import TemplateDirectory
 
@@ -21,6 +21,54 @@ def item_template(cls):
     cls.__init__ = __init__
     return cls
 
+
+class OPItemOverview(dict):
+    class URLEntry(dict):
+        def __init__(self, url_dict):
+            ud = copy.deepcopy(url_dict)
+            super().__init__(ud)
+
+        @property
+        def label(self):
+            return self["l"]
+
+        @property
+        def url(self):
+            return self["u"]
+
+    def __init__(self, overview_dict):
+        od = copy.deepcopy(overview_dict)
+        super().__init__(od)
+        _urls = self._process_urls()
+        if _urls is not None:
+            self["URLs"] = _urls
+
+    def _process_urls(self):
+        # Some item types, namely Login and Password can
+        # have a URLs dict in their overview
+        # other item types do not. We want to make accessing those
+        # transparent, making them available if they exist, but not
+        # failing if they don't
+        url_items = None
+        url_dicts = self.get("URLs", [])
+        if url_dicts:
+            url_items = []
+            for d in url_dicts:
+                url = self.URLEntry(d)
+                url_items.append(url)
+        return url_items
+
+    def url_list(self):
+        return self.get("URLs", None)
+
+    def set_url(self, url, label=""):
+        url_dict = {
+            "l": label,
+            "u": url
+        }
+        # TODO: is it an error if we alread have one or more URLs?
+        new_url = self.URLEntry(url_dict)
+        self["URLs"] = [new_url]
 
 class OPAbstractItem(ABC):
     TEMPLATE_ID = None
@@ -138,9 +186,3 @@ class OPAbstractItem(ABC):
         section_field: OPSectionField = section.fields_by_label(field_label)[0]
         value = section_field.value
         return value
-
-class OPItemOverview(dict):
-
-    def __init__(self, overview_dict: Dict):
-        od = copy.deepcopy(overview_dict)
-        super().__init__(od)

db73c82b1d28f03c20570ca0fff4bce42d2fc991
Zachary Cutlip <uid000@gmail.com>
2021-09-23 20:16:00 -0700

    reference _token as a private variable

--- pyonepassword/pyonepassword.py
+++ pyonepassword/pyonepassword.py
@@ -229,7 +229,7 @@ class OP(_OPCommandInterface):
             raise OPForgetException.from_opexception(ocfe) from ocfe
 
     def _sanitize(self):
-        self.token = None
+        self._token = None
         sess_var_name = 'OP_SESSION_{}'.format(self.account_shorthand)
         try:
             env.pop(sess_var_name)

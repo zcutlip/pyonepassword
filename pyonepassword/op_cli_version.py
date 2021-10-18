9e96b495e4012e884e3489625351d9b9368cfbf4
Zachary Cutlip <uid000@gmail.com>
2021-10-17 20:03:16 -0500

    add class to parse & compare 'op' version string

---
+++ pyonepassword/op_cli_version.py
@@ -0,0 +1,67 @@
+class OPCLIVersion:
+    def __init__(self, version_string: str):
+        version_tuple = version_string.split(".")
+        parts = []
+        for part in version_tuple:
+            parts.append(int(part, 0))
+        self._parts = parts
+
+    def _normalize(self, other):
+        parts_self = list(self._parts)
+        parts_other = list(other._parts)
+
+        # be robust about version strings of differing lenghts
+        # e.g., 11.3.1 vs 11.3.1.1
+        diff_len = len(parts_self) - len(parts_other)
+
+        # if the length difference was negative, _parts_other is longer,
+        # and we need to exitend _parts self
+        while diff_len < 0:
+            parts_self.append(0)
+            diff_len += 1
+        # if diff was positive, we need to extend _parts other
+        while diff_len > 0:
+            parts_other.append(0)
+            diff_len -= 1
+
+        # now 11.3.1 vs 11.3.1.1 becomes
+        # 11.3.1.0 vs 11.3.1.1, and can be compared 1 to 1
+        return (parts_self, parts_other)
+
+    def __eq__(self, other):
+        equal = id(self) == id(other)
+
+        if not equal:
+            if not isinstance(other, OPCLIVersion):
+                other = OPCLIVersion(other)
+
+            parts_self, parts_other = self._normalize(other)
+            equal = True
+            for i, part in enumerate(parts_self):
+                if part != parts_other[i]:
+                    equal = False
+                    break
+
+        return equal
+
+    def __ne__(self, other):
+        ne = not self.__eq__(other)
+        return ne
+
+    def __lt__(self, other):
+        if not isinstance(other, type(self)):
+            other = OPCLIVersion(other)
+
+        lt = False
+        _parts_self, _parts_other = self._normalize(other)
+
+        for i, part in enumerate(_parts_self):
+            if part < _parts_other[i]:
+                lt = True
+                break
+
+        return lt
+
+    def __le__(self, other):
+        le = self.__lt__(other) or self.__eq__(other)
+        return le

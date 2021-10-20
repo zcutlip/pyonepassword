7c1113a16b69193ebe3f60a8fa80796569878090
Zachary Cutlip <uid000@gmail.com>
2021-10-20 14:55:15 -0500

    attempt to use existing signed-in session
    - prompt for password if not signed in

--- examples/create_item.py
+++ examples/create_item.py
@@ -2,17 +2,16 @@ import getpass
 
 from pyonepassword import OP, OPCreateItemException
 from pyonepassword import OPLoginItem
+from pyonepassword.py_op_exceptions import OPNotSignedInException
 
 
 def do_signin():
-    # If you've already signed in at least once, you don't need to provide all
-    # account details on future sign-ins. Just master password
-    my_password = getpass.getpass(prompt="1Password master password:\n")
-    # You may optionally provide an account shorthand if you used a custom one during initial sign-in
-    # shorthand = "arbitrary_account_shorthand"
-    # return OP(account_shorthand=shorthand, password=my_password)
-    # Or we'll try to look up account shorthand from your latest sign-in in op's config file
-    return OP(vault="Test Data", password=my_password)
+    try:
+        op = OP(vault="Test Data", use_existing_session=True, password_prompt=False)
+    except OPNotSignedInException:
+        my_password = getpass.getpass(prompt="1Password master password:\n")
+        op = OP(vault="Test Data", password=my_password)
+    return op
 
 
 def main():

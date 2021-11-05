404de071a7894a28005913eedfb16d58025b92b2
Zachary Cutlip <uid000@gmail.com>
2021-11-05 14:38:36 -0700

    export OPUser, OPInvalidUserException as API

--- pyonepassword/__init__.py
+++ pyonepassword/__init__.py
@@ -10,7 +10,11 @@ from .op_items.password import OPPasswordItem
 from .op_items.server import OPServerItem
 from .op_items.secure_note import OPSecureNoteItem
 from .op_items.credit_card import OPCreditCardItem
-
+from .op_objects import (
+    OPUser,
+    OPInvalidObjectException,
+    OPInvalidUserException
+)
 
 from .pyonepassword import OP, OP_
 

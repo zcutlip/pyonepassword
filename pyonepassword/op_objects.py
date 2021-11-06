57c1ad825adcd22fb1e9ebea5b4e8894d987d3b2
Zachary Cutlip <uid000@gmail.com>
2021-11-05 21:57:58 -0700

    added docstrings for OPUser

--- pyonepassword/op_objects.py
+++ pyonepassword/op_objects.py
@@ -22,6 +22,16 @@ class OPInvalidUserException(OPInvalidObjectException):
 
 
 class OPUser(dict):
+    """
+    A class that represents the result from an 'op get user' operation.
+    This is a dictionary unserialized from the operation's JSON output, and can be treated
+    as a normal dictionary. In addition, it has a convenience property for each key in the
+    the dictionary.
+
+    Note: date-related properties return parsed 'datetime' objects. To access the original
+    date strings, use the corresponding dictionary key
+    """
+
     def __init__(self, user_dict_or_json: Union[str, dict]):
         if isinstance(user_dict_or_json, str):
             try:
@@ -35,40 +45,64 @@ class OPUser(dict):
 
     @property
     def uuid(self):
+        """
+        str: The user object's UUID
+        """
         return self["uuid"]
 
     @property
     def created_at(self) -> datetime:
+        """
+        datetime : The createdAt attribute parsed as a datetime object
+        """
         created = self["createdAt"]
         created = fromisoformat_z(created)
         return created
 
     @property
     def updatedAt(self) -> datetime:
+        """
+        datetime : The updatedAt attribute parsed as a datetime object
+        """
         updated = self["updatedAt"]
         updated = fromisoformat_z(updated)
         return updated
 
     @property
     def last_auth_at(self) -> datetime:
-        last_auth = self["updatedAt"]
+        """
+        datetime : The lastAuthAt attribute parsed as a datetime object
+        """
+        last_auth = self["lastAuthAt"]
         last_auth = fromisoformat_z(last_auth)
         return last_auth
 
     @property
     def email(self) -> str:
+        """
+        str : The email attribute
+        """
         return self["email"]
 
     @property
     def first_name(self) -> str:
+        """
+        str : The firstName attribute
+        """
         return self["firstName"]
 
     @property
     def last_name(self) -> str:
+        """
+        str : The lastName attribute
+        """
         return self["lastName"]
 
     @property
     def name(self) -> str:
+        """
+        str : The name attribute
+        """
         return self["name"]
 
     @property
@@ -77,32 +111,56 @@ class OPUser(dict):
 
     @property
     def keyset_version(self) -> int:
+        """
+        str : The keysetVersion attribute
+        """
         return self["keysetVersion"]
 
     @property
     def state(self) -> str:
+        """
+        str : The state attribute
+        """
         return self["state"]
 
     @property
     def type(self) -> str:
+        """
+        str : The type attribute
+        """
         return self["type"]
 
     @property
     def avatar(self) -> str:
+        """
+        str : The avatar attribute
+        """
         return self["avatar"]
 
     @property
     def language(self) -> str:
+        """
+        str : The language attribute
+        """
         return self["language"]
 
     @property
     def account_key_format(self) -> str:
+        """
+        str : The accountKeyFormat attribute
+        """
         return self["accountKeyFormat"]
 
     @property
     def account_key_uuid(self) -> str:
+        """
+        str : The accountKeyUuid attribute
+        """
         return self["accountKeyUuid"]
 
     @property
     def combined_permissions(self) -> str:
+        """
+        str : The combinedPermission attribute
+        """
         return self["combinedPermissions"]

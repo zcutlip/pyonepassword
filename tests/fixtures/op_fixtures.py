7c875fb0d12637fdb14f0642c19d439f16c8eb1d
Zachary Cutlip <uid000@gmail.com>
2021-11-22 13:49:39 -0800

    print err_output if OP() instantiation fails

--- tests/fixtures/op_fixtures.py
+++ tests/fixtures/op_fixtures.py
@@ -1,6 +1,7 @@
 import os
 from pytest import fixture
 from pyonepassword import OP
+from pyonepassword.py_op_exceptions import OPCmdFailedException
 
 from .expected_data import ExpectedData
 from .expected_group_data import ExpectedGroupData
@@ -16,8 +17,12 @@ ACCOUNT_SHORTHAND = "onepassword_username"
 def _get_signed_in_op(account_shorthand, default_vault=None):
     os.environ["MOCK_OP_RESPONSE_DIRECTORY"] = str(RESP_DIRECTORY_PATH)
     os.environ["MOCK_OP_SIGNIN_SUCCEED"] = "1"
-    op = OP(vault=default_vault, account_shorthand=account_shorthand,
-            password=OP_MASTER_PASSWORD, op_path='mock-op')
+    try:
+        op = OP(vault=default_vault, account_shorthand=account_shorthand,
+                password=OP_MASTER_PASSWORD, op_path='mock-op')
+    except OPCmdFailedException as e:
+        print(f"OP() failed: {e.err_output}")
+        raise e
     return op
 
 

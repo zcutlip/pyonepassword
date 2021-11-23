8957b79d48c3cad3e79562c2daa9160dcb92044c
Zachary Cutlip <uid000@gmail.com>
2021-11-22 22:13:56 -0800

    created base class for ExpecteItem classes

---
+++ tests/fixtures/expected_item.py
@@ -0,0 +1,47 @@
+import datetime
+from typing import Dict
+
+from ..test_support._datetime import fromisoformat_z
+
+
+class ExpectedItemBase:
+    def __init__(self, item_dict: Dict):
+        self._data = item_dict
+
+    @property
+    def password(self) -> str:
+        return self._data["password"]
+
+    @property
+    def uuid(self) -> str:
+        return self._data["uuid"]
+
+    @property
+    def title(self) -> str:
+        return self._data["title"]
+
+    @property
+    def created_at(self) -> datetime.datetime:
+        created_at = self._data["createdAt"]
+        return fromisoformat_z(created_at)
+
+    @property
+    def updated_at(self) -> datetime.datetime:
+        updated_at = self._data["updatedAt"]
+        return fromisoformat_z(updated_at)
+
+    @property
+    def changer_uuid(self) -> str:
+        return self._data["changerUuid"]
+
+    @property
+    def vault_uuid(self) -> str:
+        return self._data["vaultUuid"]
+
+    @property
+    def trashed(self) -> bool:
+        return self._data["trashed"]
+
+    @property
+    def returncode(self) -> int:
+        return self._data["returncode"]
--- tests/fixtures/expected_password_item_data.py
+++ tests/fixtures/expected_password_item_data.py
@@ -1,51 +1,14 @@
-import datetime
 from typing import Dict
 
-from ..test_support._datetime import fromisoformat_z
+from .expected_item import ExpectedItemBase
 from .expected_data import ExpectedData
 
 
-class ExpectedItemPassword:
-    def __init__(self, user_dict: Dict):
-        self._data = user_dict
+class ExpectedItemPassword(ExpectedItemBase):
 
     @property
     def password(self) -> str:
         return self._data["password"]
-
-    @property
-    def uuid(self) -> str:
-        return self._data["uuid"]
-
-    @property
-    def title(self) -> str:
-        return self._data["title"]
-
-    @property
-    def created_at(self) -> datetime.datetime:
-        created_at = self._data["createdAt"]
-        return fromisoformat_z(created_at)
-
-    @property
-    def updated_at(self) -> datetime.datetime:
-        updated_at = self._data["updatedAt"]
-        return fromisoformat_z(updated_at)
-
-    @property
-    def changer_uuid(self) -> str:
-        return self._data["changerUuid"]
-
-    @property
-    def vault_uuid(self) -> str:
-        return self._data["vaultUuid"]
-
-    @property
-    def trashed(self) -> bool:
-        return self._data["trashed"]
-
-    @property
-    def returncode(self) -> int:
-        return self._data["returncode"]
 
 
 class ExpectedItemPasswordData:

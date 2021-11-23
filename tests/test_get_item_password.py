bf9e3140b04cf1cedf5741f2c96c3987c88497ea
Zachary Cutlip <uid000@gmail.com>
2021-11-22 22:02:24 -0800

    add test module for "get item password" operations

---
+++ tests/test_get_item_password.py
@@ -0,0 +1,24 @@
+from pyonepassword import OP, OPPasswordItem
+
+
+def _lookup_password_data(data, password_identifier: str):
+    item = data.data_for_password(password_identifier)
+    return item
+
+
+def test_get_item_password_01(signed_in_op: OP, expected_item_password_data):
+    password_identifier = "Example Password"
+    vault = "Test Data"
+    expected = _lookup_password_data(
+        expected_item_password_data, password_identifier)
+    result: OPPasswordItem = signed_in_op.get_item(
+        password_identifier, vault=vault)
+    assert isinstance(result, OPPasswordItem)
+    assert result.password == expected.password
+    assert result.uuid == expected.uuid
+    assert result.title == expected.title
+    assert result.created_at == expected.created_at
+    assert result.updated_at == expected.updated_at
+    assert result.changer_uuid == expected.changer_uuid
+    assert result.vault_uuid == expected.vault_uuid
+    assert result.trashed == expected.trashed

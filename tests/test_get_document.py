5282e3f0f421470180e8f67588c1a316351a9d7c
Zachary Cutlip <uid000@gmail.com>
2021-11-24 17:00:25 -0800

    refaxtor test_get_document module to use expected_document_data fixture

--- tests/test_get_document.py
+++ tests/test_get_document.py
@@ -1,36 +1,28 @@
 import pytest
 
-from typing import Dict
 from pyonepassword import OP, OPGetDocumentException
 from .test_support.util import digest
-from .fixtures.expected_data import ExpectedData
 
 
-def _lookup_item_data(data: ExpectedData, item_id: str) -> Dict:
-    item = data.lookup_document(item_id)
-    return item
-
-
-def test_get_document_01(signed_in_op: OP, expected_data):
+def test_get_document_01(signed_in_op: OP, expected_document_data):
     item_name = "Example Login 2 - 1200px-SpongeBob_SquarePants_character.svg.png.webp"
     vault = "Test Data"
-    expected = _lookup_item_data(expected_data, item_name)
+    expected = expected_document_data.data_for_document(item_name)
     filename, data = signed_in_op.get_document(item_name, vault=vault)
     sha256_digest = digest(data)
-    assert filename == expected["filename"]
-    assert len(data) == expected["size"]
-    assert sha256_digest == expected["digest"]
+    assert filename == expected.filename
+    assert len(data) == expected.size
+    assert sha256_digest == expected.digest
 
 
 @pytest.mark.parametrize("invalid_document,vault",
                          [("Invalid Document", None),
                           ("Error Success", "Test Data")])
-def test_get_document_02(signed_in_op: OP, expected_data, invalid_document, vault):
+def test_get_document_02(signed_in_op: OP, expected_document_data, invalid_document, vault):
     exception_class = OPGetDocumentException
-    expected = _lookup_item_data(expected_data, invalid_document)
+    expected = expected_document_data.data_for_document(invalid_document)
     try:
-        filename, data = signed_in_op.get_document(
-            invalid_document, vault=vault)
+        signed_in_op.get_document(invalid_document, vault=vault)
         assert False, f"We should have caught {exception_class.__name__}"
     except exception_class as e:
-        assert expected["returncode"] == e.returncode
+        assert expected.returncode == e.returncode

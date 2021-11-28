2764d57f112f096e2b99ea2c79377eb3e5932426
Zachary Cutlip <uid000@gmail.com>
2021-11-28 10:30:57 -0800

    added sample dictionary to password item module as comment

--- pyonepassword/op_items/password.py
+++ pyonepassword/op_items/password.py
@@ -1,6 +1,28 @@
 from ._op_item_type_registry import op_register_item_type
 from ._op_items_base import OPAbstractItem
 
+# {
+#     "uuid": "2dvgl7kk5yjrq3gxwqimp5awve",
+#     "templateUuid": "005",
+#     "trashed": "N",
+#     "createdAt": "2021-11-23T05:46:01Z",
+#     "updatedAt": "2021-11-23T05:46:01Z",
+#     "changerUuid": "RAXCWKNRRNGL7I3KSZOH5ERLHI",
+#     "itemVersion": 1,
+#     "vaultUuid": "yhdg6ovhkjcfhn3u25cp2bnl6e",
+#     "details": {
+#         "notesPlain": "",
+#         "password": "iXdx8KAEmUkPqCvHHYjngHzRr7",
+#         "passwordHistory": [],
+#         "sections": []
+#     },
+#     "overview": {
+#         "ainfo": "2021-11-22 09:46 PM",
+#         "ps": 100,
+#         "title": "Example Password"
+#     }
+# }
+
 
 @op_register_item_type
 class OPPasswordItem(OPAbstractItem):

d2d97fc697566d2a12c4038697c1a397da1d1734
Zachary Cutlip <uid000@gmail.com>
2021-11-30 19:11:16 -0800

    export OPServerItemTemplate as API

--- pyonepassword/__init__.py
+++ pyonepassword/__init__.py
@@ -7,7 +7,7 @@ from .__about__ import (
 from .op_items.login import OPLoginItem
 from .op_items.document import OPDocumentItem
 from .op_items.password import OPPasswordItem
-from .op_items.server import OPServerItem
+from .op_items.server import OPServerItem, OPServerItemTemplate
 from .op_items.secure_note import OPSecureNoteItem
 from .op_items.credit_card import OPCreditCardItem
 from .op_objects import (

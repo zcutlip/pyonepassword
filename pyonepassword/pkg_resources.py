245252eecc14ec94c83a59919f1577dadfef4ca9
Zachary Cutlip <uid000@gmail.com>
2021-12-10 10:06:44 -0800

    pkg_resources module for python 3.8 compatibility

--- pyonepassword/pkg_resources.py
+++ pyonepassword/pkg_resources.py
@@ -1,4 +1,4 @@
 try:
     from importlib.resources import files as pkgfiles  # noqa: F401
 except ImportError:
-    from importlib_resources import resources as pkgresources  # noqa: F401
+    from importlib_resources import files as pkgfiles  # noqa: F401

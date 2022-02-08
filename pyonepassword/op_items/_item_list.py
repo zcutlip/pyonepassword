e303d0391863ebbc2eab5b7126cd5522c219efa7
Zachary Cutlip <uid000@gmail.com>
2022-02-07 21:29:02 -0800

    OPItemList class

---
+++ pyonepassword/op_items/_item_list.py
@@ -0,0 +1,16 @@
+import json
+from typing import List
+
+from ..op_objects import OPAbstractObject
+
+from ._item_descriptor_registry import OPItemDescriptorFactory
+
+
+class OPItemList(List[OPAbstractObject]):
+    def __init__(self, item_list_json: str):
+        super().__init__()
+        item_list = json.loads(item_list_json)
+        for i_dict in item_list:
+            descriptor = OPItemDescriptorFactory.item_descriptor_from_dict(
+                i_dict)
+            self.append(descriptor)

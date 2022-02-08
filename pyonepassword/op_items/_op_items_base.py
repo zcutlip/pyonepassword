deffbdd888faed394ab2fa031b77b5e57f117399
Zachary Cutlip <uid000@gmail.com>
2022-02-07 21:36:40 -0800

    move OPItemOverview to its own modle
    - rename OPItemOverview in this module to OPMutableItemMutableOverview

--- pyonepassword/op_items/_op_items_base.py
+++ pyonepassword/op_items/_op_items_base.py
@@ -8,48 +8,12 @@ from abc import ABC, abstractmethod
 from typing import List, Union
 
 from .._datetime import fromisoformat_z
+from ._item_overview import OPItemOverview, URLEntry
 from .item_section import OPSection, OPSectionField, OPSectionCollisionException
 from .templates import TemplateDirectory
 
 
-class OPItemOverview(dict):
-    class URLEntry(dict):
-        def __init__(self, url_dict):
-            ud = copy.deepcopy(url_dict)
-            super().__init__(ud)
-
-        @property
-        def label(self):
-            return self["l"]
-
-        @property
-        def url(self):
-            return self["u"]
-
-    def __init__(self, overview_dict):
-        od = copy.deepcopy(overview_dict)
-        super().__init__(od)
-        _urls = self._process_urls()
-        if _urls is not None:
-            self["URLs"] = _urls
-
-    def _process_urls(self):
-        # Some item types, namely Login and Password can
-        # have a URLs dict in their overview
-        # other item types do not. We want to make accessing those
-        # transparent, making them available if they exist, but not
-        # failing if they don't
-        url_items = None
-        url_dicts = self.get("URLs", [])
-        if url_dicts:
-            url_items = []
-            for d in url_dicts:
-                url = self.URLEntry(d)
-                url_items.append(url)
-        return url_items
-
-    def url_list(self):
-        return self.get("URLs", None)
+class OPMutableItemOverview(OPItemOverview):
 
     def set_url(self, url, label=""):
         url_dict = {
@@ -57,7 +21,7 @@ class OPItemOverview(dict):
             "u": url
         }
         # TODO: is it an error if we alread have one or more URLs?
-        new_url = self.URLEntry(url_dict)
+        new_url = URLEntry(url_dict)
         self["URLs"] = [new_url]
 
 

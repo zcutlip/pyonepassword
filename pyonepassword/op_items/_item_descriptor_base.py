81e74128c97d38e44cbf8bb6765345b1be1ca72c
Zachary Cutlip <uid000@gmail.com>
2022-02-07 21:27:26 -0800

    base module for OP item descriptor class

---
+++ pyonepassword/op_items/_item_descriptor_base.py
@@ -0,0 +1,58 @@
+import datetime
+
+from abc import ABC, abstractmethod
+
+from .._datetime import fromisoformat_z
+from ._item_overview import OPItemOverview
+
+
+class OPAbstractItemDescriptor(ABC):
+    TEMPLATE_ID = None
+    ITEM_CATEGORY = None
+
+    @abstractmethod
+    def __init__(self, item_dict):
+        self._from_template = False
+        self._item_dict = item_dict
+        # not every item has an overview
+        # in particular, items created from a template do not
+        overview = self._item_dict.get("overview", {})
+        self._overview = OPItemOverview(overview)
+
+    @property
+    def uuid(self) -> str:
+        return self._item_dict["uuid"]
+
+    @property
+    def title(self) -> str:
+        title = self._overview["title"]
+        return title
+
+    @property
+    def created_at(self) -> datetime.datetime:
+        created = self._item_dict["createdAt"]
+        created = fromisoformat_z(created)
+        return created
+
+    @property
+    def updated_at(self) -> datetime.datetime:
+        updated = self._item_dict["updatedAt"]
+        updated = fromisoformat_z(updated)
+        return updated
+
+    @property
+    def changer_uuid(self) -> str:
+        return self._item_dict["changerUuid"]
+
+    @property
+    def vault_uuid(self) -> str:
+        return self._item_dict["vaultUuid"]
+
+    @property
+    def trashed(self) -> bool:
+        trashed: str = self._item_dict["trashed"]
+        if trashed.lower() == "y":
+            trashed = True
+        elif trashed.lower() == "n":
+            trashed = False
+        return trashed

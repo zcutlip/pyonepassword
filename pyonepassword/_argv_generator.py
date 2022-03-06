4951604ee0d028bd2e7564ff75f879f590d362b4
Zachary Cutlip <uid000@gmail.com>
2022-03-05 18:12:26 -0800

    pass OPArgvGenerator a cli_version rather than op_exe
    - add methods to get sign-in related argv

--- pyonepassword/_argv_generator.py
+++ pyonepassword/_argv_generator.py
@@ -1,19 +1,24 @@
-from .op_cli_version import OPCLIVersion, MINIMUM_VERSION_2
-from .op_cli import OPArgvCommon, OPArgvV1
-from .op_cli import _OPCLIExecute
+from .op_cli import OPArgvCommon
+from .op_cli._op_argv_v1 import _OPArgv as OPArgv_V1
+from .op_cli_version import MINIMUM_VERSION_2
 
 
 class OPArgvGenerator:
 
-    def __init__(self, op_exe, decode="utf-8"):
-        argv = self.cli_version_argv(op_exe)
-        version_output = _OPCLIExecute._run(
-            argv, capture_stdout=True, decode=decode)
-        version = OPCLIVersion(version_output)
+    def __init__(self, cli_version, decode="utf-8"):
         self._op_argv = None
-        if version < MINIMUM_VERSION_2:
-            self._op_argv = OPArgvV1
-        self.version = version
+        if cli_version < MINIMUM_VERSION_2:
+            self._op_argv = OPArgv_V1
+        self.version = cli_version
+
+    def normal_signin_argv(self, op_exe, account_shorthand=None):
+        argv = self._op_argv.normal_signin_argv(
+            op_exe, account_shorthand=account_shorthand)
+        return argv
+
+    def get_verify_signin_argv(self, op_exe):
+        argv = self._op_argv.get_verify_signin_argv(op_exe)
+        return argv
 
     def cli_version_argv(self, op_exe):
         argv = OPArgvCommon.cli_version_argv(op_exe)

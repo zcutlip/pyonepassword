a1686adb5938be2e44493dc34b671eb079c88a70
Zachary Cutlip <uid000@gmail.com>
2022-04-10 19:20:49 -0700

    refactor op_items.login for op version 2.0

--- pyonepassword/op_items/login.py
+++ pyonepassword/op_items/login.py
@@ -6,7 +6,7 @@ from ._item_descriptor_registry import op_register_item_descriptor_type
 
 @op_register_item_descriptor_type
 class OPLoginDescriptorItem(OPAbstractItemDescriptor):
-    TEMPLATE_ID = "001"
+    CATEGORY = "LOGIN"
 
     def __init__(self, item_dict):
         super().__init__(item_dict)
@@ -14,65 +14,20 @@ class OPLoginDescriptorItem(OPAbstractItemDescriptor):
 
 @op_register_item_type
 class OPLoginItem(OPAbstractItem):
-    TEMPLATE_ID = "001"
-    ITEM_CATEGORY = "Login"
+    CATEGORY = "LOGIN"
 
     def __init__(self, item_dict, **kwargs):
         super().__init__(item_dict, **kwargs)
 
-    def get_details_subfield(self, subfield_designation):
-        """
-        "details": {
-            "fields": [
-            {
-                "designation": "password",
-                "name": "password",
-                "type": "P",
-                "value": "doth-parrot-hid-tussock-veldt"
-            },
-            {
-                "designation": "username",
-                "name": "username",
-                "type": "T",
-                "value": "zcutlip"
-            }
-            ],
-            "notesPlain": ""
-        }
-        """
-        subfields = self.get_details_value("fields")
-        field = None
-        for f in subfields:
-            if f["designation"] == subfield_designation:
-                field = f
-                break
-        return field
-
-    def get_details_subfield_value(self, field_designation):
-        field_value = None
-        field = self.get_details_subfield(field_designation)
-        field_value = field["value"]
-        return field_value
-
     @property
     def username(self):
-        username = self.get_details_subfield_value("username")
+        username = self.field_value_by_id("username")
         return username
-
-    @username.setter
-    def username(self, username: str):
-        username_field = self.get_details_subfield("username")
-        username_field["value"] = username
 
     @property
     def password(self):
-        password = self.get_details_subfield_value("password")
+        password = self.field_value_by_id("password")
         return password
-
-    @password.setter
-    def password(self, password: str):
-        password_field = self.get_details_subfield("password")
-        password_field["value"] = password
 
 
 class OPLoginItemTemplate(OPItemTemplateMixin, OPLoginItem):

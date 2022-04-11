fc9098d6646d4ded2e0b3b829e43be6dd0621666
Zachary Cutlip <uid000@gmail.com>
2022-04-11 14:05:02 -0700

    update "get document" example to check for biometric

--- examples/example-signin-get-document.py
+++ examples/example-signin-get-document.py
@@ -1,6 +1,7 @@
 import getpass
 import os
 import sys
+
 parent_path = os.path.dirname(
     os.path.dirname(
         os.path.abspath(__file__)
@@ -11,23 +12,34 @@ if parent_path not in sys.path:
 
 from pyonepassword import (  # noqa: E402
     OP,
-    OPSigninException,
+    OPConfigNotFoundException,
     OPGetDocumentException,
     OPInvalidDocumentException,
     OPNotFoundException,
-    OPConfigNotFoundException
+    OPNotSignedInException,
+    OPSigninException
 )
 
 
 def do_signin():
-    # If you've already signed in at least once, you don't need to provide all
-    # account details on future sign-ins. Just master password
-    my_password = getpass.getpass(prompt="1Password master password:\n")
-    # You may optionally provide an account shorthand if you used a custom one during initial sign-in
-    # shorthand = "arbitrary_account_shorthand"
-    # return OP(account_shorthand=shorthand, password=my_password)
-    # Or we'll try to look up account shorthand from your latest sign-in in op's config file
-    return OP(password=my_password)
+    # op_path = "op-binaries/2.0.0/op"
+    op_path = "op"
+    uses_biometric = OP.uses_biometric(op_path=op_path)
+    try:
+        op = OP(op_path=op_path,
+                use_existing_session=True, password_prompt=False)
+    except OPNotSignedInException as e:
+        if uses_biometric:
+            raise e
+        # If you've already signed in at least once, you don't need to provide all
+        # account details on future sign-ins. Just master password
+        my_password = getpass.getpass(prompt="1Password master password:\n")
+        # You may optionally provide an account shorthand if you used a custom one during initial sign-in
+        # shorthand = "arbitrary_account_shorthand"
+        # return OP(account_shorthand=shorthand, password=my_password)
+        # Or we'll try to look up account shorthand from your latest sign-in in op's config file
+        op = OP(password=my_password)
+    return op
 
 
 if __name__ == "__main__":

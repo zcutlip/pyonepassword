10d165682bedc8ee5c3a42fe42aa5d4ac390a1e4
Zachary Cutlip <uid000@gmail.com>
2022-04-11 13:59:10 -0700

    add 'OPDocumentFile' class

--- pyonepassword/op_items/document.py
+++ pyonepassword/op_items/document.py
@@ -4,6 +4,27 @@ from ._op_item_type_registry import op_register_item_type
 from ._op_items_base import OPAbstractItem
 
 
+class OPDocumentFile(dict):
+    def __init__(self, file_dict):
+        super().__init__(file_dict)
+
+    @property
+    def file_id(self) -> str:
+        return self["id"]
+
+    @property
+    def name(self) -> str:
+        return self["name"]
+
+    @property
+    def size(self) -> int:
+        return self["size"]
+
+    @property
+    def content_path(self) -> str:
+        return self["content_path"]
+
+
 @op_register_item_descriptor_type
 class OPDocumentItemDescriptor(OPAbstractItemDescriptor):
     TEMPLATE_ID = "006"

adab78bbc2769f26a69b5641fa68bc19c5c71004
Zachary Cutlip <uid000@gmail.com>
2022-04-12 16:40:42 -0700

    update "get vault" example

--- examples/example-signin-get-vault.py
+++ examples/example-signin-get-vault.py
@@ -11,7 +11,7 @@ parent_path = os.path.dirname(
 if parent_path not in sys.path:
     sys.path.append(parent_path)
 
-from pyonepassword import OPGetVaultException  # noqa: E402
+from pyonepassword import OPVault, OPGetVaultException  # noqa: E402
 
 
 if __name__ == "__main__":
@@ -21,14 +21,22 @@ if __name__ == "__main__":
     print("Signed in.")
     print("Looking up vault \"Test Data\"...")
     try:
-        vault_dict = op.get_vault("Test Data")
-        print(vault_dict)
+        vault_1: OPVault = op.get_vault("Test Data")
+        print(vault_1)
         print("")
         print("Vaults can also be looked up by their uuid")
         print("")
         print("Looking up uuid \"yhdg6ovhkjcfhn3u25cp2bnl6e\"...")
-        vault_dict_2 = op.get_vault("yhdg6ovhkjcfhn3u25cp2bnl6e")
-        print("Vault dictionaries match? {}".format(vault_dict == vault_dict_2))
+        vault_2: OPVault = op.get_vault("yhdg6ovhkjcfhn3u25cp2bnl6e")
+        if (vault_1.unique_id == vault_2.unique_id and
+                vault_1.name == vault_2.name):
+            pass
+        print("Vault dictionaries match? {}".format(vault_1.unique_id == vault_2.unique_id and
+                                                    vault_1.name == vault_2.name and
+                                                    vault_1.description == vault_2.description and
+                                                    vault_1.items == vault_2.items and
+                                                    vault_1.created_at == vault_2.created_at and
+                                                    vault_1.updated_at == vault_2.updated_at))
     except OPGetVaultException as ope:
         print("1Password lookup failed: {}".format(ope))
         print(ope.err_output)

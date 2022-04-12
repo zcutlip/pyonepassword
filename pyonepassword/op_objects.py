9d0c7fb80247400c450f1e207f2c2bac67e7f3fd
Zachary Cutlip <uid000@gmail.com>
2022-04-12 16:33:24 -0700

    refactor vault, user, & group objects for op v2.0

--- pyonepassword/op_objects.py
+++ pyonepassword/op_objects.py
@@ -89,11 +89,18 @@ class OPAbstractObject(dict, metaclass=ABCMeta):
         super().__init__(obj_dict)
 
     @property
-    def uuid(self):
+    def unique_id(self):
         """
-        str: The object's UUID
+        str: The object's "id" field
         """
-        return self["uuid"]
+        return self["id"]
+
+    @property
+    def name(self) -> str:
+        """
+        str : The name attribute
+        """
+        return self["name"]
 
 
 class OPBaseObject(OPAbstractObject):
@@ -104,18 +111,18 @@ class OPBaseObject(OPAbstractObject):
     @property
     def created_at(self) -> datetime:
         """
-        datetime : The createdAt attribute parsed as a datetime object
+        datetime : The created_at attribute parsed as a datetime object
         """
-        created = self["createdAt"]
+        created = self["created_at"]
         created = fromisoformat_z(created)
         return created
 
     @property
     def updated_at(self) -> datetime:
         """
-        datetime : The updatedAt attribute parsed as a datetime object
+        datetime : The updated_at attribute parsed as a datetime object
         """
-        updated = self["updatedAt"]
+        updated = self["updated_at"]
         updated = fromisoformat_z(updated)
         return updated
 
@@ -153,59 +160,11 @@ class OPUser(OPBaseObject):
                 f"Failed to unserialize user json: {jdce}", user_dict_or_json) from jdce
 
     @property
-    def last_auth_at(self) -> datetime:
-        """
-        datetime : The lastAuthAt attribute parsed as a datetime object
-        """
-        last_auth = self["lastAuthAt"]
-        last_auth = fromisoformat_z(last_auth)
-        return last_auth
-
-    @property
     def email(self) -> str:
         """
         str : The email attribute
         """
         return self["email"]
-
-    @property
-    def first_name(self) -> str:
-        """
-        str : The firstName attribute
-        """
-        return self["firstName"]
-
-    @property
-    def last_name(self) -> str:
-        """
-        str : The lastName attribute
-        """
-        return self["lastName"]
-
-    @property
-    def name(self) -> str:
-        """
-        str : The name attribute
-        """
-        return self["name"]
-
-    @property
-    def attr_version(self) -> int:
-        return self["attrVersion"]
-
-    @property
-    def keyset_version(self) -> int:
-        """
-        str : The keysetVersion attribute
-        """
-        return self["keysetVersion"]
-
-    @property
-    def state(self) -> str:
-        """
-        str : The state attribute
-        """
-        return self["state"]
 
     @property
     def type(self) -> str:
@@ -215,39 +174,20 @@ class OPUser(OPBaseObject):
         return self["type"]
 
     @property
-    def avatar(self) -> str:
+    def state(self) -> str:
         """
-        str : The avatar attribute
+        str : The state attribute
         """
-        return self["avatar"]
+        return self["state"]
 
     @property
-    def language(self) -> str:
+    def last_auth_at(self) -> datetime:
         """
-        str : The language attribute
+        datetime : The lastAuthAt attribute parsed as a datetime object
         """
-        return self["language"]
-
-    @property
-    def account_key_format(self) -> str:
-        """
-        str : The accountKeyFormat attribute
-        """
-        return self["accountKeyFormat"]
-
-    @property
-    def account_key_uuid(self) -> str:
-        """
-        str : The accountKeyUuid attribute
-        """
-        return self["accountKeyUuid"]
-
-    @property
-    def combined_permissions(self) -> int:
-        """
-        str : The combinedPermission attribute
-        """
-        return self["combinedPermissions"]
+        last_auth = self["last_auth_at"]
+        last_auth = fromisoformat_z(last_auth)
+        return last_auth
 
 
 class OPGroup(OPBaseObject):
@@ -283,59 +223,26 @@ class OPGroup(OPBaseObject):
                 f"Failed to unserialize group json: {jdce}") from jdce
 
     @property
-    def type(self) -> str:
-        """
-        str : The type attribute
-        """
-        return self["type"]
-
-    @property
-    def name(self) -> str:
-        """
-        str : The name attribute
-        """
-        return self["name"]
-
-    @property
-    def desc(self) -> str:
+    def description(self) -> str:
         """
         The desc attribute
         """
-        return self["desc"]
-
-    @property
-    def created_at(self) -> datetime:
-        """
-        datetime : The createdAt attribute parsed as a datetime object
-        """
-        created = self["createdAt"]
-        created = fromisoformat_z(created)
-        return created
-
-    @property
-    def updated_at(self) -> datetime:
-        """
-        datetime : The updatedAt attribute parsed as a datetime object
-        """
-        updated = self["updatedAt"]
-        updated = fromisoformat_z(updated)
-        return updated
-
-    @property
-    def active_keyset_uuid(self) -> str:
-        return self["activeKeysetUuid"]
-
-    @property
-    def attr_version(self) -> int:
-        return self["attrVersion"]
+        return self["description"]
 
     @property
     def state(self) -> str:
         return self["state"]
 
     @property
-    def permissions(self) -> int:
+    def permissions(self) -> List[str]:
         return self["permissions"]
+
+    @property
+    def type(self) -> str:
+        """
+        str : The type attribute
+        """
+        return self["type"]
 
 
 class OPUserDescriptor(OPAbstractObject):
@@ -347,27 +254,6 @@ class OPUserDescriptor(OPAbstractObject):
         super().__init__(user_descriptor_dict)
 
     @property
-    def first_name(self) -> str:
-        """
-        str : The firstName attribute
-        """
-        return self["firstName"]
-
-    @property
-    def last_name(self) -> str:
-        """
-        str : The lastName attribute
-        """
-        return self["lastName"]
-
-    @property
-    def name(self) -> str:
-        """
-        str : The name attribute
-        """
-        return self["name"]
-
-    @property
     def email(self) -> str:
         """
         str : The email attribute
@@ -375,11 +261,11 @@ class OPUserDescriptor(OPAbstractObject):
         return self["email"]
 
     @property
-    def avatar(self) -> str:
+    def type(self) -> str:
         """
-        str : The avatar attribute
+        str : The type attribute
         """
-        return self["avatar"]
+        return self["type"]
 
     @property
     def state(self) -> str:
@@ -387,13 +273,6 @@ class OPUserDescriptor(OPAbstractObject):
         str : The state attribute
         """
         return self["state"]
-
-    @property
-    def type(self) -> str:
-        """
-        str : The type attribute
-        """
-        return self["type"]
 
 
 class OPUserDescriptorList(_OPDescriptorList[OPUserDescriptor]):
@@ -422,33 +301,24 @@ class OPGroupDescriptor(OPAbstractObject):
     """
     A dictionary describing a group as returned by 'op list groups'. This is a subset of a full OPGroupOboject.
     """
-    @property
-    def type(self) -> str:
-        """
-        str : The type attribute
-        """
-        return self["type"]
 
     @property
-    def name(self) -> str:
-        """
-        str : The name attribute
-        """
-        return self["name"]
-
-    @property
-    def desc(self) -> str:
+    def description(self) -> str:
         """
         The desc attribute
         """
-        return self["desc"]
+        return self["description"]
+
+    @property
+    def state(self) -> str:
+        return self["state"]
 
     @property
     def created_at(self) -> datetime:
         """
-        datetime : The createdAt attribute parsed as a datetime object
+        datetime : The created_at attribute parsed as a datetime object
         """
-        created = self["createdAt"]
+        created = self["created_at"]
         created = fromisoformat_z(created)
         return created
 
@@ -485,13 +355,6 @@ class OPVaultDescriptor(OPAbstractObject):
     def __init__(self, vault_dict_or_json):
         super().__init__(vault_dict_or_json)
 
-    @property
-    def name(self) -> str:
-        """
-        str : The vault's name
-        """
-        return self["name"]
-
 
 class OPVault(OPVaultDescriptor):
     """
@@ -507,6 +370,17 @@ class OPVault(OPVaultDescriptor):
                 f"Failed to unserialize vault json: {jdce}", vault_dict_or_json) from jdce
 
     @property
+    def description(self) -> str:
+        """
+        str : The vault description
+        """
+        return self["description"]
+
+    @property
+    def items(self) -> int:
+        return self["items"]
+
+    @property
     def type(self) -> str:
         """
         str: The "type" attribute of the vault.
@@ -514,20 +388,22 @@ class OPVault(OPVaultDescriptor):
         return self["type"]
 
     @property
-    def desc(self) -> str:
+    def created_at(self) -> datetime:
         """
-        str : The vault description
+        datetime : The created_at attribute parsed as a datetime object
         """
-        return self["desc"]
+        created = self["created_at"]
+        created = fromisoformat_z(created)
+        return created
 
     @property
-    def avatar(self) -> Union[str, None]:
+    def updated_at(self) -> datetime:
         """
-        Union[str, None] : The avatar filename associated with this vault
-
-        Note: This object does not appear to be queryable in any way
+        datetime : The updated_at attribute parsed as a datetime object
         """
-        return self.get("avatar", None)
+        updated = self["updated_at"]
+        updated = fromisoformat_z(updated)
+        return updated
 
 
 class OPVaultDescriptorList(_OPDescriptorList[OPVaultDescriptor]):

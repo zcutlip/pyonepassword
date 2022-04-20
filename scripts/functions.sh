afd5088fa6d46e89c67c212014fb96b5966cb44d
Zachary Cutlip <uid000@gmail.com>
2022-04-19 20:24:41 -0700

    add functions to get real path, and to check md5 sums

--- scripts/functions.sh
+++ scripts/functions.sh
@@ -1,3 +1,4 @@
+# shellcheck shell=dash
 # In shell, success is exit(0), error is anything else, e.g., exit(1)
 SUCCESS=0
 FAILURE=1
@@ -5,15 +6,15 @@ FAILURE=1
 quit(){
     if [ $# -gt 1 ];
     then
-        echo $1
+        echo "$1"
         shift
     fi
-    exit $1
+    exit "$1"
 }
 
 branch_is_master(){
-    local branch=$(git rev-parse --abbrev-ref HEAD)
-    if [ $branch == "master" ];
+    _branch=$(git rev-parse --abbrev-ref HEAD)
+    if [ "$_branch" = "master" ];
     then
         return $SUCCESS;
     else
@@ -22,8 +23,8 @@ branch_is_master(){
 }
 
 branch_is_clean(){
-    local modified=$(git ls-files -m) || quit "Unable to check for modified files." $?
-    if [ -z "$modified" ];
+    _modified=$(git ls-files -m) || quit "Unable to check for modified files." $?
+    if [ -z "$_modified" ];
     then
         return $SUCCESS;
     else
@@ -32,16 +33,16 @@ branch_is_clean(){
 }
 
 current_version() {
-    local version="$(python ./setup.py --version)" || quit "Unable to detect package version" $?
-    printf "%s" "$version"
+    _version="$(python ./setup.py --version)" || quit "Unable to detect package version" $?
+    printf "%s" "$_version"
 }
 
 version_is_tagged(){
-    local version="$1"
+    _version="$1"
     # e.g., verion = 0.1.0
     # check if git tag -l v0.1.0 exists
-    tag_description=$(git tag -l v"$version")
-    if [ ! -z "$tag_description" ];
+    tag_description=$(git tag -l v"$_version")
+    if [ -n "$tag_description" ];
     then
         return $SUCCESS;
     else
@@ -51,14 +52,35 @@ version_is_tagged(){
 
 prompt_yes_no(){
     prompt_string="$1"
-    read -p "$prompt_string [Y/n] " response
+    read -r -p "$prompt_string [Y/n] " response
 
     case $response in
-    [yY][eE][sS]|[yY]) 
+    [yY][eE][sS]|[yY])
         return $SUCCESS
         ;;
         *)
         return $FAILURE
         ;;
     esac
+}
+
+_readlink(){ readlink "$1" || echo "$1"; }
+
+# Don't shadow the 'realpath' executable which may be installed on
+# some systems (e.g., via homebrew)
+_realpath() { cd "$(dirname "$0")" && _readlink "$(pwd)"/"$(basename "$0")"; }
+
+md5check(){
+    _file1="$1"
+    _file2="$2"
+    hash1="$(md5sum "$_file1" | awk '{ print $1 }')"
+    hash2="$(md5sum "$_file2" | awk '{ print $1 }')"
+    [ "$hash1" = "$hash2" ]
+    ret=$?
+    if [ $ret -ne 0 ];
+    then
+        echo "$hash1"
+        echo "$hash2"
+    fi
+    return $ret
 }

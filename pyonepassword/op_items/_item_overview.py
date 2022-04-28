24ebe72d65d4b8bb4f9f09716ebe92c9791b2442
Zachary Cutlip <uid000@gmail.com>
2022-04-28 10:19:06 -0700

    remove OPItemOverview class

--- pyonepassword/op_items/_item_overview.py
+++ pyonepassword/op_items/_item_overview.py
@@ -13,31 +13,3 @@ class URLEntry(dict):
     @property
     def url(self):
         return self["u"]
-
-
-class OPItemOverview(dict):
-
-    def __init__(self, overview_dict):
-        od = copy.deepcopy(overview_dict)
-        super().__init__(od)
-        _urls = self._process_urls()
-        if _urls is not None:
-            self["URLs"] = _urls
-
-    def _process_urls(self):
-        # Some item types, namely Login and Password can
-        # have a URLs dict in their overview
-        # other item types do not. We want to make accessing those
-        # transparent, making them available if they exist, but not
-        # failing if they don't
-        url_items = None
-        url_dicts = self.get("URLs", [])
-        if url_dicts:
-            url_items = []
-            for d in url_dicts:
-                url = URLEntry(d)
-                url_items.append(url)
-        return url_items
-
-    def url_list(self):
-        return self.get("URLs", None)

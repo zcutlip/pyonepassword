d7ba18be446e7bed16556dae16395ebaddc4debb
Zachary Cutlip <uid000@gmail.com>
2022-04-29 11:15:41 -0700

    update "test_get_secure_note"

--- tests/test_get_secure_note.py
+++ tests/test_get_secure_note.py
@@ -22,17 +22,16 @@ def _lookup_note_data(data, note_identifier: str):
 def test_get_secure_note_item_01(signed_in_op: OP, expected_secure_note_item_data, note_identifier, vault):
     expected = _lookup_note_data(
         expected_secure_note_item_data, note_identifier)
-    result: OPSecureNoteItem = signed_in_op.get_item(
+    result: OPSecureNoteItem = signed_in_op.item_get(
         note_identifier, vault=vault)
     assert isinstance(result, OPSecureNoteItem)
     assert result.note_text == expected.note_text
-    assert result.uuid == expected.uuid
+    assert result.unique_id == expected.unique_id
     assert result.title == expected.title
     assert result.created_at == expected.created_at
     assert result.updated_at == expected.updated_at
-    assert result.changer_uuid == expected.changer_uuid
-    assert result.vault_uuid == expected.vault_uuid
-    assert result.trashed == expected.trashed
+    assert result.last_edited_by == expected.last_edited_by
+    assert result.vault_id == expected.vault_id
 
 
 @pytest.mark.parametrize("invalid_note,vault",
@@ -43,7 +42,7 @@ def test_get_secure_note_item_02(signed_in_op: OP, expected_secure_note_item_dat
     expected = _lookup_note_data(
         expected_secure_note_item_data, invalid_note)
     try:
-        signed_in_op.get_item(invalid_note, vault=vault)
+        signed_in_op.item_get(invalid_note, vault=vault)
         assert False, f"We should have caught {exception_class.__name__}"
     except exception_class as e:
         assert e.returncode == expected.returncode

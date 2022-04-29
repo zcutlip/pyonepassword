ea66a5368159b72fb32c14cd31d6c8eb61a13142
Zachary Cutlip <uid000@gmail.com>
2022-04-29 10:33:35 -0700

    break credit card item tests out into individual tests

--- tests/test_get_item_credit_card.py
+++ tests/test_get_item_credit_card.py
@@ -5,25 +5,131 @@ if TYPE_CHECKING:
     from .fixtures.expected_credit_card import ExpectedCreditCardData
 
 
-def test_get_item_password_01(signed_in_op: OP, expected_credit_card_data: ExpectedCreditCardData):
+def test_get_item_credit_card_01(signed_in_op: OP, expected_credit_card_data: ExpectedCreditCardData):
     credit_card_id = "Example Credit Card"
     vault = "Test Data"
     expected = expected_credit_card_data.data_for_credit_card(credit_card_id)
-    result: OPCreditCardItem = signed_in_op.get_item(
+    result: OPCreditCardItem = signed_in_op.item_get(
         credit_card_id, vault=vault)
 
     assert result.credit_card_number == expected.credit_card_number
+
+
+def test_get_item_credit_card_02(signed_in_op: OP, expected_credit_card_data: ExpectedCreditCardData):
+    credit_card_id = "Example Credit Card"
+    vault = "Test Data"
+    expected = expected_credit_card_data.data_for_credit_card(credit_card_id)
+    result: OPCreditCardItem = signed_in_op.item_get(
+        credit_card_id, vault=vault)
+
     assert result.cvv == expected.cvv
+
+
+def test_get_item_credit_card_03(signed_in_op: OP, expected_credit_card_data: ExpectedCreditCardData):
+    credit_card_id = "Example Credit Card"
+    vault = "Test Data"
+    expected = expected_credit_card_data.data_for_credit_card(credit_card_id)
+    result: OPCreditCardItem = signed_in_op.item_get(
+        credit_card_id, vault=vault)
+
     assert result.expiry_date == expected.expiry_date
+
+
+def test_get_item_credit_card_04(signed_in_op: OP, expected_credit_card_data: ExpectedCreditCardData):
+    credit_card_id = "Example Credit Card"
+    vault = "Test Data"
+    expected = expected_credit_card_data.data_for_credit_card(credit_card_id)
+    result: OPCreditCardItem = signed_in_op.item_get(
+        credit_card_id, vault=vault)
+
     assert result.valid_from == expected.valid_from
+
+
+def test_get_item_credit_card_05(signed_in_op: OP, expected_credit_card_data: ExpectedCreditCardData):
+    credit_card_id = "Example Credit Card"
+    vault = "Test Data"
+    expected = expected_credit_card_data.data_for_credit_card(credit_card_id)
+    result: OPCreditCardItem = signed_in_op.item_get(
+        credit_card_id, vault=vault)
+
     assert result.pin == expected.pin
+
+
+def test_get_item_credit_card_06(signed_in_op: OP, expected_credit_card_data: ExpectedCreditCardData):
+    credit_card_id = "Example Credit Card"
+    vault = "Test Data"
+    expected = expected_credit_card_data.data_for_credit_card(credit_card_id)
+    result: OPCreditCardItem = signed_in_op.item_get(
+        credit_card_id, vault=vault)
+
     assert result.credit_limit == expected.credit_limit
+
+
+def test_get_item_credit_card_07(signed_in_op: OP, expected_credit_card_data: ExpectedCreditCardData):
+    credit_card_id = "Example Credit Card"
+    vault = "Test Data"
+    expected = expected_credit_card_data.data_for_credit_card(credit_card_id)
+    result: OPCreditCardItem = signed_in_op.item_get(
+        credit_card_id, vault=vault)
+
     assert result.cash_withdrawal_limit == expected.cash_withdrawal_limit
+
+
+def test_get_item_credit_card_08(signed_in_op: OP, expected_credit_card_data: ExpectedCreditCardData):
+    credit_card_id = "Example Credit Card"
+    vault = "Test Data"
+    expected = expected_credit_card_data.data_for_credit_card(credit_card_id)
+    result: OPCreditCardItem = signed_in_op.item_get(
+        credit_card_id, vault=vault)
+
     assert result.interest_rate == expected.interest_rate
-    assert result.uuid == expected.uuid
+
+
+def test_get_item_credit_card_09(signed_in_op: OP, expected_credit_card_data: ExpectedCreditCardData):
+    credit_card_id = "Example Credit Card"
+    vault = "Test Data"
+    expected = expected_credit_card_data.data_for_credit_card(credit_card_id)
+    result: OPCreditCardItem = signed_in_op.item_get(
+        credit_card_id, vault=vault)
+
+    assert result.unique_id == expected.unique_id
+
+
+def test_get_item_credit_card_10(signed_in_op: OP, expected_credit_card_data: ExpectedCreditCardData):
+    credit_card_id = "Example Credit Card"
+    vault = "Test Data"
+    expected = expected_credit_card_data.data_for_credit_card(credit_card_id)
+    result: OPCreditCardItem = signed_in_op.item_get(
+        credit_card_id, vault=vault)
+
     assert result.title == expected.title
+
+
+def test_get_item_credit_card_11(signed_in_op: OP, expected_credit_card_data: ExpectedCreditCardData):
+    credit_card_id = "Example Credit Card"
+    vault = "Test Data"
+    expected = expected_credit_card_data.data_for_credit_card(credit_card_id)
+    result: OPCreditCardItem = signed_in_op.item_get(
+        credit_card_id, vault=vault)
+
     assert result.created_at == expected.created_at
+
+
+def test_get_item_credit_card_12(signed_in_op: OP, expected_credit_card_data: ExpectedCreditCardData):
+    credit_card_id = "Example Credit Card"
+    vault = "Test Data"
+    expected = expected_credit_card_data.data_for_credit_card(credit_card_id)
+    result: OPCreditCardItem = signed_in_op.item_get(
+        credit_card_id, vault=vault)
+
     assert result.updated_at == expected.updated_at
-    assert result.changer_uuid == expected.changer_uuid
-    assert result.vault_uuid == expected.vault_uuid
-    assert result.trashed == expected.trashed
+
+
+def test_get_item_credit_card_13(signed_in_op: OP, expected_credit_card_data: ExpectedCreditCardData):
+    credit_card_id = "Example Credit Card"
+    vault = "Test Data"
+    expected = expected_credit_card_data.data_for_credit_card(credit_card_id)
+    result: OPCreditCardItem = signed_in_op.item_get(
+        credit_card_id, vault=vault)
+
+    assert result.last_edited_by == expected.last_edited_by

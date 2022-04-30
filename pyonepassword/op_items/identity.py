f9a6aa7c5c91f2003a7c9de84255e25cba57571e
Zachary Cutlip <uid000@gmail.com>
2022-04-30 12:57:43 -0700

    set CATEGORY for "itendity" item descriptor

--- pyonepassword/op_items/identity.py
+++ pyonepassword/op_items/identity.py
@@ -4,7 +4,7 @@ from ._item_descriptor_registry import op_register_item_descriptor_type
 
 @op_register_item_descriptor_type
 class OPIdentityItemDescriptor(OPAbstractItemDescriptor):
-    CATEGORY = None
+    CATEGORY = "IDENTITY"
 
     def __init__(self, item_dict):
         super().__init__(item_dict)

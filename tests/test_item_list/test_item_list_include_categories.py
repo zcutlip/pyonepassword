f5d2c762fd3c6c9ccfeb757dc031d525ca6d11be
Zachary Cutlip <uid000@gmail.com>
2022-04-30 15:59:08 -0700

    add test module to list items including categories

---
+++ tests/test_item_list/test_item_list_include_categories.py
@@ -0,0 +1,36 @@
+from __future__ import annotations
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from pyonepassword import OP
+
+from pyonepassword import OPItemList
+from pyonepassword.op_items.login import OPLoginDescriptorItem
+
+
+def _item_by_id(item_list: OPItemList, item_id):
+    item = None
+    for item in item_list:
+        if item.unique_id == item_id:
+            break
+    return item
+
+
+def _items_by_title(item_list: OPItemList, item_title):
+    items = []
+    for item in item_list:
+        if item.title == item_title:
+            items.append(item)
+    return items
+
+
+def test_items_list_identity_01(signed_in_op: OP, expected_login_item_data):
+    login_item_title = "Example Login 1"
+    items: OPItemList = signed_in_op.item_list(
+        vault="Test Data", categories=["login", "identity"])
+    result = _items_by_title(items, login_item_title)
+    result: OPLoginDescriptorItem = result[0]
+    expected = expected_login_item_data.data_for_login(login_item_title)
+    assert isinstance(result, OPLoginDescriptorItem)
+    assert result.unique_id == expected.unique_id
+    assert result.created_at == expected.created_at

2a3ce72b280ba77ea3048173f6cbc0b8c35725b5
Zachary Cutlip <uid000@gmail.com>
2022-04-30 15:02:30 -0700

    fix type hint for OPItemList

--- pyonepassword/op_items/_item_list.py
+++ pyonepassword/op_items/_item_list.py
@@ -1,12 +1,11 @@
 import json
 from typing import List
 
-from ..op_objects import OPAbstractObject
-
+from ._item_descriptor_base import OPAbstractItemDescriptor
 from ._item_descriptor_registry import OPItemDescriptorFactory
 
 
-class OPItemList(List[OPAbstractObject]):
+class OPItemList(List[OPAbstractItemDescriptor]):
     def __init__(self, item_list_json: str):
         super().__init__()
         item_list = json.loads(item_list_json)

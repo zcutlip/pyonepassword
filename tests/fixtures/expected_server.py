1f70515d29c092ab867ee55e914d53c88e40934b
Zachary Cutlip <uid000@gmail.com>
2022-04-30 15:05:53 -0700

    make ExpectedServer subclass ExpectedItemBase

--- tests/fixtures/expected_server.py
+++ tests/fixtures/expected_server.py
@@ -1,6 +1,6 @@
 from typing import Dict
 
-from .expected_item import ExpectedItemData
+from .expected_item import ExpectedItemBase, ExpectedItemData
 
 
 class ExpectedServerSSHKeys:
@@ -21,23 +21,24 @@ class ExpectedServerSSHKeys:
         return self._data["passphrase"]
 
 
-class ExpectedServer:
+class ExpectedServer(ExpectedItemBase):
     """
-    "Example Server":{
-        "username": "admin",
-        "password": "example_admin_password",
-        "ssh_keys":{
-            "id_ed25519":{
-                "pubkey_digest":"83312cfb5a2d9bb6bb90e89b73c48554df964ef3b29021e66ffa2ffbda649ac4",
-                "privkey_digest":"78670118cc184cf45d0fee82bcac4c3a0d2528dd4aa5b8406ee4a059133efa2a",
-                "passphrase": "example_ssh_key_password"
+    Example Server":{
+            "unique_id": "hqnf5vpzc5mg4x4asrpbvoi54u",
+            "title": "Example Server",
+            "username": "admin",
+            "password": "example_admin_password",
+            "created_at": "2021-06-29T18:31:30Z",
+            "updated_at": "2021-06-29T18:31:30Z",
+            "ssh_keys":{
+                "id_ed25519":{
+                    "pubkey_digest":"83312cfb5a2d9bb6bb90e89b73c48554df964ef3b29021e66ffa2ffbda649ac4",
+                    "privkey_digest":"78670118cc184cf45d0fee82bcac4c3a0d2528dd4aa5b8406ee4a059133efa2a",
+                    "passphrase": "example_ssh_key_password"
+                }
             }
-        }
-    },
+        },
     """
-
-    def __init__(self, server_dict):
-        self._data = server_dict
 
     @property
     def username(self) -> str:

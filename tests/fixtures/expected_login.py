2518c7801db12d2118575d1bccb3c7d0da66a76b
Zachary Cutlip <uid000@gmail.com>
2022-04-30 21:06:25 -0700

    enable ExpectedLogin to parse URL entries

--- tests/fixtures/expected_login.py
+++ tests/fixtures/expected_login.py
@@ -1,7 +1,23 @@
 from .expected_item import ExpectedItemBase, ExpectedItemData
 
 
-class ExampleLogin(ExpectedItemBase):
+class ExpectedLoginURL:
+    def __init__(self, url_dict):
+        self.href = url_dict["href"]
+        self.label = url_dict.get("label")
+        self.primary = url_dict.get("primary", False)
+
+
+class ExpectedLogin(ExpectedItemBase):
+
+    def __init__(self, item_dict):
+        super().__init__(item_dict)
+        url_list = self._data.get("urls", [])
+        urls = []
+        for url_dict in url_list:
+            url = ExpectedLoginURL(url_dict)
+            urls.append(url)
+        self._urls = urls
 
     @property
     def username(self) -> str:
@@ -11,10 +27,23 @@ class ExampleLogin(ExpectedItemBase):
     def password(self) -> str:
         return self._data["password"]
 
+    @property
+    def urls(self):
+        return self._urls
+
+    @property
+    def primary_url(self):
+        primary = None
+        for url in self._urls:
+            if url.primary:
+                primary = url
+                break
+        return primary
+
 
 class ExpectedLoginItemData(ExpectedItemData):
 
     def data_for_login(self, note_identifier):
         item_dict = self._data[note_identifier]
-        login_item = ExampleLogin(item_dict)
+        login_item = ExpectedLogin(item_dict)
         return login_item

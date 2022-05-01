8537effbd10e331a6043148b3b5de142368d30dd
Zachary Cutlip <uid000@gmail.com>
2022-04-30 21:05:39 -0700

    give OPLoginItem parsed URLs

--- pyonepassword/op_items/login.py
+++ pyonepassword/op_items/login.py
@@ -1,3 +1,5 @@
+from typing import List
+
 from ._item_descriptor_base import OPAbstractItemDescriptor
 from ._op_item_type_registry import op_register_item_type
 from ._op_items_base import OPAbstractItem
@@ -12,9 +14,40 @@ class OPLoginDescriptorItem(OPAbstractItemDescriptor):
         super().__init__(item_dict)
 
 
+class OPLoginItemURL(dict):
+    def __init__(self, url_dict):
+        super().__init__(url_dict)
+
+    @property
+    def label(self) -> str:
+        return self.get("label")
+
+    @property
+    def primary(self) -> bool:
+        primary = self.get("primary", False)
+        return primary
+
+    @property
+    def href(self) -> str:
+        return self["href"]
+
+
 @op_register_item_type
 class OPLoginItem(OPAbstractItem):
     CATEGORY = "LOGIN"
+
+    def __init__(self, item_dict_or_json):
+        super().__init__(item_dict_or_json)
+        urls = []
+        primary_url = None
+        url_list = self.get("urls", [])
+        for url_dict in url_list:
+            url = OPLoginItemURL(url_dict)
+            if url.primary:
+                primary_url = url
+            urls.append(url)
+        self._urls = urls
+        self._primary_url = primary_url
 
     @property
     def username(self):
@@ -25,3 +58,11 @@ class OPLoginItem(OPAbstractItem):
     def password(self):
         password = self.field_value_by_id("password")
         return password
+
+    @property
+    def urls(self) -> List[OPLoginItemURL]:
+        return self._urls
+
+    @property
+    def primary_url(self) -> OPLoginItemURL:
+        return self._primary_url

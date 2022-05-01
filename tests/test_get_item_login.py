ad1388c7e7f6f73754f3586c24ecf0e1a664f50d
Zachary Cutlip <uid000@gmail.com>
2022-04-30 21:07:27 -0700

    update tests to use ExpectedLogin fixtures

--- tests/test_get_item_login.py
+++ tests/test_get_item_login.py
@@ -12,33 +12,35 @@ def _lookup_item_data(data: ExpectedData, item_id: str) -> Dict:
     return item
 
 
-def test_get_item_login_01(signed_in_op: OP, expected_data):
+def test_get_item_login_01(signed_in_op: OP, expected_login_item_data):
     # get item "Example Login 1" --vault "Test Data"
     item_name = "Example Login 1"
     vault = "Test Data"
-    expected = _lookup_item_data(expected_data, item_name)
+    expected = expected_login_item_data.data_for_login(item_name)
     result = signed_in_op.item_get(item_name, vault=vault)
-    assert result.username == expected["username"]
-    assert result.password == expected["password"]
+    assert result.username == expected.username
+    assert result.password == expected.password
+    assert result.primary_url.href == expected.primary_url.href
+    assert result.primary_url.label == expected.primary_url.label
 
 
-def test_get_item_login_02(signed_in_op: OP, expected_data):
+def test_get_item_login_02(signed_in_op: OP, expected_login_item_data):
     # get item nok7367v4vbsfgg2fczwu4ei44
     item_uuid = "nok7367v4vbsfgg2fczwu4ei44"
-    expected = _lookup_item_data(expected_data, item_uuid)
+    expected = expected_login_item_data.data_for_login(item_uuid)
     result = signed_in_op.item_get(item_uuid)
-    assert result.username == expected["username"]
-    assert result.password == expected["password"]
+    assert result.username == expected.username
+    assert result.password == expected.password
 
 
-def test_get_item_login_03(signed_in_op: OP, expected_data):
+def test_get_item_login_03(signed_in_op: OP, expected_login_item_data):
     # get item "Example Login" --vault "Test Data 2"
     item_name = "Example Login"
     vault = "Test Data 2"
-    expected = _lookup_item_data(expected_data, item_name)
+    expected = expected_login_item_data.data_for_login(item_name)
     result: OPLoginItem = signed_in_op.item_get(item_name, vault=vault)
-    assert result.username == expected["username"]
-    assert result.password == expected["password"]
+    assert result.username == expected.username
+    assert result.password == expected.password
 
 
 def test_get_invalid_item_login_01(signed_in_op: OP, expected_data):

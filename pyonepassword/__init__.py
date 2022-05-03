8ef711a67abdc6f8fc578afae82da1ecfd076859
Zachary Cutlip <uid000@gmail.com>
2022-05-03 16:29:29 -0700

    import OPTOTPItem in pyonepassword/__init__.py

--- pyonepassword/__init__.py
+++ pyonepassword/__init__.py
@@ -12,6 +12,7 @@ from .op_items.password import OPPasswordItem
 from .op_items.server import OPServerItem
 from .op_items.secure_note import OPSecureNoteItem
 from .op_items.credit_card import OPCreditCardItem
+from .op_items.totp import OPTOTPItem
 from .op_objects import (
     OPUser,
     OPUserDescriptor,

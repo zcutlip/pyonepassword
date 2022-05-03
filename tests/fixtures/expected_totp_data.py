5532e7de79b26be03d4686459ab56ce201455f46
Zachary Cutlip <uid000@gmail.com>
2022-05-03 16:29:53 -0700

    add expected_totp_data fixture module

---
+++ tests/fixtures/expected_totp_data.py
@@ -0,0 +1,45 @@
+from .expected_item import ExpectedItemBase, ExpectedItemData
+
+
+class ExpectedTOTP(ExpectedItemBase):
+    """
+    "Login With TOTP":{
+            "id": "TOTP_obe3whgcenxulnxs5cjyjy763i",
+            "section": {
+                "id": "add more"
+            },
+            "type": "OTP",
+            "label": "one-time password",
+            "value": "AAAAY3DPEHPK3PXP",
+            "totp": "741354",
+            "reference": "op://Test Data/Login With TOTP/add more/one-time password"
+        }
+    """
+
+    @property
+    def unique_id(self) -> str:
+        return self._data["id"]
+
+    @property
+    def label(self) -> str:
+        return self._data["label"]
+
+    @property
+    def value(self) -> str:
+        return self._data["value"]
+
+    @property
+    def totp(self) -> str:
+        return self._data["totp"]
+
+    @property
+    def reference(self) -> str:
+        return self._data["reference"]
+
+
+class ExpectedTOTPData(ExpectedItemData):
+
+    def totp_data_for_login(self, login_identifier):
+        item_dict = self._data[login_identifier]
+        login_item = ExpectedTOTP(item_dict)
+        return login_item

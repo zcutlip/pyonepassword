09d2bcba4cd4444c15b9f49da42ef0d76ccea08d
Zachary Cutlip <uid000@gmail.com>
2022-05-02 20:39:56 -0700

    update example-signin-get-user

--- examples/example-signin-get-user.py
+++ examples/example-signin-get-user.py
@@ -1,6 +1,6 @@
-import getpass
 import os
 import sys
+from do_signin import do_signin
 parent_path = os.path.dirname(
     os.path.dirname(
         os.path.abspath(__file__)
@@ -9,21 +9,7 @@ parent_path = os.path.dirname(
 if parent_path not in sys.path:
     sys.path.append(parent_path)
 
-from pyonepassword import (  # noqa: E402
-    OP,
-    OPGetUserException
-)
-
-
-def do_signin():
-    # If you've already signed in at least once, you don't need to provide all
-    # account details on future sign-ins. Just master password
-    my_password = getpass.getpass(prompt="1Password master password:\n")
-    # You may optionally provide an account shorthand if you used a custom one during initial sign-in
-    # shorthand = "arbitrary_account_shorthand"
-    # return OP(account_shorthand=shorthand, password=my_password)
-    # Or we'll try to look up account shorthand from your latest sign-in in op's config file
-    return OP(password=my_password)
+from pyonepassword import OPGetUserException  # noqa: E402
 
 
 if __name__ == "__main__":
@@ -33,13 +19,13 @@ if __name__ == "__main__":
     print("Signed in.")
     print("Looking up user \"Firstname Lastname\"...")
     try:
-        user_dict = op.get_user("Firstname Lastname")
+        user_dict = op.user_get("Firstname Lastname")
         print(user_dict)
         print("")
         print("Users can also be looked up by their uuid")
         print("")
         print("Looking up uuid \"QBXCWKNZZNGL8I3KSZOH5ERLHI\"...")
-        user_dict_2 = op.get_user("QBXCWKNZZNGL8I3KSZOH5ERLHI")
+        user_dict_2 = op.user_get("QBXCWKNZZNGL8I3KSZOH5ERLHI")
         print("User dictionaries match? {}".format(user_dict == user_dict_2))
     except OPGetUserException as ope:
         print("1Password lookup failed: {}".format(ope))

484bcb950007d55aef5744ee05b069e550cbe19c
Zachary Cutlip <uid000@gmail.com>
2022-05-05 14:34:35 -0700

    test modules for "item get" errors

---
+++ tests/test_item_errors.py
@@ -0,0 +1,35 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+import pytest
+from pyonepassword.op_items import (
+    OPFieldNotFoundException,
+    OPItemFactory,
+    OPUnknownItemType
+)
+from pyonepassword.py_op_exceptions import OPInvalidItemException
+
+if TYPE_CHECKING:
+    from pyonepassword import OP, OPLoginItem
+
+
+def test_unknown_item_type_01(invalid_data):
+    invalid_item_json = invalid_data.data_for_name("invalid-item")
+    with pytest.raises(OPUnknownItemType):
+        _ = OPItemFactory.op_item(invalid_item_json)
+
+
+def test_malformed_item_json_01(invalid_data):
+    malformed_json = invalid_data.data_for_name("malformed-item-json")
+    with pytest.raises(OPInvalidItemException):
+        _ = OPItemFactory.op_item(malformed_json)
+
+
+def test_item_field_not_found_01(signed_in_op: OP):
+    item_name = "Example Login 1"
+    vault = "Test Data"
+    result: OPLoginItem
+    result = signed_in_op.item_get(item_name, vault=vault)
+    with pytest.raises(OPFieldNotFoundException):
+        result.field_by_id("Non-existent-field")

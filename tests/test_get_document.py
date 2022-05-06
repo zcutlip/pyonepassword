efc579501b156e9e865c974cc380dfd2527aadc1
Zachary Cutlip <uid000@gmail.com>
2022-05-05 20:07:03 -0700

    add a method to test OPDocumentFile objects

--- tests/test_get_document.py
+++ tests/test_get_document.py
@@ -1,12 +1,14 @@
 from __future__ import annotations
+
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
     from pyonepassword import OP
 
 import pytest
+from pyonepassword import OPDocumentItem, OPGetDocumentException
+from pyonepassword.op_items.document import OPDocumentFile
 
-from pyonepassword import OPGetDocumentException
 from .test_support.util import digest
 
 
@@ -33,3 +35,18 @@ def test_get_document_02(signed_in_op: OP, expected_document_data, invalid_docum
         assert False, f"We should have caught {exception_class.__name__}"
     except exception_class as e:
         assert expected.returncode == e.returncode
+
+
+def test_get_document_03(signed_in_op: OP, expected_document_data):
+    item_name = "Example Login 2 - 1200px-SpongeBob_SquarePants_character.svg.png.webp"
+    vault = "Test Data"
+    expected: OPDocumentItem = expected_document_data.data_for_document(
+        item_name)
+    document_item = signed_in_op.item_get(item_name, vault=vault)
+    assert isinstance(document_item, OPDocumentItem)
+    document_files = document_item.files
+    file_obj = document_files[0]
+    assert isinstance(file_obj, OPDocumentFile)
+    assert file_obj.size == expected.size
+    assert file_obj.content_path == expected.content_path
+    assert file_obj.file_id == expected.file_id

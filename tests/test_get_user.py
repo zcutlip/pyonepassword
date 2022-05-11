5d24c06c83d132dff6b1d4b452fe651d084e2f1e
Zachary Cutlip <uid000@gmail.com>
2022-05-10 19:13:52 -0700

    split "user get" tests out into individual cases

--- tests/test_get_user.py
+++ tests/test_get_user.py
@@ -20,12 +20,52 @@ def test_get_user_01(signed_in_op: OP, expected_user_data):
     user_identifier = "Example User"
     expected = _lookup_user_data(expected_user_data, user_identifier)
     result = signed_in_op.user_get(user_identifier)
-
+    assert isinstance(result, OPUser)
     assert result.unique_id == expected.unique_id
+
+
+def test_get_user_02(signed_in_op: OP, expected_user_data):
+    # get vault "Test Data"
+    user_identifier = "Example User"
+    expected = _lookup_user_data(expected_user_data, user_identifier)
+    result = signed_in_op.user_get(user_identifier)
+    assert isinstance(result, OPUser)
     assert result.name == expected.name
+
+
+def test_get_user_03(signed_in_op: OP, expected_user_data):
+    # get vault "Test Data"
+    user_identifier = "Example User"
+    expected = _lookup_user_data(expected_user_data, user_identifier)
+    result = signed_in_op.user_get(user_identifier)
+    assert isinstance(result, OPUser)
     assert result.email == expected.email
+
+
+def test_get_user_04(signed_in_op: OP, expected_user_data):
+    # get vault "Test Data"
+    user_identifier = "Example User"
+    expected = _lookup_user_data(expected_user_data, user_identifier)
+    result = signed_in_op.user_get(user_identifier)
+    assert isinstance(result, OPUser)
     assert result.updated_at == expected.updated_at
+
+
+def test_get_user_05(signed_in_op: OP, expected_user_data):
+    # get vault "Test Data"
+    user_identifier = "Example User"
+    expected = _lookup_user_data(expected_user_data, user_identifier)
+    result = signed_in_op.user_get(user_identifier)
+    assert isinstance(result, OPUser)
     assert result.created_at == expected.created_at
+
+
+def test_get_user_06(signed_in_op: OP, expected_user_data):
+    # get vault "Test Data"
+    user_identifier = "Example User"
+    expected = _lookup_user_data(expected_user_data, user_identifier)
+    result = signed_in_op.user_get(user_identifier)
+    assert isinstance(result, OPUser)
     assert result.last_auth_at == expected.last_auth_at
 
 

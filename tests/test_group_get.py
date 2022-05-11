fc5d500b9dfb95f476f97b6406cbb1a6d5f05958
Zachary Cutlip <uid000@gmail.com>
2022-05-10 19:33:48 -0700

    eliminate _lookup_group_data() method

--- tests/test_group_get.py
+++ tests/test_group_get.py
@@ -10,14 +10,9 @@ if TYPE_CHECKING:
 from pyonepassword import OPGetGroupException, OPGroup, OPInvalidGroupException
 
 
-def _lookup_group_data(data, group_identifier: str):
-    group = data.data_for_group(group_identifier)
-    return group
-
-
 def test_group_get_01(signed_in_op: OP, expected_group_data):
     group_identifier = "Team Members"
-    expected = _lookup_group_data(expected_group_data, group_identifier)
+    expected = expected_group_data.data_for_group(group_identifier)
     result = signed_in_op.group_get(group_identifier)
     assert isinstance(result, OPGroup)
     assert result.unique_id == expected.unique_id
@@ -25,7 +20,7 @@ def test_group_get_01(signed_in_op: OP, expected_group_data):
 
 def test_group_get_02(signed_in_op: OP, expected_group_data):
     group_identifier = "Team Members"
-    expected = _lookup_group_data(expected_group_data, group_identifier)
+    expected = expected_group_data.data_for_group(group_identifier)
     result = signed_in_op.group_get(group_identifier)
     assert isinstance(result, OPGroup)
     assert result.name == expected.name
@@ -33,7 +28,7 @@ def test_group_get_02(signed_in_op: OP, expected_group_data):
 
 def test_group_get_03(signed_in_op: OP, expected_group_data):
     group_identifier = "Team Members"
-    expected = _lookup_group_data(expected_group_data, group_identifier)
+    expected = expected_group_data.data_for_group(group_identifier)
     result = signed_in_op.group_get(group_identifier)
     assert isinstance(result, OPGroup)
     assert result.description == expected.description
@@ -41,7 +36,7 @@ def test_group_get_03(signed_in_op: OP, expected_group_data):
 
 def test_group_get_04(signed_in_op: OP, expected_group_data):
     group_identifier = "Team Members"
-    expected = _lookup_group_data(expected_group_data, group_identifier)
+    expected = expected_group_data.data_for_group(group_identifier)
     result = signed_in_op.group_get(group_identifier)
     assert isinstance(result, OPGroup)
     assert result.updated_at == expected.updated_at
@@ -49,7 +44,7 @@ def test_group_get_04(signed_in_op: OP, expected_group_data):
 
 def test_group_get_05(signed_in_op: OP, expected_group_data):
     group_identifier = "Team Members"
-    expected = _lookup_group_data(expected_group_data, group_identifier)
+    expected = expected_group_data.data_for_group(group_identifier)
     result = signed_in_op.group_get(group_identifier)
     assert isinstance(result, OPGroup)
     assert result.created_at == expected.created_at
@@ -57,7 +52,7 @@ def test_group_get_05(signed_in_op: OP, expected_group_data):
 
 def test_get_invalid_user_01(signed_in_op: OP, expected_group_data):
     group_identifier = "No Such group"
-    expected = _lookup_group_data(expected_group_data, group_identifier)
+    expected = expected_group_data.data_for_group(group_identifier)
     try:
         signed_in_op.group_get(group_identifier)
         assert False, "Should have caught an OPGetGroupException"

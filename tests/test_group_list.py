ee10ba086272451c83a780c26b958abed6a6cca2
Zachary Cutlip <uid000@gmail.com>
2022-05-10 18:51:38 -0700

    add test case for malformed group list

--- tests/test_group_list.py
+++ tests/test_group_list.py
@@ -2,7 +2,11 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING, List
 
-from pyonepassword.op_objects import OPGroupDescriptor
+import pytest
+from pyonepassword.op_objects import (
+    OPGroupDescriptor,
+    OPInvalidGroupListException
+)
 
 # make imports for type-hinting disappear at run-time to avoid
 # circular imports.
@@ -10,9 +14,12 @@ from pyonepassword.op_objects import OPGroupDescriptor
 if TYPE_CHECKING:
     from pyonepassword import OP
 
-from .fixtures.expected_group_data import ExpectedGroupListEntry, ExpectedGroupListData
-
 from pyonepassword import OPGroupDescriptorList
+
+from .fixtures.expected_group_data import (
+    ExpectedGroupListData,
+    ExpectedGroupListEntry
+)
 
 
 def test_group_list_01(signed_in_op: OP, expected_group_list_data: ExpectedGroupListData):
@@ -26,6 +33,48 @@ def test_group_list_01(signed_in_op: OP, expected_group_list_data: ExpectedGroup
     assert isinstance(group_entry, OPGroupDescriptor)
 
     assert group_entry.unique_id == expected.unique_id
+
+
+def test_group_list_02(signed_in_op: OP, expected_group_list_data: ExpectedGroupListData):
+    group_list_key = "all-groups"
+    expected_group_list: List[ExpectedGroupListEntry]
+    expected_group_list = expected_group_list_data.data_for_key(group_list_key)
+    expected = expected_group_list[0]
+    result = signed_in_op.group_list()
+    group_entry = result[0]
+    assert isinstance(result, OPGroupDescriptorList)
+    assert isinstance(group_entry, OPGroupDescriptor)
+
     assert group_entry.description == expected.description
+
+
+def test_group_list_03(signed_in_op: OP, expected_group_list_data: ExpectedGroupListData):
+    group_list_key = "all-groups"
+    expected_group_list: List[ExpectedGroupListEntry]
+    expected_group_list = expected_group_list_data.data_for_key(group_list_key)
+    expected = expected_group_list[0]
+    result = signed_in_op.group_list()
+    group_entry = result[0]
+    assert isinstance(result, OPGroupDescriptorList)
+    assert isinstance(group_entry, OPGroupDescriptor)
+
     assert group_entry.state == expected.state
+
+
+def test_group_list_04(signed_in_op: OP, expected_group_list_data: ExpectedGroupListData):
+    group_list_key = "all-groups"
+    expected_group_list: List[ExpectedGroupListEntry]
+    expected_group_list = expected_group_list_data.data_for_key(group_list_key)
+    expected = expected_group_list[0]
+    result = signed_in_op.group_list()
+    group_entry = result[0]
+    assert isinstance(result, OPGroupDescriptorList)
+    assert isinstance(group_entry, OPGroupDescriptor)
+
     assert group_entry.created_at == expected.created_at
+
+
+def test_group_list_malformed_json_01(invalid_data):
+    malformed_json = invalid_data.data_for_name("malformed-group-list-json")
+    with pytest.raises(OPInvalidGroupListException):
+        OPGroupDescriptorList(malformed_json)

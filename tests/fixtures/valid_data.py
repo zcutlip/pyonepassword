855e916dbdac70905f92c7e2ada0ba08ac593251
Zachary Cutlip <uid000@gmail.com>
2022-05-12 16:14:27 -0700

    added ValidData support class

---
+++ tests/fixtures/valid_data.py
@@ -0,0 +1,26 @@
+import json
+from pathlib import Path
+
+from .paths import VALID_DATA_PATH, VALID_DATA_REGISTRY_PATH
+
+
+class ValidData:
+
+    def __init__(self):
+        registry = json.load(open(VALID_DATA_REGISTRY_PATH, "r"))
+        self._registry = registry
+
+    def data_for_name(self, entry_name):
+        entry = self._registry[entry_name]
+        item_filename = entry["name"]
+        item_type = entry["type"]
+        item_path = Path(VALID_DATA_PATH, item_filename)
+
+        if item_type in ["text", "json"]:
+            mode = "r"
+        else:
+            mode = "rb"
+        data = open(item_path, mode).read()
+        if item_type == "json":
+            data = json.loads(data)
+        return data

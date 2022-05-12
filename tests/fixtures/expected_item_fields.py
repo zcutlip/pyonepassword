128bd92955a5669088b1d9d1da863b55a16ecf38
Zachary Cutlip <uid000@gmail.com>
2022-05-12 16:16:27 -0700

    add support classes and fixture for expected item fields

---
+++ tests/fixtures/expected_item_fields.py
@@ -0,0 +1,46 @@
+from typing import Dict, Union
+
+from .expected_data import ExpectedData
+
+
+class ExpectedItemField:
+
+    def __init__(self, item_field: Dict):
+        self._data = item_field
+
+    @property
+    def field_id(self) -> str:
+        return self._data["field_id"]
+
+    @property
+    def type(self) -> str:
+        return self._data["type"]
+
+    @property
+    def purpose(self) -> str:
+        return self._data["purpose"]
+
+    @property
+    def label(self) -> str:
+        return self._data["label"]
+
+    @property
+    def value(self) -> Union[str, Dict]:
+        return self._data["value"]
+
+    @property
+    def reference(self) -> str:
+        return self._data["reference"]
+
+
+class ExpectedItemFieldData:
+
+    def __init__(self) -> None:
+        expected_data = ExpectedData()
+        field_data: Dict = expected_data.item_fields
+        self._data: Dict = field_data
+
+    def data_for_key(self, key):
+        data = self._data[key]
+        data = ExpectedItemField(data)
+        return data
--- tests/fixtures/op_fixtures.py
+++ tests/fixtures/op_fixtures.py
@@ -11,6 +11,7 @@ from .expected_credit_card import ExpectedCreditCardData
 from .expected_data import ExpectedData
 from .expected_document_data import ExpectedDocumentData
 from .expected_group_data import ExpectedGroupData, ExpectedGroupListData
+from .expected_item_fields import ExpectedItemFieldData
 from .expected_login import ExpectedLoginItemData
 from .expected_password_item_data import ExpectedPasswordItemData
 from .expected_secure_note_item_data import ExpectedSecureNoteItemData
@@ -140,6 +141,12 @@ def expected_totp_data():
 @fixture
 def expected_api_credential_data():
     data = ExpectedAPICredentialData()
+    return data
+
+
+@fixture
+def expected_item_field_data():
+    data = ExpectedItemFieldData()
     return data
 
 

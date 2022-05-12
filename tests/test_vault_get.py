67efe2d27493626ce81f2cb2f9cb1b6ef0eb3c01
Zachary Cutlip <uid000@gmail.com>
2022-05-12 11:29:39 -0700

    verify "vault get" results are actually OPVault objects

--- tests/test_vault_get.py
+++ tests/test_vault_get.py
@@ -4,7 +4,7 @@ from typing import Dict, TYPE_CHECKING
 if TYPE_CHECKING:
     from pyonepassword import OP
 
-from pyonepassword import OPGetVaultException
+from pyonepassword import OPGetVaultException, OPVault
 
 
 def _lookup_item_data(data, vault_id: str) -> Dict:
@@ -17,6 +17,7 @@ def test_vault_get_01(signed_in_op: OP, expected_vault_data):
     vault_name = "Test Data"
     expected = _lookup_item_data(expected_vault_data, vault_name)
     result = signed_in_op.vault_get(vault_name)
+    assert isinstance(result, OPVault)
     assert result.unique_id == expected.unique_id
     assert result.description == expected.description
 
@@ -26,6 +27,7 @@ def test_vault_get_02(signed_in_op: OP, expected_vault_data):
     vault_uuid = "yv7w5ijyxbdhxgh3njphwsxujy"
     expected = _lookup_item_data(expected_vault_data, vault_uuid)
     result = signed_in_op.vault_get(vault_uuid)
+    assert isinstance(result, OPVault)
     assert result.name == expected.name
     assert result.description == expected.description
 

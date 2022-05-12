46f7f48f65753314cb5d2901cc6853e8b77bed2c
Zachary Cutlip <uid000@gmail.com>
2022-05-12 11:07:34 -0700

    sort imports

--- pyonepassword/__init__.py
+++ pyonepassword/__init__.py
@@ -1,50 +1,44 @@
-from .__about__ import (
-    __version__,
-    __title__,
-    __summary__
-)
+from .__about__ import __summary__, __title__, __version__
 from .op_items._item_list import OPItemList
 from .op_items.api_credential import OPAPICredentialItem
-from .op_items.login import OPLoginItem
-from .op_items.identity import OPIdentityItemDescriptor
-from .op_items.document import OPDocumentItem
-from .op_items.password import OPPasswordItem
-from .op_items.server import OPServerItem
-from .op_items.secure_note import OPSecureNoteItem
 from .op_items.credit_card import OPCreditCardItem
+from .op_items.document import OPDocumentItem
+from .op_items.identity import OPIdentityItemDescriptor
+from .op_items.login import OPLoginItem
+from .op_items.password import OPPasswordItem
+from .op_items.secure_note import OPSecureNoteItem
+from .op_items.server import OPServerItem
 from .op_items.totp import OPTOTPItem
 from .op_objects import (
-    OPUser,
-    OPUserDescriptor,
-    OPUserDescriptorList,
-    OPInvalidObjectException,
-    OPInvalidUserException,
-    OPInvalidUserListException,
     OPGroup,
     OPGroupDescriptor,
     OPGroupDescriptorList,
     OPInvalidGroupException,
     OPInvalidGroupListException,
+    OPInvalidObjectException,
+    OPInvalidUserException,
+    OPInvalidUserListException,
+    OPUser,
+    OPUserDescriptor,
+    OPUserDescriptorList,
     OPVault,
     OPVaultDescriptor,
     OPVaultDescriptorList
 )
-
-from .pyonepassword import OP
-
 from .py_op_exceptions import (
-    OPSigninException,
-    OPNotFoundException,
-    OPGetItemException,
-    OPGetDocumentException,
-    OPGetUserException,
-    OPGetGroupException,
-    OPGetVaultException,
-    OPListEventsException,
-    OPInvalidDocumentException,
-    OPSignoutException,
-    OPForgetException,
     OPConfigNotFoundException,
     OPCreateItemException,
-    OPNotSignedInException
+    OPForgetException,
+    OPGetDocumentException,
+    OPGetGroupException,
+    OPGetItemException,
+    OPGetUserException,
+    OPGetVaultException,
+    OPInvalidDocumentException,
+    OPListEventsException,
+    OPNotFoundException,
+    OPNotSignedInException,
+    OPSigninException,
+    OPSignoutException
 )
+from .pyonepassword import OP

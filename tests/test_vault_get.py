1bf2cd4fd7a972ba97f2ecdb2f1e4b4473871a00
Zachary Cutlip <uid000@gmail.com>
2022-05-12 12:11:05 -0700

    test each OPVault property

--- tests/test_vault_get.py
+++ tests/test_vault_get.py
@@ -1,40 +1,172 @@
 from __future__ import annotations
-from typing import Dict, TYPE_CHECKING
+
+from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
     from pyonepassword import OP
+    from .fixtures.expected_vault_data import (
+        ExpectedVaultData,
+        ExpectedVault
+    )
 
 from pyonepassword import OPGetVaultException, OPVault
 
 
-def _lookup_item_data(data, vault_id: str) -> Dict:
-    item = data.data_for_vault(vault_id)
-    return item
-
-
-def test_vault_get_01(signed_in_op: OP, expected_vault_data):
+def test_vault_get_01(signed_in_op: OP, expected_vault_data: ExpectedVaultData):
     # get vault "Test Data"
+    expected: ExpectedVault
+    result: OPVault
+
     vault_name = "Test Data"
-    expected = _lookup_item_data(expected_vault_data, vault_name)
+    expected = expected_vault_data.data_for_vault(vault_name)
     result = signed_in_op.vault_get(vault_name)
+
     assert isinstance(result, OPVault)
     assert result.unique_id == expected.unique_id
+
+
+def test_vault_get_02(signed_in_op: OP, expected_vault_data: ExpectedVaultData):
+    # get vault "Test Data"
+    expected: ExpectedVault
+    result: OPVault
+
+    vault_name = "Test Data"
+    expected = expected_vault_data.data_for_vault(vault_name)
+    result = signed_in_op.vault_get(vault_name)
+    assert isinstance(result, OPVault)
     assert result.description == expected.description
 
 
-def test_vault_get_02(signed_in_op: OP, expected_vault_data):
-    # get vault "yv7w5ijyxbdhxgh3njphwsxujy"
+def test_vault_get_03(signed_in_op: OP, expected_vault_data: ExpectedVaultData):
+    # get vault "Test Data"
+    expected: ExpectedVault
+    result: OPVault
+
+    vault_name = "Test Data"
+    expected = expected_vault_data.data_for_vault(vault_name)
+    result = signed_in_op.vault_get(vault_name)
+    assert isinstance(result, OPVault)
+    assert result.items > 0
+    assert result.items == expected.items
+
+
+def test_vault_get_04(signed_in_op: OP, expected_vault_data: ExpectedVaultData):
+    # get vault "Test Data"
+    expected: ExpectedVault
+    result: OPVault
+
+    vault_name = "Test Data"
+    expected = expected_vault_data.data_for_vault(vault_name)
+    result = signed_in_op.vault_get(vault_name)
+    assert isinstance(result, OPVault)
+    assert result.type == expected.type
+
+
+def test_vault_get_05(signed_in_op: OP, expected_vault_data: ExpectedVaultData):
+    # get vault "Test Data"
+    expected: ExpectedVault
+    result: OPVault
+
+    vault_name = "Test Data"
+    expected = expected_vault_data.data_for_vault(vault_name)
+    result = signed_in_op.vault_get(vault_name)
+    assert isinstance(result, OPVault)
+    assert result.created_at == expected.created_at
+
+
+def test_vault_get_06(signed_in_op: OP, expected_vault_data: ExpectedVaultData):
+    # get vault "Test Data"
+    expected: ExpectedVault
+    result: OPVault
+
+    vault_name = "Test Data"
+    expected = expected_vault_data.data_for_vault(vault_name)
+    result = signed_in_op.vault_get(vault_name)
+    assert isinstance(result, OPVault)
+    assert result.updated_at == expected.updated_at
+
+
+def test_vault_get_07(signed_in_op: OP, expected_vault_data: ExpectedVaultData):
+    expected: ExpectedVault
+    result: OPVault
+
     vault_uuid = "yv7w5ijyxbdhxgh3njphwsxujy"
-    expected = _lookup_item_data(expected_vault_data, vault_uuid)
+    expected = expected_vault_data.data_for_vault(vault_uuid)
+    result = signed_in_op.vault_get(vault_uuid)
+    assert isinstance(result, OPVault)
+    assert result.unique_id == expected.unique_id
+
+
+def test_vault_get_08(signed_in_op: OP, expected_vault_data: ExpectedVaultData):
+    expected: ExpectedVault
+    result: OPVault
+
+    vault_uuid = "yv7w5ijyxbdhxgh3njphwsxujy"
+    expected = expected_vault_data.data_for_vault(vault_uuid)
     result = signed_in_op.vault_get(vault_uuid)
     assert isinstance(result, OPVault)
     assert result.name == expected.name
+
+
+def test_vault_get_09(signed_in_op: OP, expected_vault_data: ExpectedVaultData):
+    expected: ExpectedVault
+    result: OPVault
+
+    vault_uuid = "yv7w5ijyxbdhxgh3njphwsxujy"
+    expected = expected_vault_data.data_for_vault(vault_uuid)
+    result = signed_in_op.vault_get(vault_uuid)
+    assert isinstance(result, OPVault)
     assert result.description == expected.description
+
+
+def test_vault_get_10(signed_in_op: OP, expected_vault_data: ExpectedVaultData):
+    expected: ExpectedVault
+    result: OPVault
+
+    vault_uuid = "yv7w5ijyxbdhxgh3njphwsxujy"
+    expected = expected_vault_data.data_for_vault(vault_uuid)
+    result = signed_in_op.vault_get(vault_uuid)
+    assert isinstance(result, OPVault)
+    assert result.items > 0
+    assert result.items == expected.items
+
+
+def test_vault_get_11(signed_in_op: OP, expected_vault_data: ExpectedVaultData):
+    expected: ExpectedVault
+    result: OPVault
+
+    vault_uuid = "yv7w5ijyxbdhxgh3njphwsxujy"
+    expected = expected_vault_data.data_for_vault(vault_uuid)
+    result = signed_in_op.vault_get(vault_uuid)
+    assert isinstance(result, OPVault)
+    assert result.type == expected.type
+
+
+def test_vault_get_12(signed_in_op: OP, expected_vault_data: ExpectedVaultData):
+    expected: ExpectedVault
+    result: OPVault
+
+    vault_uuid = "yv7w5ijyxbdhxgh3njphwsxujy"
+    expected = expected_vault_data.data_for_vault(vault_uuid)
+    result = signed_in_op.vault_get(vault_uuid)
+    assert isinstance(result, OPVault)
+    assert result.created_at == expected.created_at
+
+
+def test_vault_get_13(signed_in_op: OP, expected_vault_data: ExpectedVaultData):
+    expected: ExpectedVault
+    result: OPVault
+
+    vault_uuid = "yv7w5ijyxbdhxgh3njphwsxujy"
+    expected = expected_vault_data.data_for_vault(vault_uuid)
+    result = signed_in_op.vault_get(vault_uuid)
+    assert isinstance(result, OPVault)
+    assert result.updated_at == expected.updated_at
 
 
 def test_vault_get_invalid_01(signed_in_op: OP, expected_vault_data):
     vault_name = "Invalid Vault"
-    expected = _lookup_item_data(expected_vault_data, vault_name)
+    expected = expected_vault_data.data_for_vault(vault_name)
     try:
         _ = signed_in_op.vault_get(vault_name)
         assert False, "We should have caught an exception"

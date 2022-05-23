d217dcf0b56c0ae61f8edff0ed0a0ce9fb83ff68
Zachary Cutlip <uid000@gmail.com>
2022-05-23 16:17:32 -0700

    explicity run /test.sh and pass additional arguments to it

--- docker_testing/run.sh
+++ docker_testing/run.sh
@@ -2,8 +2,8 @@
 
 ret=0
 
-docker run   --rm -it -v "$(pwd):/usr/src/testdir" docker_py38
+docker run   --rm -it -v "$(pwd):/usr/src/testdir" docker_py38 /test.sh "$@"
 ret=`expr $? + $ret`
-docker run   --rm -it -v "$(pwd):/usr/src/testdir" docker_py39
+docker run   --rm -it -v "$(pwd):/usr/src/testdir" docker_py39 /test.sh "$@"
 ret=`expr $? + $ret`
 exit $ret

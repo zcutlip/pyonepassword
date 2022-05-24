f0c33aaf401fe94bda25c3bc8ef53f1e192ed926
Zachary Cutlip <uid000@gmail.com>
2022-05-23 18:44:42 -0700

    add tests to list vault for specific user

--- tests/test_vault_list.py
+++ tests/test_vault_list.py
@@ -73,6 +73,56 @@ def test_vault_list_all_vaults_03(signed_in_op: OP, expected_vault_list_data: Ex
     assert vault_entry.name == expected.name
 
 
+def test_vault_list_example_user_01(signed_in_op: OP, expected_vault_list_data: ExpectedVaultListData):
+    expected: List[ExpectedVaultListEntry]
+    result: OPVaultDescriptorList
+
+    expected = expected_vault_list_data.data_for_key("user-example-user")
+    result = signed_in_op.vault_list(
+        user_name_or_id="example_user@example.email")
+
+    _sanity_check_vault_list(result, expected)
+
+    result_ids = {vault_obj.unique_id for vault_obj in result}
+    expected_ids = {entry.unique_id for entry in expected}
+    assert result_ids == expected_ids
+
+
+def test_vault_list_example_user_02(signed_in_op: OP, expected_vault_list_data: ExpectedVaultListData):
+    epected_vault_list: List[ExpectedVaultListEntry]
+    expected: ExpectedVaultListEntry
+
+    expected_vault_list = expected_vault_list_data.data_for_key(
+        "user-example-user")
+    expected = expected_vault_list[0]
+    result = signed_in_op.vault_list(
+        user_name_or_id="example_user@example.email")
+    _sanity_check_vault_list(result, expected_vault_list)
+
+    vault_entry = result[0]
+
+    assert isinstance(vault_entry, OPVaultDescriptor)
+    assert vault_entry.unique_id == expected.unique_id
+
+
+def test_vault_list_example_user_03(signed_in_op: OP, expected_vault_list_data: ExpectedVaultListData):
+    epected_vault_list: List[ExpectedVaultListEntry]
+    expected: ExpectedVaultListEntry
+
+    expected_vault_list = expected_vault_list_data.data_for_key(
+        "user-example-user")
+    expected = expected_vault_list[2]
+    result = signed_in_op.vault_list(
+        user_name_or_id="example_user@example.email")
+
+    _sanity_check_vault_list(result, expected_vault_list)
+
+    vault_entry = result[2]
+
+    assert isinstance(vault_entry, OPVaultDescriptor)
+    assert vault_entry.name == expected.name
+
+
 def test_vault_list_malformed_json_01(invalid_data):
     malformed_json = invalid_data.data_for_name("malformed-vault-list-json")
 

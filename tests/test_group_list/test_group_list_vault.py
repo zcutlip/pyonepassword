da6508c1aea6084b77ff13f2e1959e88cf05e74d
Zachary Cutlip <uid000@gmail.com>
2022-05-23 22:08:06 -0700

    actually query group list for specific vault

--- tests/test_group_list/test_group_list_vault.py
+++ tests/test_group_list/test_group_list_vault.py
@@ -21,13 +21,15 @@ from pyonepassword import OPGroupDescriptorList
 # ensure HOME env variable is set, and there's a valid op config present
 pytestmark = pytest.mark.usefixtures("valid_op_cli_config_homedir")
 
+TEST_DATA_VAULT = "Test Data"
+
 
 def test_group_list_test_data_01(signed_in_op: OP, expected_group_list_data: ExpectedGroupListData):
     group_list_key = "vault-test-data"
     expected_group_list: List[ExpectedGroupListEntry]
     expected_group_list = expected_group_list_data.data_for_key(group_list_key)
     expected = expected_group_list[0]
-    result = signed_in_op.group_list()
+    result = signed_in_op.group_list(vault=TEST_DATA_VAULT)
     group_entry = result[0]
     assert isinstance(result, OPGroupDescriptorList)
     assert isinstance(group_entry, OPGroupDescriptor)
@@ -40,7 +42,7 @@ def test_group_list_test_data_02(signed_in_op: OP, expected_group_list_data: Exp
     expected_group_list: List[ExpectedGroupListEntry]
     expected_group_list = expected_group_list_data.data_for_key(group_list_key)
     expected = expected_group_list[1]
-    result = signed_in_op.group_list()
+    result = signed_in_op.group_list(vault=TEST_DATA_VAULT)
     group_entry = result[1]
     assert isinstance(result, OPGroupDescriptorList)
     assert isinstance(group_entry, OPGroupDescriptor)
@@ -53,7 +55,7 @@ def test_group_list_test_data_03(signed_in_op: OP, expected_group_list_data: Exp
     expected_group_list: List[ExpectedGroupListEntry]
     expected_group_list = expected_group_list_data.data_for_key(group_list_key)
     expected = expected_group_list[2]
-    result = signed_in_op.group_list()
+    result = signed_in_op.group_list(vault=TEST_DATA_VAULT)
     group_entry = result[2]
     assert isinstance(result, OPGroupDescriptorList)
     assert isinstance(group_entry, OPGroupDescriptor)
@@ -66,7 +68,7 @@ def test_group_list_test_data_04(signed_in_op: OP, expected_group_list_data: Exp
     expected_group_list: List[ExpectedGroupListEntry]
     expected_group_list = expected_group_list_data.data_for_key(group_list_key)
     expected = expected_group_list[2]
-    result = signed_in_op.group_list()
+    result = signed_in_op.group_list(vault=TEST_DATA_VAULT)
     group_entry = result[2]
     assert isinstance(result, OPGroupDescriptorList)
     assert isinstance(group_entry, OPGroupDescriptor)

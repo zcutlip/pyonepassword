c7bf79d71a025e4a0e5dce4f280f9c8fa679ffe7
Zachary Cutlip <uid000@gmail.com>
2022-05-26 12:04:52 -0700

    make fromisoformat_z() a little more robust

--- pyonepassword/_datetime.py
+++ pyonepassword/_datetime.py
@@ -8,11 +8,17 @@ def fromisoformat_z(date_string: str) -> datetime:
     And there's no way to do it without an external package like dateutil
     https://bugs.python.org/issue35829
     """
-    if not date_string.endswith('Z'):
+
+    if date_string.endswith('Z'):
+        _str = date_string.rstrip('Z')
+        _str += "+00:00"
+    elif date_string.endswith("+00:00"):
+        # We shouldn't ever get this, but we also shouldn't blow up on it,
+        # so work with it if it happens
+        _str = date_string
+    else:
         # Mirror the ValueError that datetime.fromisoformat() raises
         raise ValueError(
             f"Invalid Z-terminated isoformat string: '{date_string}'")
-    _str = date_string.rstrip('Z')
-    _str += "+00:00"
     datetime_obj = datetime.fromisoformat(_str)
     return datetime_obj

49eade6ee4b8fa7dda076d2f7a3f9245a563068d
Zachary Cutlip <uid000@gmail.com>
2022-05-25 18:10:12 -0700

    add test case for op signin failure

--- tests/test_op_cli.py
+++ tests/test_op_cli.py
@@ -1,8 +1,20 @@
+import os
+
 import pytest
 
-from pyonepassword import OP, OPNotFoundException
+from pyonepassword import OP, OPNotFoundException, OPSigninException
+
+from .fixtures.paths import RESP_DIRECTORY_PATH
 
 
 def test_missing_op():
     with pytest.raises(OPNotFoundException):
         OP(op_path="no-such-op")
+
+
+def test_signin_fail():
+    os.environ["MOCK_OP_RESPONSE_DIRECTORY"] = str(RESP_DIRECTORY_PATH)
+    os.environ["LOG_OP_ERR"] = "1"
+    os.environ["MOCK_OP_SIGNIN_SUCCEED"] = "0"
+    with pytest.raises(OPSigninException):
+        OP(op_path="mock-op")

f88dbf2fb034e5808605da97ed11dd507eb70506
Zachary Cutlip <uid000@gmail.com>
2022-06-04 14:26:16 -0400

    add test cases for:
    - biometric disabled, no op session variable
    - biometric disabled, no "latest sign-in"

--- tests/test_misc.py
+++ tests/test_misc.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 import pytest
 
-from pyonepassword import OP
+from pyonepassword import OP, OPNotSignedInException
 from pyonepassword._op_cli_config import OPCLIConfig
 
 # ensure HOME env variable is set, and there's a valid op config present
@@ -53,3 +53,28 @@ def test_session_var_property_01(expected_misc_data):
     expected_var_name = expected_misc_data.data_for_key("op-session-var")
     op = OP(op_path='mock-op', account_shorthand="example_shorthand")
     assert op.session_var == expected_var_name
+
+
+@pytest.mark.usefixtures("valid_op_cli_config_homedir")
+@pytest.mark.usefixtures("setup_alt_op_env")
+def test_use_existing_session_01():
+    """
+    Simulate an pyonepassword environment that doesn't use biometric auth, and doesn't have
+    OP_SESSION_<session ID> env variable set
+    check that OP() fails with OPNotSignedIn
+    """
+    with pytest.raises(OPNotSignedInException):
+        OP(op_path='mock-op', use_existing_session=True, password_prompt=False)
+
+
+@pytest.mark.usefixtures("valid_op_cli_config_no_shorthand")
+@pytest.mark.usefixtures("setup_alt_op_env")
+def test_no_shorthand_no_bio_01():
+    """
+    If biometric is disabled, a shorthand is required, so:
+    - Simulate an pyonepassword environment that doesn't use biometric auth
+    - Provide a config that doesn't have a "latest sign-in", so no shorthand
+    - Check that OP() fails with OPNotSignedInException
+    """
+    with pytest.raises(OPNotSignedInException):
+        OP(op_path='mock-op')

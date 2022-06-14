31b96230129ec8d39ba4e42d3999e77e256c257e
Zachary Cutlip <uid000@gmail.com>
2022-06-13 20:50:31 -0700

    ensure op object types get initialized and registered at first import

--- pyonepassword/__init__.py
+++ pyonepassword/__init__.py
@@ -1,2 +1,4 @@
 from .__about__ import __summary__, __title__, __version__
+# Import and discard _API_INITIALIZER to ensure all API classes get registered
+from ._api_initializer import _API_INITIALIZED as _
 from .pyonepassword import OP
---
+++ pyonepassword/_api_initializer.py
@@ -0,0 +1,9 @@
+# Most of the object types and descriptor types need to
+# register themselves with a factory class
+# so we need to import them here to ensure that happens
+from .api.descriptor_types import *  # noqa: F401, F403
+from .api.object_types import *  # noqa: F401, F403
+
+# Declare token object that can be imported into __init__.py without polluting namespace
+# to ensure the classes mentioned above get imported & initialized
+_API_INITIALIZED = True

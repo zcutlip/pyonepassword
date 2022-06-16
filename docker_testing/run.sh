02a9293d8699e45f720d5ed7741b8b33136b6fe0
Zachary Cutlip <uid000@gmail.com>
2022-06-16 08:57:56 -0700

    fix shellcheck errors

--- docker_testing/run.sh
+++ docker_testing/run.sh
@@ -3,7 +3,7 @@
 ret=0
 
 docker run   --rm -it -v "$(pwd):/usr/src/testdir" docker_py38 /test.sh "$@"
-ret=`expr $? + $ret`
+ret="$(($?+ret))"
 docker run   --rm -it -v "$(pwd):/usr/src/testdir" docker_py39 /test.sh "$@"
-ret=`expr $? + $ret`
-exit $ret
+ret="$(($?+ret))"
+exit "$ret"

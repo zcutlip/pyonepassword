7f2192f68cfd5b90585a13b67ce20a502535d555
Zachary Cutlip <uid000@gmail.com>
2022-06-19 16:25:48 -0700

    move dockerfiles and test script to common directory
    - build docker container using -f flag to use the right file

--- docker_testing/build.sh
+++ docker_testing/build.sh
@@ -2,5 +2,5 @@
 
 SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 
-docker build "$SCRIPT_DIR"/docker/3.8 -t docker_py38
-docker build "$SCRIPT_DIR"/docker/3.9 -t docker_py39
+docker build "$SCRIPT_DIR"/docker/ -f "$SCRIPT_DIR"/docker/Dockerfile_py38 -t docker_py38 || exit
+docker build "$SCRIPT_DIR"/docker/ -f "$SCRIPT_DIR"/docker/Dockerfile_py39 -t docker_py39 || exit
---
+++ docker_testing/docker/test.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+uname -a
+NUM_PROCS="$(nproc)"
+export NUM_PROCS
+
+echo "Number of processors: $NUM_PROCS"
+
+cd "$TESTDIR" || exit
+
+if [ -z "$PYTEST_ENV" ];
+then
+    echo "PYTEST_ENV not set"
+    exit 1
+fi
+
+tox --parallel--safe-build -e "$PYTEST_ENV" --workdir "$TOX_WORKDIR" -- "$@"

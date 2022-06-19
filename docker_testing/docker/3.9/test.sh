0ef95cc09764710bcb9e4522108d6933e30bc192
Zachary Cutlip <uid000@gmail.com>
2022-06-19 16:08:25 -0700

    make in-container test script detect number of processors and pass to tox/pytest

--- docker_testing/docker/3.9/test.sh
+++ docker_testing/docker/3.9/test.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 uname -a
+NUM_PROCS="$(nproc)"
+export NUM_PROCS
+
+echo "Number of processors: $NUM_PROCS"
+
 cd "$TESTDIR" || exit
 tox --parallel--safe-build -e py39 --workdir "$TOX_WORKDIR" -- "$@"

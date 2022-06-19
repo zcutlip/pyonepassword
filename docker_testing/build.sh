57c2f695fd77e8dd8d127903a0c4f246e44bfc9b
Zachary Cutlip <uid000@gmail.com>
2022-06-19 16:29:07 -0700

    update docker build script

--- docker_testing/build.sh
+++ docker_testing/build.sh
@@ -2,5 +2,5 @@
 
 SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 
-docker build "$SCRIPT_DIR"/docker/ -f "$SCRIPT_DIR"/docker/Dockerfile_py38 -t docker_py38 || exit
-docker build "$SCRIPT_DIR"/docker/ -f "$SCRIPT_DIR"/docker/Dockerfile_py39 -t docker_py39 || exit
+docker build "$SCRIPT_DIR"/docker/ -f "$SCRIPT_DIR"/docker/py38.Dockerfile -t docker_py38 || exit
+docker build "$SCRIPT_DIR"/docker/ -f "$SCRIPT_DIR"/docker/py39.Dockerfile -t docker_py39 || exit

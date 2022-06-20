3b37cf3fdfe6c847be68a93c3cb3dc0f66cca415
Zachary Cutlip <uid000@gmail.com>
2022-06-19 20:20:04 -0700

    add api.decorators to _api_initializer

--- pyonepassword/_api_initializer.py
+++ pyonepassword/_api_initializer.py
@@ -1,6 +1,7 @@
 # Most of the object types and descriptor types need to
 # register themselves with a factory class
 # so we need to import them here to ensure that happens
+from .api.decorators import *  # noqa: F401, F403
 from .api.descriptor_types import *  # noqa: F401, F403
 from .api.object_types import *  # noqa: F401, F403
 

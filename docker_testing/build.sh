8597fcd1f309eaea39e5105b25cd588802b08fec
Zachary Cutlip <uid000@gmail.com>
2022-06-20 23:03:42 -0700

    update build/clean/run scripts for additional python 3.10 docker containers

--- docker_testing/build.sh
+++ docker_testing/build.sh
@@ -4,3 +4,4 @@ SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 
 docker build "$SCRIPT_DIR"/docker/ -f "$SCRIPT_DIR"/docker/py38.Dockerfile -t docker_py38 || exit
 docker build "$SCRIPT_DIR"/docker/ -f "$SCRIPT_DIR"/docker/py39.Dockerfile -t docker_py39 || exit
+docker build "$SCRIPT_DIR"/docker/ -f "$SCRIPT_DIR"/docker/py310.Dockerfile -t docker_py310 || exit

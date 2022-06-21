6e291f13175d08bc58311cf3abd481e9850265b4
Zachary Cutlip <uid000@gmail.com>
2022-06-21 12:01:18 -0700

    add ssh key item type and descriptor classes

---
+++ pyonepassword/op_items/ssh_key.py
@@ -0,0 +1,58 @@
+from ._item_descriptor_base import OPAbstractItemDescriptor
+from ._item_descriptor_registry import op_register_item_descriptor_type
+from ._op_item_type_registry import op_register_item_type
+from ._op_items_base import OPAbstractItem
+
+
+@op_register_item_descriptor_type
+class OPSSHKeyItemDescriptor(OPAbstractItemDescriptor):
+    CATEGORY = "SSH_KEY"
+
+    def __init__(self, item_dict):
+        super().__init__(item_dict)
+
+    @property
+    def fingerprint(self) -> str:
+        return self["additional_information"]
+
+
+@op_register_item_type
+class OPSSHKeyItem(OPAbstractItem):
+    CATEGORY = "SSH_KEY"
+
+    def __init__(self, item_dict):
+        super().__init__(item_dict)
+
+    @property
+    def public_key(self) -> str:
+        password = self.field_value_by_id("public_key")
+        return password
+
+    @property
+    def public_key_reference(self) -> str:
+        password = self.field_reference_by_id("public_key")
+        return password
+
+    @property
+    def fingerprint(self) -> str:
+        return self.field_value_by_id("fingerprint")
+
+    @property
+    def fingerprint_reference(self) -> str:
+        return self.field_reference_by_id("fingerprint")
+
+    @property
+    def private_key(self) -> str:
+        return self.field_value_by_id("private_key")
+
+    @property
+    def private_key_reference(self) -> str:
+        return self.field_reference_by_id("private_key")
+
+    @property
+    def key_type(self) -> str:
+        return self.field_value_by_id("key_type")
+
+    @property
+    def key_type_reference(self) -> str:
+        return self.field_reference_by_id("key_type")

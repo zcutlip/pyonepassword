09590921b3e21677f8583774a1e35b00394a5b43
Zachary Cutlip <uid000@gmail.com>
2022-06-22 11:42:55 -0700

    don't expect vault list to be ordered
    - get name or ID of expected vault entry
    - look up result vault entry based on name/ID

--- tests/test_vault_list/test_vault_list_all.py
+++ tests/test_vault_list/test_vault_list_all.py
@@ -29,6 +29,18 @@ def _sanity_check_vault_list(vault_list, expected_list):
     assert len(vault_list) == len(expected_list)
 
 
+def _vault_entry_for_name_or_id(vault_list: OPVaultDescriptorList, identifier):
+    # 'op vault list' doesn't guarantee a particular order, so our expected data may not be
+    # sorted the same as the result data
+    # so we need to comb over the result data to locate the object we want to test
+    vault_entry = None
+    for v in vault_list:
+        if identifier in (v["id"], v["name"]):
+            vault_entry = v
+            break
+    return vault_entry
+
+
 def test_vault_list_all_vaults_01(signed_in_op: OP, expected_vault_list_data: ExpectedVaultListData):
     expected: List[ExpectedVaultListEntry]
     result: OPVaultDescriptorList
@@ -46,13 +58,20 @@ def test_vault_list_all_vaults_02(signed_in_op: OP, expected_vault_list_data: Ex
     epected_vault_list: List[ExpectedVaultListEntry]
     expected: ExpectedVaultListEntry
 
+    # just pick one expected data entry. doesn't matter which
+    ARBITRARY_EXPECTED_IDX = 0
+
     expected_vault_list = expected_vault_list_data.data_for_key("all-vaults")
-    expected = expected_vault_list[0]
+    expected = expected_vault_list[ARBITRARY_EXPECTED_IDX]
+
+    # since we're testing vault_entry.unique_id property
+    # let's instead use the expected 'name' value to locate the right vault
+    # we'll flip it around when we test the name property
+    identifier = expected.name
+
     result = signed_in_op.vault_list()
-
     _sanity_check_vault_list(result, expected_vault_list)
-
-    vault_entry = result[0]
+    vault_entry = _vault_entry_for_name_or_id(result, identifier)
 
     assert isinstance(vault_entry, OPVaultDescriptor)
     assert vault_entry.unique_id == expected.unique_id
@@ -62,13 +81,19 @@ def test_vault_list_all_vaults_03(signed_in_op: OP, expected_vault_list_data: Ex
     epected_vault_list: List[ExpectedVaultListEntry]
     expected: ExpectedVaultListEntry
 
+    # just pick one expected data entry. doesn't matter which
+    ARBITRARY_EXPECTED_IDX = 2
+
     expected_vault_list = expected_vault_list_data.data_for_key("all-vaults")
-    expected = expected_vault_list[2]
+    expected = expected_vault_list[ARBITRARY_EXPECTED_IDX]
+
+    # since we're testing vault_entry.name property
+    # let's instead use the expected 'id' value to locate the right vault
+    identifier = expected.name
+
     result = signed_in_op.vault_list()
-
     _sanity_check_vault_list(result, expected_vault_list)
-
-    vault_entry = result[2]
+    vault_entry = _vault_entry_for_name_or_id(result, identifier)
 
     assert isinstance(vault_entry, OPVaultDescriptor)
     assert vault_entry.name == expected.name

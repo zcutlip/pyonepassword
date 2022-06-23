8bb90671c708a4e4ce9b7fc467f513d81284da8e
Zachary Cutlip <uid000@gmail.com>
2022-06-23 16:17:22 -0700

    added several test cases to test_item_get_ssh_keys module

--- tests/test_item_get_ssh_keys.py
+++ tests/test_item_get_ssh_keys.py
@@ -28,3 +28,124 @@ def test_item_get_ssh_key_01(signed_in_op: OP, expected_ssh_key_data: ExpectedSS
     result = signed_in_op.item_get(item_name)
     assert isinstance(result, OPSSHKeyItem)
     assert result.unique_id == expected.unique_id
+
+
+def test_item_get_ssh_key_02(signed_in_op: OP, expected_ssh_key_data: ExpectedSSHKeyData):
+    item_name = "Example SSH Key"
+    expected: ExpectedSSHKey
+    result: OPSSHKeyItem
+
+    expected = expected_ssh_key_data.data_for_ssh_key(item_name)
+    result = signed_in_op.item_get(item_name)
+    assert isinstance(result, OPSSHKeyItem)
+    assert result.title == expected.title
+
+
+def test_item_get_ssh_key_03(signed_in_op: OP, expected_ssh_key_data: ExpectedSSHKeyData):
+    item_name = "Example SSH Key"
+    expected: ExpectedSSHKey
+    result: OPSSHKeyItem
+
+    expected = expected_ssh_key_data.data_for_ssh_key(item_name)
+    result = signed_in_op.item_get(item_name)
+    assert isinstance(result, OPSSHKeyItem)
+    assert result.category == expected.category
+
+
+def test_item_get_ssh_key_04(signed_in_op: OP, expected_ssh_key_data: ExpectedSSHKeyData):
+    item_name = "Example SSH Key"
+    expected: ExpectedSSHKey
+    result: OPSSHKeyItem
+
+    expected = expected_ssh_key_data.data_for_ssh_key(item_name)
+    result = signed_in_op.item_get(item_name)
+    assert isinstance(result, OPSSHKeyItem)
+    assert result.last_edited_by == expected.last_edited_by
+
+
+def test_item_get_ssh_key_05(signed_in_op: OP, expected_ssh_key_data: ExpectedSSHKeyData):
+    item_name = "Example SSH Key"
+    expected: ExpectedSSHKey
+    result: OPSSHKeyItem
+
+    expected = expected_ssh_key_data.data_for_ssh_key(item_name)
+    result = signed_in_op.item_get(item_name)
+    assert isinstance(result, OPSSHKeyItem)
+    assert result.public_key == expected.public_key
+
+
+def test_item_get_ssh_key_06(signed_in_op: OP, expected_ssh_key_data: ExpectedSSHKeyData):
+    item_name = "Example SSH Key"
+    expected: ExpectedSSHKey
+    result: OPSSHKeyItem
+
+    expected = expected_ssh_key_data.data_for_ssh_key(item_name)
+    result = signed_in_op.item_get(item_name)
+    assert isinstance(result, OPSSHKeyItem)
+    assert result.public_key_reference == expected.public_key_ref
+
+
+def test_item_get_ssh_key_07(signed_in_op: OP, expected_ssh_key_data: ExpectedSSHKeyData):
+    item_name = "Example SSH Key"
+    expected: ExpectedSSHKey
+    result: OPSSHKeyItem
+
+    expected = expected_ssh_key_data.data_for_ssh_key(item_name)
+    result = signed_in_op.item_get(item_name)
+    assert isinstance(result, OPSSHKeyItem)
+    assert result.fingerprint == expected.fingerprint
+
+
+def test_item_get_ssh_key_08(signed_in_op: OP, expected_ssh_key_data: ExpectedSSHKeyData):
+    item_name = "Example SSH Key"
+    expected: ExpectedSSHKey
+    result: OPSSHKeyItem
+
+    expected = expected_ssh_key_data.data_for_ssh_key(item_name)
+    result = signed_in_op.item_get(item_name)
+    assert isinstance(result, OPSSHKeyItem)
+    assert result.fingerprint_reference == expected.fingerprint_ref
+
+
+def test_item_get_ssh_key_09(signed_in_op: OP, expected_ssh_key_data: ExpectedSSHKeyData):
+    item_name = "Example SSH Key"
+    expected: ExpectedSSHKey
+    result: OPSSHKeyItem
+
+    expected = expected_ssh_key_data.data_for_ssh_key(item_name)
+    result = signed_in_op.item_get(item_name)
+    assert isinstance(result, OPSSHKeyItem)
+    assert result.private_key == expected.private_key
+
+
+def test_item_get_ssh_key_10(signed_in_op: OP, expected_ssh_key_data: ExpectedSSHKeyData):
+    item_name = "Example SSH Key"
+    expected: ExpectedSSHKey
+    result: OPSSHKeyItem
+
+    expected = expected_ssh_key_data.data_for_ssh_key(item_name)
+    result = signed_in_op.item_get(item_name)
+    assert isinstance(result, OPSSHKeyItem)
+    assert result.private_key_reference == expected.private_key_ref
+
+
+def test_item_get_ssh_key_11(signed_in_op: OP, expected_ssh_key_data: ExpectedSSHKeyData):
+    item_name = "Example SSH Key"
+    expected: ExpectedSSHKey
+    result: OPSSHKeyItem
+
+    expected = expected_ssh_key_data.data_for_ssh_key(item_name)
+    result = signed_in_op.item_get(item_name)
+    assert isinstance(result, OPSSHKeyItem)
+    assert result.key_type == expected.key_type
+
+
+def test_item_get_ssh_key_12(signed_in_op: OP, expected_ssh_key_data: ExpectedSSHKeyData):
+    item_name = "Example SSH Key"
+    expected: ExpectedSSHKey
+    result: OPSSHKeyItem
+
+    expected = expected_ssh_key_data.data_for_ssh_key(item_name)
+    result = signed_in_op.item_get(item_name)
+    assert isinstance(result, OPSSHKeyItem)
+    assert result.key_type_reference == expected.key_type_ref

32956cc48367a8912378afa07ffc96f6f3e12d9e
Zachary Cutlip <uid000@gmail.com>
2022-06-27 18:48:44 -0700

    add test cases for item field lookup

--- tests/test_item_field.py
+++ tests/test_item_field.py
@@ -1,9 +1,10 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, List
 
 import pytest
 
+from pyonepassword.api.object_types import OPLoginItem
 from pyonepassword.op_items.item_section import OPItemField
 
 if TYPE_CHECKING:
@@ -11,7 +12,19 @@ if TYPE_CHECKING:
         ExpectedItemField,
         ExpectedItemFieldData
     )
+    from .fixtures.expected_login import ExpectedLogin
     from .fixtures.valid_data import ValidData
+
+
+def _lookup_exepcted_item_field(fields: List[ExpectedItemField], field_id: str):
+    """
+    Field order may not be reliable, so we have to hunt for the right one
+    """
+    field = None
+    for f in fields:
+        if f.field_id == field_id:
+            field = f
+    return field
 
 
 # ensure HOME env variable is set, and there's a valid op config present
@@ -140,3 +153,39 @@ def test_item_field_17(valid_data: ValidData, expected_item_field_data: Expected
     low = expected_entropy * (1 - margin)
     assert field.entropy < high
     assert field.entropy > low
+
+
+def test_item_lookup_field_01(valid_data: ValidData, expected_login_item_data):
+    item_name = "Example Login with Fields"
+    field_label = "Example Field"
+    field_id = "zyhpoqkrwjtrq6qh5vhwylh6ie"
+    expected_login: ExpectedLogin = expected_login_item_data.data_for_login(
+        item_name)
+
+    expected_field_list: ExpectedItemField = expected_login.fields_by_label(
+        field_label)
+    expected_field = _lookup_exepcted_item_field(expected_field_list, field_id)
+
+    valid_item_dict = valid_data.data_for_name("example-login-with-fields")
+    result_login_item = OPLoginItem(valid_item_dict)
+    result = result_login_item.fields_by_label(field_label)
+    result = result[1]
+    assert result.value == expected_field.value
+
+
+def test_item_lookup_field_02(valid_data: ValidData, expected_login_item_data):
+    item_name = "Example Login with Fields"
+    field_label = "Example Field"
+    field_id = "ikr76mnggw767qwqoel624oqv4"
+    expected_login: ExpectedLogin = expected_login_item_data.data_for_login(
+        item_name)
+
+    expected_field_list: ExpectedItemField = expected_login.fields_by_label(
+        field_label)
+    expected_field = _lookup_exepcted_item_field(expected_field_list, field_id)
+
+    valid_item_dict = valid_data.data_for_name("example-login-with-fields")
+    result_login_item = OPLoginItem(valid_item_dict)
+    result = result_login_item.first_field_by_label(field_label)
+
+    assert result.value == expected_field.value

c71252e665ea0a33bd41c8414098e30c97cde11d
Zachary Cutlip <uid000@gmail.com>
2022-07-06 18:22:41 -0700

    add missing functions to archive_op_binary

--- scripts/archive_op_binary.sh
+++ scripts/archive_op_binary.sh
@@ -1,6 +1,29 @@
 #!/bin/sh -e
 
 . "$(dirname "$0")"/functions.sh
+
+
+_readlink(){ readlink "$1" || echo "$1"; }
+
+# Don't shadow the 'realpath' executable which may be installed on
+# some systems (e.g., via homebrew)
+_realpath() { cd "$(dirname "$0")" && _readlink "$(pwd)"/"$(basename "$0")"; }
+
+md5check(){
+    _file1="$1"
+    _file2="$2"
+    hash1="$(md5sum "$_file1" | awk '{ print $1 }')"
+    hash2="$(md5sum "$_file2" | awk '{ print $1 }')"
+    [ "$hash1" = "$hash2" ]
+    ret=$?
+    if [ $ret -ne 0 ];
+    then
+        echo "$hash1"
+        echo "$hash2"
+    fi
+    return $ret
+}
+
 real_path="$(_realpath "$0")"
 SRC_ROOT="$(cd "$(dirname "$real_path")" && dirname "$(pwd)")"
 OP_BINARY_PATH="$SRC_ROOT/op-binaries"

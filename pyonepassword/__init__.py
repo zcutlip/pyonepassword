695b6c11ffedacdf2b24e89634f81ff962d1e5ab
Zachary Cutlip <uid000@gmail.com>
2022-08-17 13:03:56 -0700

    import colored_traceback.auto if it's present

--- pyonepassword/__init__.py
+++ pyonepassword/__init__.py
@@ -1,3 +1,10 @@
+try:
+    # register colored_traceback hook if in a dev environment (dev-reqs.txt)
+    import colored_traceback.auto
+except ImportError:
+    # otherwise just stick to the basics
+    pass
+
 from .__about__ import __summary__, __title__, __version__
 # Import and discard _API_INITIALIZER to ensure all API classes get registered
 from ._api_initializer import _API_INITIALIZED as _

f9482682e144fe062e7c94c3c4a5a92285c430f5
Zachary Cutlip <uid000@gmail.com>
2022-08-16 19:59:27 -0700

    initialize new item field classes at subpackage level so they get registered

--- pyonepassword/op_items/__init__.py
+++ pyonepassword/op_items/__init__.py
@@ -1,2 +1,3 @@
+from ._new_fields import OPNewConcealedField, OPNewStringField
 from ._op_item_type_registry import OPItemFactory, OPUnknownItemTypeException
 from ._op_items_base import OPFieldNotFoundException

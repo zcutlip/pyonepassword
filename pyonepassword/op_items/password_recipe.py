7abda1174edd89efc8f18f31a434db5cae92b8db
Zachary Cutlip <uid000@gmail.com>
2022-08-18 20:40:55 -0700

    add password_recipe module

---
+++ pyonepassword/op_items/password_recipe.py
@@ -0,0 +1,24 @@
+class OPInvalidPasswordRecipeException(Exception):
+    pass
+
+
+class OPPasswordRecipe:
+    def __init__(self, len: int = 20, letters=True, digits=True, symbols=True):
+        recipe_parts = [str(len)]
+
+        if True not in [letters, digits, symbols]:
+            raise OPInvalidPasswordRecipeException(
+                "Letters, digits, & symbols all disabled. One or more must be enabled")
+
+        if letters:
+            recipe_parts.append("letters")
+        if digits:
+            recipe_parts.append("digits")
+        if symbols:
+            recipe_parts.append("symbols")
+
+        self.recipe = recipe_parts
+
+    def __str__(self) -> str:
+        recipe_str = ",".join(self.recipe)
+        return recipe_str

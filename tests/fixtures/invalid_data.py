2464e6a3cdddd06101b6d02e4fbc172758691135
Zachary Cutlip <uid000@gmail.com>
2022-08-28 19:41:40 -0700

    make InvalidData extend ValidData

--- tests/fixtures/invalid_data.py
+++ tests/fixtures/invalid_data.py
@@ -1,24 +1,7 @@
-import json
-from pathlib import Path
-
 from .paths import INVALID_DATA_PATH, INVALID_DATA_REGISTRY_PATH
+from .valid_data import ValidData
 
 
-class InvalidData:
-
-    def __init__(self):
-        registry = json.load(open(INVALID_DATA_REGISTRY_PATH, "r"))
-        self._registry = registry
-
-    def data_for_name(self, entry_name):
-        entry = self._registry[entry_name]
-        item_filename = entry["name"]
-        item_type = entry["type"]
-        item_path = Path(INVALID_DATA_PATH, item_filename)
-
-        if item_type == "text":
-            mode = "r"
-        else:
-            mode = "rb"
-        data = open(item_path, mode).read()
-        return data
+class InvalidData(ValidData):
+    REGISTRY_PATH = INVALID_DATA_REGISTRY_PATH
+    DATA_PATH = INVALID_DATA_PATH

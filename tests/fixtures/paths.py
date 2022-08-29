3642c63123283b563b2edf79b2f721757ec14b04
Zachary Cutlip <uid000@gmail.com>
2022-08-28 19:42:45 -0700

    add paths for epected data registry,
    - rearrange path constants to make more sense

--- tests/fixtures/paths.py
+++ tests/fixtures/paths.py
@@ -3,14 +3,16 @@ from pathlib import Path
 TEST_DATA_PATH = Path("tests", "data")
 TEST_CONFIG_PATH = Path("tests", "config")
 
-EXPECTED_DATA_PATH = Path(TEST_DATA_PATH, "expected-data.json")
 MOCK_OP_CONFIG_PATH = Path(TEST_CONFIG_PATH, "mock-op")
 RESP_DIRECTORY_PATH = Path(MOCK_OP_CONFIG_PATH, "response-directory.json")
 ALT_RESP_DIRECTORY_PATH = Path(
     MOCK_OP_CONFIG_PATH, "alternate-response-directory.json")
 UNAUTH_RESP_DIRECTORY_PATH = Path(
     MOCK_OP_CONFIG_PATH, "unauth-response-directory.json")
-INVALID_DATA_REGISTRY_PATH = Path(TEST_DATA_PATH, "invalid-data-registry.json")
 VALID_DATA_REGISTRY_PATH = Path(TEST_DATA_PATH, "valid-data-registry.json")
-INVALID_DATA_PATH = Path(TEST_DATA_PATH, "invalid-data")
 VALID_DATA_PATH = Path(TEST_DATA_PATH, "valid-data")
+INVALID_DATA_REGISTRY_PATH = Path(TEST_DATA_PATH, "invalid-data-registry.json")
+INVALID_DATA_PATH = Path(TEST_DATA_PATH, "invalid-data")
+EXPECTED_DATA_REGISTRY_PATH = Path(
+    TEST_DATA_PATH, "expected-data-registry.json")
+EXPECTED_DATA_PATH = Path(TEST_DATA_PATH, "expected-data")

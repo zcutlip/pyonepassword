d4d81c4e1c05bab9cb037dd56dce2f8814f5e9f5
Zachary Cutlip <uid000@gmail.com>
2022-08-28 19:41:15 -0700

    have ValidData get its paths from class attributes
    - allow it to be extended for other types of data

--- tests/fixtures/valid_data.py
+++ tests/fixtures/valid_data.py
@@ -5,16 +5,18 @@ from .paths import VALID_DATA_PATH, VALID_DATA_REGISTRY_PATH
 
 
 class ValidData:
+    REGISTRY_PATH = VALID_DATA_REGISTRY_PATH
+    DATA_PATH = VALID_DATA_PATH
 
     def __init__(self):
-        registry = json.load(open(VALID_DATA_REGISTRY_PATH, "r"))
+        registry = json.load(open(self.REGISTRY_PATH, "r"))
         self._registry = registry
 
     def data_for_name(self, entry_name):
         entry = self._registry[entry_name]
         item_filename = entry["name"]
         item_type = entry["type"]
-        item_path = Path(VALID_DATA_PATH, item_filename)
+        item_path = Path(self.DATA_PATH, item_filename)
 
         if item_type in ["text", "json"]:
             mode = "r"

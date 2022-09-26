5d957d8150737d8adf40bdb27c0987545c95b249
Zachary Cutlip <uid000@gmail.com>
2022-09-25 21:25:02 -0700

    test that OPNotSignedInException is raised
    - if existing_auth=EXISTING_AUTH_REQD
    - session token is not valid

--- tests/test_misc.py
+++ tests/test_misc.py
@@ -3,7 +3,10 @@ from __future__ import annotations
 import pytest
 
 from pyonepassword import OP
-from pyonepassword.api.authentication import EXISTING_AUTH_AVAIL
+from pyonepassword.api.authentication import (
+    EXISTING_AUTH_AVAIL,
+    EXISTING_AUTH_REQD
+)
 from pyonepassword.api.exceptions import (
     OPNotSignedInException,
     OPUnknownAccountException
@@ -92,6 +95,22 @@ def test_use_existing_session_04():
     """
 
     OP(op_path='mock-op', existing_auth=EXISTING_AUTH_AVAIL, password_prompt=False)
+
+
+@pytest.mark.usefixtures("valid_op_cli_config_homedir")
+@pytest.mark.usefixtures("setup_op_sess_var_unauth_env")
+def test_use_existing_session_05():
+    """
+    Simulate a pyonepassword environment that:
+    - doesn't use biometric
+    - DOES have OP_SESSION_<user uuid> env variable set
+    - session token not valid
+    Tell OP it MUST use an existing session.
+    Check that OP(use_existing_session=True) fails
+    """
+    with pytest.raises(OPNotSignedInException):
+        OP(op_path='mock-op', existing_auth=EXISTING_AUTH_REQD,
+           account="example_shorthand")
 
 
 @pytest.mark.usefixtures("valid_op_cli_config_no_shorthand")

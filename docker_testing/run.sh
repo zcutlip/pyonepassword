b9d007a2fce22d61519b29479fbf8f18420fc186
Zachary Cutlip <uid000@gmail.com>
2022-09-26 22:37:26 -0700

    handle sigint in docker_testing/run.sh

--- docker_testing/run.sh
+++ docker_testing/run.sh
@@ -1,5 +1,22 @@
 #!/bin/sh
 
+quit(){
+    set -x
+    if [ $# -ge 2 ];
+    then
+        echo "$1"
+        shift
+    fi
+    kill -TERM -$$
+}
+
+handle_sig(){
+    quit "Got keyboard interrupt" 1
+}
+
+trap handle_sig INT
+
+
 ret=0
 
 docker run   --rm -it -v "$(pwd):/usr/src/testdir" docker_py38 /test.sh "$@"

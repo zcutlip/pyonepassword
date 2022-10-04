cad4e093dcd31aa7c4fe4e63e5fb5d84bb32f0ec
Zachary Cutlip <uid000@gmail.com>
2022-10-03 19:55:34 -0700

    add test module for new item field classes

---
+++ tests/new_item/test_new_item_field.py
@@ -0,0 +1,117 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+from pyonepassword.op_items._new_fields import OPNewUsernameField
+from pyonepassword.op_items.item_section import OPItemField
+
+if TYPE_CHECKING:
+    from ..fixtures.expected_item_fields import (
+        ExpectedItemField,
+        ExpectedItemFieldData
+    )
+    from ..fixtures.valid_data import ValidData
+
+
+def test_new_username_field_01(valid_data: ValidData, expected_item_field_data: ExpectedItemFieldData):
+    expected: ExpectedItemField
+
+    expected = expected_item_field_data.data_for_key("example-login-username")
+    field_dict = valid_data.data_for_name("login-item-field-username")
+
+    username = field_dict["value"]
+    field_id = field_dict["id"]
+    new_field = OPNewUsernameField(field_id, username, field_id=field_id)
+
+    assert new_field.value == expected.value
+
+
+def test_new_username_field_02(valid_data: ValidData, expected_item_field_data: ExpectedItemFieldData):
+    expected: ExpectedItemField
+
+    expected = expected_item_field_data.data_for_key("example-login-username")
+    field_dict = valid_data.data_for_name("login-item-field-username")
+
+    username = field_dict["value"]
+    field_id = field_dict["id"]
+    new_field = OPNewUsernameField(field_id, username, field_id=field_id)
+
+    assert new_field.field_id == expected.field_id
+
+
+def test_new_username_field_03(valid_data: ValidData, expected_item_field_data: ExpectedItemFieldData):
+    expected: ExpectedItemField
+
+    expected = expected_item_field_data.data_for_key("example-login-username")
+    field_dict = valid_data.data_for_name("login-item-field-username")
+
+    username = field_dict["value"]
+    field_id = field_dict["id"]
+    new_field = OPNewUsernameField(field_id, username, field_id=field_id)
+
+    assert new_field.label == expected.label
+
+
+def test_new_username_field_04(valid_data: ValidData, expected_item_field_data: ExpectedItemFieldData):
+    expected: ExpectedItemField
+
+    expected = expected_item_field_data.data_for_key("example-login-username")
+    field_dict = valid_data.data_for_name("login-item-field-username")
+
+    username = field_dict["value"]
+    field_id = field_dict["id"]
+    new_field = OPNewUsernameField(field_id, username, field_id=field_id)
+
+    assert new_field.purpose == expected.purpose
+
+
+def test_new_username_field_05(valid_data: ValidData, expected_item_field_data: ExpectedItemFieldData):
+    expected: ExpectedItemField
+
+    expected = expected_item_field_data.data_for_key("example-login-username")
+    field_dict = valid_data.data_for_name("login-item-field-username")
+
+    existing_field = OPItemField(field_dict)
+
+    new_field = OPNewUsernameField.from_field(existing_field)
+
+    assert new_field.value == expected.value
+
+
+def test_new_username_field_06(valid_data: ValidData, expected_item_field_data: ExpectedItemFieldData):
+    expected: ExpectedItemField
+
+    expected = expected_item_field_data.data_for_key("example-login-username")
+    field_dict = valid_data.data_for_name("login-item-field-username")
+
+    existing_field = OPItemField(field_dict)
+
+    new_field = OPNewUsernameField.from_field(existing_field)
+
+    assert new_field.field_id == expected.field_id
+
+
+def test_new_username_field_07(valid_data: ValidData, expected_item_field_data: ExpectedItemFieldData):
+    expected: ExpectedItemField
+
+    expected = expected_item_field_data.data_for_key("example-login-username")
+    field_dict = valid_data.data_for_name("login-item-field-username")
+
+    existing_field = OPItemField(field_dict)
+
+    new_field = OPNewUsernameField.from_field(existing_field)
+
+    assert new_field.label == expected.label
+
+
+def test_new_username_field_08(valid_data: ValidData, expected_item_field_data: ExpectedItemFieldData):
+    expected: ExpectedItemField
+
+    expected = expected_item_field_data.data_for_key("example-login-username")
+    field_dict = valid_data.data_for_name("login-item-field-username")
+
+    existing_field = OPItemField(field_dict)
+
+    new_field = OPNewUsernameField.from_field(existing_field)
+
+    assert new_field.purpose == expected.purpose

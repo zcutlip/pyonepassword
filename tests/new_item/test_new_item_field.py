865cf6fa83605b387319f2cb49d33ff858bf1d8f
Zachary Cutlip <uid000@gmail.com>
2022-10-05 19:18:00 -0700

    add field UUID generation test

--- tests/new_item/test_new_item_field.py
+++ tests/new_item/test_new_item_field.py
@@ -132,3 +132,22 @@ def test_new_field_generate_uuid_01(valid_data: ValidData):
     print(existing_field.field_id)
     print(new_field.field_id)
     assert new_field.field_id != existing_field.field_id
+
+
+def test_new_field_generate_uuid_02(valid_data: ValidData):
+    """
+    Generate two identical fields without specificying field ID
+    Verify field ID gets generated and is random for both
+    """
+    field_dict = valid_data.data_for_name("example-field-with-uuid")
+    existing_field = OPItemField(field_dict)
+
+    field_label = existing_field.label
+    field_value = existing_field.value
+
+    new_field_1 = OPNewStringField(field_label, field_value)
+    new_field_2 = OPNewStringField(field_label, field_value)
+
+    assert new_field_1.field_id is not None
+    assert new_field_2.field_id is not None
+    assert new_field_1.field_id != new_field_2.field_id

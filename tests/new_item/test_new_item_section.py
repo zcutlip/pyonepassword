11995868c7cd4e94c19994e37639c578ba85306e
Zachary Cutlip <uid000@gmail.com>
2022-10-07 08:52:57 -0700

    add docstrings to new item section tests

--- tests/new_item/test_new_item_section.py
+++ tests/new_item/test_new_item_section.py
@@ -14,6 +14,10 @@ if TYPE_CHECKING:
 
 
 def test_new_item_section_01(expected_item_section_data: ExpectedItemSectionData):
+    """
+    Create a new section providing only a lable
+    Verify the label is set as expected
+    """
     expected: ExpectedItemSection = expected_item_section_data.data_for_key(
         "example-section-1")
     label = expected.label
@@ -23,6 +27,10 @@ def test_new_item_section_01(expected_item_section_data: ExpectedItemSectionData
 
 
 def test_new_item_section_02(expected_item_section_data: ExpectedItemSectionData):
+    """
+    Create a new section, providing only a label
+    Verify the generated section ID is a UUID
+    """
     expected = expected_item_section_data.data_for_key("example-section-1")
     label = expected.label
     new_section = OPNewSection(label)

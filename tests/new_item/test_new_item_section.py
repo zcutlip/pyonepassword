8b84ef5ea2c7a8a4aeb97f0204c1177e13feed1f
Zachary Cutlip <uid000@gmail.com>
2022-10-06 20:25:25 -0700

    add test module for creating new item sections

---
+++ tests/new_item/test_new_item_section.py
@@ -0,0 +1,30 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+from pyonepassword.op_items._new_item import OPNewSection
+
+from ..test_support.util import is_uuid
+
+if TYPE_CHECKING:
+    from ..fixtures.expected_item_sections import (
+        ExpectedItemSection,
+        ExpectedItemSectionData
+    )
+
+
+def test_new_item_section_01(expected_item_section_data: ExpectedItemSectionData):
+    expected: ExpectedItemSection = expected_item_section_data.data_for_key(
+        "example-section-1")
+    label = expected.label
+    new_section = OPNewSection(label)
+
+    assert new_section.label == expected.label
+
+
+def test_new_item_section_02(expected_item_section_data: ExpectedItemSectionData):
+    expected = expected_item_section_data.data_for_key("example-section-1")
+    label = expected.label
+    new_section = OPNewSection(label)
+
+    assert is_uuid(new_section.section_id)

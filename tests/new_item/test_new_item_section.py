a264ee8a3c97ee4b57787249ed71aca8c1f5aad4
Zachary Cutlip <uid000@gmail.com>
2022-10-07 08:55:40 -0700

    add test that two sections' generated IDs aren't the same

--- tests/new_item/test_new_item_section.py
+++ tests/new_item/test_new_item_section.py
@@ -36,3 +36,20 @@ def test_new_item_section_02(expected_item_section_data: ExpectedItemSectionData
     new_section = OPNewSection(label)
 
     assert is_uuid(new_section.section_id)
+
+
+def test_new_item_section_03(expected_item_section_data: ExpectedItemSectionData):
+    """
+    Create two new sections providing the same lable
+    Verify the sections' IDs are UUIDs and are not the same as each other
+    """
+    expected = expected_item_section_data.data_for_key("example-section-1")
+    label = expected.label
+
+    new_section_1 = OPNewSection(label)
+    new_section_2 = OPNewSection(label)
+
+    assert is_uuid(new_section_1.section_id)
+    assert is_uuid(new_section_2.section_id)
+
+    assert new_section_1.section_id != new_section_2.section_id

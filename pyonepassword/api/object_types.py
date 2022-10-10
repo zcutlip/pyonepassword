730683bbdf6ee72e7219a07159c7e83a41894c83
Zachary Cutlip <uid000@gmail.com>
2022-10-09 20:46:23 -0700

    add to api.object_types:
    - OPLoginItemNewPrimaryURL
    - OPLoginItemNewURL
    - OPNewLoginItem

--- pyonepassword/api/object_types.py
+++ pyonepassword/api/object_types.py
@@ -3,7 +3,12 @@ from ..op_items._item_list import OPItemList
 from ..op_items.api_credential import OPAPICredentialItem
 from ..op_items.credit_card import OPCreditCardItem
 from ..op_items.document import OPDocumentFile, OPDocumentItem
-from ..op_items.login import OPLoginItem
+from ..op_items.login import (
+    OPLoginItem,
+    OPLoginItemNewPrimaryURL,
+    OPLoginItemNewURL,
+    OPNewLoginItem
+)
 from ..op_items.password import OPPasswordItem
 from ..op_items.secure_note import OPSecureNoteItem
 from ..op_items.server import OPServerItem

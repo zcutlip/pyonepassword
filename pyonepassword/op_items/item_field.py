7d9f988f15cc9a165408349b6239ea7c8038e521
Zachary Cutlip <uid000@gmail.com>
2022-10-19 16:32:51 -0700

    add item_field module for OPItemField subclasses

---
+++ pyonepassword/op_items/item_field.py
@@ -0,0 +1,21 @@
+from .field_registry import op_register_item_field_type
+from .item_field_base import OPItemField
+
+
+@op_register_item_field_type
+class OPStringField(OPItemField):
+    FIELD_TYPE = "STRING"
+
+
+@op_register_item_field_type
+class OPConcealedField(OPItemField):
+    FIELD_TYPE = "CONCEALED"
+
+
+@op_register_item_field_type
+class OPTOTPField(OPStringField):
+    FIELD_TYPE = "OTP"
+
+    @property
+    def totp_secret(self):
+        return self.value

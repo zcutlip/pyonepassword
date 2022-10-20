5de82a4daca6e1a884dc0fb7b416d5f4092423b3
Zachary Cutlip <uid000@gmail.com>
2022-10-19 21:39:23 -0700

    add "strip" option to valid/invalid data

--- tests/fixtures/valid_data.py
+++ tests/fixtures/valid_data.py
@@ -1,5 +1,6 @@
 import json
 from pathlib import Path
+from typing import Dict
 
 from .paths import VALID_DATA_PATH, VALID_DATA_REGISTRY_PATH
 
@@ -13,9 +14,10 @@ class ValidData:
         self._registry = registry
 
     def data_for_name(self, entry_name):
-        entry = self._registry[entry_name]
+        entry: Dict = self._registry[entry_name]
         item_filename = entry["name"]
         item_type = entry["type"]
+        strip = entry.get("strip", False)
         item_path = Path(self.DATA_PATH, item_filename)
 
         if item_type in ["text", "json"]:
@@ -25,4 +27,7 @@ class ValidData:
         data = open(item_path, mode).read()
         if item_type == "json":
             data = json.loads(data)
+
+        if strip:
+            data = data.rstrip()
         return data

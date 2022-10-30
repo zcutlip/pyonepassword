6d46871c95c0c51ea4d20d3433e778606de55980
Zachary Cutlip <uid000@gmail.com>
2022-10-29 21:34:51 -0700

    add item_creation ipython snippet

---
+++ ipython_snippets/item_creation.py
@@ -0,0 +1,45 @@
+from typing import List
+
+from pyonepassword import OP  # noqa: F401
+from pyonepassword.api.object_types import (
+    OPItemField,
+    OPNewLoginItem,
+    OPNewStringField,
+    OPPasswordRecipe,
+    OPSection,
+    OPSecureNoteItem
+)
+from pyonepassword.op_items._new_item import OPNewItemMixin
+
+username = "test_username"
+title = "Test Login Item"
+
+recipe = OPPasswordRecipe(length=40, digits=False, symbols=False)
+new_item_1 = OPNewLoginItem(title, username)
+
+
+class OPNewSecureNoteItem(OPNewItemMixin, OPSecureNoteItem):
+
+    def __init__(self,
+                 title: str,
+                 note_text: str,
+                 fields: List[OPItemField] = [],
+                 sections: List[OPSection] = []):
+
+        if sections is None:  # pragma: no coverage
+            sections = []
+        else:
+            sections = list(sections)
+        if fields is None:  # pragma: no coverage
+            fields = []
+        else:
+            fields = list(fields)
+
+        note_text_field = OPNewStringField(
+            "notesPlain",
+            note_text,
+            field_id="notesPlain")
+
+        fields.append(note_text_field)
+
+        super().__init__(title, sections=sections, fields=fields)

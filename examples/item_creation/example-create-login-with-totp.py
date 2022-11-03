fc703eb0ce0017951fa9d309e7fccf36c2d553a4
Zachary Cutlip <uid000@gmail.com>
2022-11-03 10:47:06 -0700

    clean up example-create-login-with-totp.py

--- examples/item_creation/example-create-login-with-totp.py
+++ examples/item_creation/example-create-login-with-totp.py
@@ -14,42 +14,53 @@ from pyonepassword.op_items.password_recipe import (
     OPPasswordRecipe
 )
 
-if __name__ == "__main__":
 
+def totp_secret():
+    # Normally the website/service provides this, but
+    # we generate one for our example
+    secret = secrets.token_bytes(10)
+    secret = base64.b32encode(secret).decode()
+    secret = secret.rstrip("=")
+    return secret
+
+
+def build_totp_uri():
+    issuer = "Example Website"
+    account_name = "newuser@website"
+    secret = totp_secret()
+
+    # If website/service provides fully-formed URI string
+    new_totp_uri = "otpauth://totp/Example%20Website:newuser%40website?secret=EPW4UE4E7IKC2QMB&issuer=Example%20Website"
+
+    # or for more sanity checking & proper encoding, create a OPNewTOTPUri object
+    new_totp_uri = OPNewTOTPUri(
+        secret, account_name=account_name, issuer=issuer)
+
+    return new_totp_uri
+
+
+def main():
     username = "test_username"
-    title = "Test Login Item"
+    title = "New Login with TOT"
 
     op = OP()
     recipe = OPPasswordRecipe(length=40, digits=False, symbols=False)
     # or...
     recipe = LETTERS_DIGITS_SYMBOLS_20
-    secret = secrets.token_bytes(10)
-    secret = base64.b32encode(secret).decode()
-    secret = secret.rstrip("=")
 
-    issuer = "Example Website"
-    account_name = "newuser@website"
-    new_totp = "otpauth://totp/Example%20Website:newuser%40website?secret=EPW4UE4E7IKC2QMB&issuer=Example%20Website"
-    # or...
-    new_totp = OPNewTOTPUri(secret, account_name=account_name, issuer=issuer)
-    print(str(new_totp))
-
+    new_totp_uri = build_totp_uri()
     totp_field_label = "One-time Password"
-    totp_field = OPNewTOTPField(totp_field_label, new_totp)
+    totp_field = OPNewTOTPField(totp_field_label, new_totp_uri)
 
-    new_item_template = OPNewLoginItem(
-        "New Login with TOTP", account_name, fields=[totp_field])
+    new_item_template = OPNewLoginItem(title, username, fields=[totp_field])
 
     new_item: OPLoginItem = op.item_create(
         new_item_template, password_recipe=recipe, vault="Test Data")
 
-    # pprint(new_item, indent=2)
     totp_field: OPTOTPField = new_item.first_field_by_label(totp_field_label)
-    print(totp_field.totp_secret)
-    print(totp_field.totp)
-    # new_item_2 = op.login_item_create("New Login 2",
-    #                                   "new_user_2",
-    #                                   url="https://website.example",
-    #                                   password="correct-horse-battery-staple",
-    #                                   vault="Test Data")
-    # pprint(new_item_2, indent=2)
+    totp_code = totp_field.totp
+    print(f"{totp_code}")
+
+
+if __name__ == "__main__":
+    main()

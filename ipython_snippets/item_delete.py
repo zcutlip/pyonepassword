5401a1349d0898254729dc20763a2702769c3537
Zachary Cutlip <uid000@gmail.com>
2022-11-08 21:36:35 -0800

    update item_delte ipython_snippit

--- ipython_snippets/item_delete.py
+++ ipython_snippets/item_delete.py
@@ -1,6 +1,7 @@
 from pyonepassword import OP, logging
+from pyonepassword.api.exceptions import OPCmdFailedException  # noqa: F401
 
 logger = logging.console_logger("item-delete", logging.DEBUG)
-op = OP(logger=logger)
+op = OP(logger=logger, op_path="mock-op")
 vault = "Test Data"
 item_name = "Delete Me Unique"

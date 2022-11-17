d4c91db62099710b6c373ed2ad0c98faf6158662
Zachary Cutlip <uid000@gmail.com>
2022-11-16 18:00:38 -0800

    add ipython snippets for excercising pytest code

---
+++ ipython_snippets/pytest/misc.py
@@ -0,0 +1,23 @@
+import os
+
+from pyonepassword import OP, logging
+from pyonepassword._op_cli_config import OPCLIConfig
+from pyonepassword._py_op_commands import EXISTING_AUTH_REQD
+from tests.fixtures.op_fixtures import _setup_alt_env, _setup_normal_env
+from tests.fixtures.valid_op_cli_config import (
+    VALID_OP_CONFIG_NO_SHORTHAND_KEY,
+    ValidOPCLIConfig
+)
+
+# _setup_alt_env()
+# _setup_normal_env()
+logger = logging.console_logger("pytest-misc", logging.DEBUG)
+
+# config_obj = ValidOPCLIConfig(
+#     valid_data_key=VALID_OP_CONFIG_NO_SHORTHAND_KEY)
+# config = OPCLIConfig()
+# shorthand = "NO_SUCH_SHORTHAND"
+# conf = config.get_config(account_id=shorthand)
+
+logger.debug(f"HOME: {os.environ['HOME']}")
+op = OP(logger=logger)

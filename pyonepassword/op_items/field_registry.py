0a11887c5485937667d4f948fe575473f572f453
Zachary Cutlip <uid000@gmail.com>
2022-11-22 14:49:40 -0800

    more type-hinting fixes

--- pyonepassword/op_items/field_registry.py
+++ pyonepassword/op_items/field_registry.py
@@ -1,27 +1,27 @@
-from typing import Dict
+from typing import Any, Dict, Type
 
 from .item_field_base import OPItemField
 
 
 class OPItemFieldFactory:
-    _TYPE_REGISTRY: Dict[str, OPItemField] = {}
+    _TYPE_REGISTRY: Dict[str, Type[OPItemField]] = {}
 
     @classmethod
     def register_op_field_type(cls, item_class):
         cls._TYPE_REGISTRY[item_class.FIELD_TYPE] = item_class
 
     @classmethod
-    def field_type_lookup(cls, field_dict):
+    def field_type_lookup(cls, field_dict: Dict[str, Any]):
         item_type = field_dict["type"]
-        field_class = None
+        field_class = OPItemField
         try:
             field_class = cls._TYPE_REGISTRY[item_type]
         except KeyError:
-            field_class = OPItemField
+            pass
         return field_class
 
     @classmethod
-    def item_field(cls, field_dict: Dict, *args):
+    def item_field(cls, field_dict: Dict[str, Any], *args):
         item_cls = cls.field_type_lookup(field_dict)
 
         obj = item_cls(field_dict)

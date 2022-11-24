f07cc0da291e49a285d98646d8767f372be7e763
Zachary Cutlip <uid000@gmail.com>
2022-11-23 18:49:16 -0800

    properly export all API types

--- pyonepassword/api/authentication.py
+++ pyonepassword/api/authentication.py
@@ -4,3 +4,13 @@ from .._py_op_commands import (
     EXISTING_AUTH_REQD,
     ExistingAuthEnum
 )
+
+# This causes these types to properly re-exported
+# https://mypy.readthedocs.io/en/stable/config_file.html?highlight=export#confval-implicit_reexport
+# anything that gets imported needs to be added to this list
+__all__ = [
+    "EXISTING_AUTH_AVAIL",
+    "EXISTING_AUTH_IGNORE",
+    "EXISTING_AUTH_REQD",
+    "ExistingAuthEnum"
+]

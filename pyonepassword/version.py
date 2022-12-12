fe4958cf0ea6f2d056022cd6ff8e4dbfe7ad659a
Zachary Cutlip <uid000@gmail.com>
2022-12-11 21:15:31 -0800

    rename PyonepasswordAbout
    - PyOPAbout
    - add instance variables

--- pyonepassword/version.py
+++ pyonepassword/version.py
@@ -1,7 +1,11 @@
 from . import __summary__, __title__, __version__
 
 
-class PyonepasswordAbout:
+class PyOPAbout:
+    def __init__(self) -> None:
+        self.version = __version__
+        self.summary = __summary__
+        self.title = __title__
 
     def __str__(self):
-        return "%s %s version %s" % (__title__, __summary__, __version__)
+        return f"{self.title.upper()}: {self.summary}. Version {self.version}"

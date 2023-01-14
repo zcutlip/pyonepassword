a360c8f67d20075fcd781d52af3d96beaca4ff3b
Zachary Cutlip <uid000@gmail.com>
2023-01-13 19:57:37 -0800

    allow OPItemList to be constructed from a JSON string or a list object

--- pyonepassword/op_items/_item_list.py
+++ pyonepassword/op_items/_item_list.py
@@ -1,12 +1,13 @@
 from typing import List, Union
 
 from ..json import safe_unjson
+from ..json import safe_unjson
 from ._item_descriptor_base import OPAbstractItemDescriptor
 from ._item_descriptor_registry import OPItemDescriptorFactory
 
 
 class OPItemList(List[OPAbstractItemDescriptor]):
-    def __init__(self, item_list_json: Union[str, List], generic_okay=False):
+    def __init__(self, item_list_json: Union[Union[str, List], List], generic_okay=False):
         super().__init__()
         item_list = safe_unjson(item_list_json)
         for i_dict in item_list:

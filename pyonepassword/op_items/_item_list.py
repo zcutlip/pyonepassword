e52f5986e744724e8b6be2cb69e44702e47663ff
Zachary Cutlip <uid000@gmail.com>
2023-01-26 19:01:51 -0800

    add "generic item" support to OPItemList

--- pyonepassword/op_items/_item_list.py
+++ pyonepassword/op_items/_item_list.py
@@ -1,14 +1,15 @@
 import json
-from typing import List
+from typing import List, Union
 
 from ._item_descriptor_base import OPAbstractItemDescriptor
 from ._item_descriptor_registry import OPItemDescriptorFactory
 
 
 class OPItemList(List[OPAbstractItemDescriptor]):
-    def __init__(self, item_list_json: str):
+    def __init__(self, item_list_json: Union[str, List], generic_okay=False):
         super().__init__()
         item_list = json.loads(item_list_json)
         for i_dict in item_list:
-            descriptor = OPItemDescriptorFactory.item_descriptor(i_dict)
+            descriptor = OPItemDescriptorFactory.item_descriptor(
+                i_dict, generic_item=generic_okay)
             self.append(descriptor)

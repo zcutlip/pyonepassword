752c7f17b861b25aae8e74e20ef3595bda19c02b
Zachary Cutlip <uid000@gmail.com>
2023-02-14 17:40:08 -0800

    unserialize item list with safe_unjson()

--- pyonepassword/op_items/_item_list.py
+++ pyonepassword/op_items/_item_list.py
@@ -1,6 +1,6 @@
-import json
 from typing import List, Union
 
+from ..json import safe_unjson
 from ._item_descriptor_base import OPAbstractItemDescriptor
 from ._item_descriptor_registry import OPItemDescriptorFactory
 
@@ -8,7 +8,7 @@ from ._item_descriptor_registry import OPItemDescriptorFactory
 class OPItemList(List[OPAbstractItemDescriptor]):
     def __init__(self, item_list_json: Union[str, List], generic_okay=False):
         super().__init__()
-        item_list = json.loads(item_list_json)
+        item_list = safe_unjson(item_list_json)
         for i_dict in item_list:
             descriptor = OPItemDescriptorFactory.item_descriptor(
                 i_dict, generic_okay=generic_okay)

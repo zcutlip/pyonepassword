487c092cc1afe99369ab5a9749cd9f32d647695a
Zachary Cutlip <uid000@gmail.com>
2023-03-10 15:14:08 -0800

    add additional item-get-database tests
    - example-database-missing-fields

--- tests/test_item_get/test_item_get_database.py
+++ tests/test_item_get/test_item_get_database.py
@@ -403,3 +403,172 @@ def test_item_get_database_24(signed_in_op: OP, expected_database_data: Expected
     result: OPDatabaseItem = signed_in_op.item_get(item_name, vault=vault)
     assert isinstance(result, OPDatabaseItem)
     assert result.connection_options is None
+
+
+# For the following tests, the database item
+# was created from a template with most fields missing
+# as a result the resulting item has most fields missing
+# these tests verify the field values returned are None
+def test_item_get_database_25(signed_in_op: OP):
+    """
+    call OP.item_get() to get a database item
+
+    Verify:
+        username is None
+    """
+    item_name = "Example Database Missing Fields"
+    vault = "Test Data"
+
+    result = signed_in_op.item_get(item_name, vault=vault)
+    assert isinstance(result, OPDatabaseItem)
+    assert result.username is None
+
+
+def test_item_get_database_26(signed_in_op: OP):
+    """
+    call OP.item_get() to get a database item
+
+    Verify:
+        password is None
+    """
+    item_name = "Example Database Missing Fields"
+    vault = "Test Data"
+    result = signed_in_op.item_get(item_name, vault=vault)
+    assert isinstance(result, OPDatabaseItem)
+    assert result.password is None
+
+
+def test_item_get_database_27(signed_in_op: OP, expected_database_data: ExpectedDatabaseItemData):
+    """
+    call OP.item_get() to get a database item
+
+    Verify:
+        expected database_type matches actual database_type
+    """
+    item_name = "Example Database Missing Fields"
+    vault = "Test Data"
+    expected: ExpectedDatabaseItem = expected_database_data.data_for_database(
+        item_name)
+    result = signed_in_op.item_get(item_name, vault=vault)
+    assert isinstance(result, OPDatabaseItem)
+    assert result.database_type
+    assert result.database_type == expected.database_type
+
+
+def test_item_get_database_28(signed_in_op):
+    """
+    call OP.item_get() to get a database item
+
+    Verify:
+        hostname is None
+    """
+    item_name = "Example Database Missing Fields"
+    vault = "Test Data"
+
+    result: OPDatabaseItem = signed_in_op.item_get(item_name, vault=vault)
+    assert isinstance(result, OPDatabaseItem)
+    assert result.hostname is None
+
+
+def test_item_get_database_29(signed_in_op: OP):
+    """
+    call OP.item_get() to get a database item
+
+    Verify:
+        convenience property "server" returns None
+    """
+    item_name = "Example Database Missing Fields"
+    vault = "Test Data"
+    result: OPDatabaseItem = signed_in_op.item_get(item_name, vault=vault)
+    assert isinstance(result, OPDatabaseItem)
+    assert result.server is None
+
+
+def test_item_get_database_30(signed_in_op: OP):
+    """
+    call OP.item_get() to get a database item
+
+    Verify:
+        port is None
+    """
+    item_name = "Example Database Missing Fields"
+    vault = "Test Data"
+
+    result: OPDatabaseItem = signed_in_op.item_get(item_name, vault=vault)
+    assert isinstance(result, OPDatabaseItem)
+    assert result.port is None
+
+
+def test_item_get_database_31(signed_in_op: OP):
+    """
+    call OP.item_get() to get a database item
+
+    Verify:
+        "database" property returns None
+    """
+    item_name = "Example Database Missing Fields"
+    vault = "Test Data"
+
+    result: OPDatabaseItem = signed_in_op.item_get(item_name, vault=vault)
+    assert isinstance(result, OPDatabaseItem)
+    assert result.database is None
+
+
+def test_item_get_database_32(signed_in_op: OP):
+    """
+    call OP.item_get() to get a database item
+
+    Verify:
+        sid property returns None
+    """
+    item_name = "Example Database Missing Fields"
+    vault = "Test Data"
+
+    result: OPDatabaseItem = signed_in_op.item_get(item_name, vault=vault)
+    assert isinstance(result, OPDatabaseItem)
+    assert result.sid is None
+
+
+def test_item_get_database_33(signed_in_op: OP):
+    """
+    call OP.item_get() to get a database item
+
+    Verify:
+        alias property returns None
+    """
+    item_name = "Example Database Missing Fields"
+    vault = "Test Data"
+
+    result: OPDatabaseItem = signed_in_op.item_get(item_name, vault=vault)
+    assert isinstance(result, OPDatabaseItem)
+    assert result.alias is None
+
+
+def test_item_get_database_34(signed_in_op: OP):
+    """
+    call OP.item_get() to get a database item
+
+    Verify:
+        options property returns None
+    """
+    item_name = "Example Database Missing Fields"
+    vault = "Test Data"
+
+    result: OPDatabaseItem = signed_in_op.item_get(item_name, vault=vault)
+    assert isinstance(result, OPDatabaseItem)
+    assert result.options is None
+
+
+def test_item_get_database_35(signed_in_op: OP):
+    """
+    call OP.item_get() to get a database item
+
+    Verify:
+        convenience property "connection_options" returns None
+    """
+    item_name = "Example Database Missing Fields"
+    vault = "Test Data"
+
+    result: OPDatabaseItem = signed_in_op.item_get(item_name, vault=vault)
+    assert isinstance(result, OPDatabaseItem)
+    assert result.connection_options is None

faa6d517a36a1d36537bc5264d2db8c2c7b0241d
Zachary Cutlip <uid000@gmail.com>
2023-03-31 19:12:57 -0700

    split test_item_password test into multiple tests

--- tests/test_item_types/test_item_password.py
+++ tests/test_item_types/test_item_password.py
@@ -5,7 +5,7 @@ from typing import TYPE_CHECKING
 import pytest
 
 if TYPE_CHECKING:
-    from pyonepassword import OP
+    from ..fixtures.valid_data import ValidData
 
 from pyonepassword.api.object_types import OPPasswordItem
 
@@ -18,18 +18,78 @@ def _lookup_password_data(data, password_identifier: str):
     return item
 
 
-def test_item_password_01(signed_in_op: OP, expected_item_password_data):
+def test_item_password_010(valid_data: ValidData, expected_item_password_data):
+    item_dict = valid_data.data_for_name("example-password")
     password_identifier = "Example Password"
-    vault = "Test Data"
+
     expected = _lookup_password_data(
         expected_item_password_data, password_identifier)
-    result: OPPasswordItem = signed_in_op.item_get(
-        password_identifier, vault=vault)
-    assert isinstance(result, OPPasswordItem)
+    result = OPPasswordItem(item_dict)
+
     assert result.password == expected.password
+
+
+def test_item_password_020(valid_data: ValidData, expected_item_password_data):
+    item_dict = valid_data.data_for_name("example-password")
+    password_identifier = "Example Password"
+
+    expected = _lookup_password_data(
+        expected_item_password_data, password_identifier)
+    result = OPPasswordItem(item_dict)
+
     assert result.unique_id == expected.unique_id
+
+
+def test_item_password_030(valid_data: ValidData, expected_item_password_data):
+    item_dict = valid_data.data_for_name("example-password")
+    password_identifier = "Example Password"
+
+    expected = _lookup_password_data(
+        expected_item_password_data, password_identifier)
+    result = OPPasswordItem(item_dict)
+
     assert result.title == expected.title
+
+
+def test_item_password_040(valid_data: ValidData, expected_item_password_data):
+    item_dict = valid_data.data_for_name("example-password")
+    password_identifier = "Example Password"
+
+    expected = _lookup_password_data(
+        expected_item_password_data, password_identifier)
+    result = OPPasswordItem(item_dict)
+
     assert result.created_at == expected.created_at
+
+
+def test_item_password_050(valid_data: ValidData, expected_item_password_data):
+    item_dict = valid_data.data_for_name("example-password")
+    password_identifier = "Example Password"
+
+    expected = _lookup_password_data(
+        expected_item_password_data, password_identifier)
+    result = OPPasswordItem(item_dict)
+
     assert result.updated_at == expected.updated_at
+
+
+def test_item_password_060(valid_data: ValidData, expected_item_password_data):
+    item_dict = valid_data.data_for_name("example-password")
+    password_identifier = "Example Password"
+
+    expected = _lookup_password_data(
+        expected_item_password_data, password_identifier)
+    result = OPPasswordItem(item_dict)
+
     assert result.last_edited_by == expected.last_edited_by
+
+
+def test_item_password_070(valid_data: ValidData, expected_item_password_data):
+    item_dict = valid_data.data_for_name("example-password")
+    password_identifier = "Example Password"
+
+    expected = _lookup_password_data(
+        expected_item_password_data, password_identifier)
+    result = OPPasswordItem(item_dict)
+
     assert result.vault_id == expected.vault_id

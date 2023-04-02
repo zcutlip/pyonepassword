b5d0548c16e1e7f11d2df943b40d27a88621f8f7
Zachary Cutlip <uid000@gmail.com>
2023-04-02 14:53:13 -0700

    move test_item_get_server.py
    - under test_item_types
    - rename to test_serve_item.py
    - delete tests related to SSH keys

--- tests/test_item_types/test_server_item.py
+++ tests/test_item_types/test_server_item.py
@@ -6,25 +6,17 @@ from typing import TYPE_CHECKING
 
 import pytest
 
-from ..test_support.util import digest
-
 # make imports for type-hinting disappear at run-time to avoid
 # circular imports.
 # this also reduced exercising tested code simply by importing
 if TYPE_CHECKING:
-    from ..fixtures.expected_server import ExpectedServer, ExpectedServerSSHKeys, ExpectedServerItemData
+    from ..fixtures.expected_server import ExpectedServer, ExpectedServerItemData
     from pyonepassword import OP
 
 from pyonepassword.api.object_types import OPServerItem
 
 # ensure HOME env variable is set, and there's a valid op config present
 pytestmark = pytest.mark.usefixtures("valid_op_cli_config_homedir")
-
-
-def _lookup_ssh_key_data(expected_server_data, server_name, keyname) -> ExpectedServerSSHKeys:
-    expected = expected_server_data.data_for_server(server_name)
-    key_data = expected.ssh_keys_for_id(keyname)
-    return key_data
 
 
 def test_admin_user_01(signed_in_op: OP, expected_server_data: ExpectedServerItemData):
@@ -68,64 +60,6 @@ def test_server_url_02(signed_in_op: OP, expected_server_data: ExpectedServerIte
     result = signed_in_op.item_get(server_id)
     assert isinstance(result, OPServerItem)
     assert result.url == expected.url
-
-
-def test_ssh_key_passphrase_01(signed_in_op: OP, expected_server_data: ExpectedServerItemData):
-    server_name = "Example Server"
-    vault = "Test Data"
-    keyname = "id_ed25519"
-    passphrase_field = f"{keyname} passphrase"
-
-    expected_key_data = _lookup_ssh_key_data(
-        expected_server_data, server_name, keyname)
-
-    server_item: OPServerItem = signed_in_op.item_get(server_name, vault=vault)
-
-    passphrase = server_item.field_value_by_section_title(
-        "SSH Keys", passphrase_field)
-    assert passphrase == expected_key_data.passphrase
-
-
-def test_ssh_priv_key_01(signed_in_op: OP, expected_server_data: ExpectedServerItemData):
-    server_name = "Example Server"
-    vault = "Test Data"
-    keyname = "id_ed25519"
-    expected_key_data = _lookup_ssh_key_data(
-        expected_server_data, server_name, keyname)
-    server_item: OPServerItem = signed_in_op.item_get(server_name, vault=vault)
-
-    priv_key = server_item.field_value_by_section_title(
-        "SSH Keys", keyname)
-    priv_key_digest = digest(priv_key.encode("utf-8"))
-    assert priv_key_digest == expected_key_data.pivkey_digest
-
-
-def test_ssh_pub_key_01(signed_in_op: OP, expected_server_data: ExpectedServerItemData):
-    server_name = "Example Server"
-    vault = "Test Data"
-    keyname = "id_ed25519"
-    pub_keyname = f"{keyname}.pub"
-    expected_key_data = _lookup_ssh_key_data(
-        expected_server_data, server_name, keyname)
-    server_item: OPServerItem = signed_in_op.item_get(server_name, vault=vault)
-
-    pub_key = server_item.field_value_by_section_title(
-        "SSH Keys", pub_keyname)
-    pub_key_digest = digest(pub_key.encode("utf-8"))
-    assert pub_key_digest == expected_key_data.pubkey_digest
-
-
-def test_server_get_by_id_01(signed_in_op: OP, expected_server_data: ExpectedServerItemData):
-    expected: ExpectedServer
-    result: OPServerItem
-    server_id = "3wcd3zsps7fvij47fef6scznxq"
-
-    expected = expected_server_data.data_for_server(server_id)
-
-    result = signed_in_op.item_get(server_id)
-
-    assert isinstance(result, OPServerItem)
-    assert result.title == expected.title
 
 
 def test_server_admin_console_01(signed_in_op: OP, expected_server_data: ExpectedServerItemData):

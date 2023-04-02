7b92ef87bf2130123b3ed1047b3a1676ce162498
Zachary Cutlip <uid000@gmail.com>
2023-04-01 22:30:16 -0700

    refactor test_ssh_key_item
    - create OPSSHKeyItem from valid data rather than calling op.item_get()

--- tests/test_item_types/test_ssh_key_item.py
+++ tests/test_item_types/test_ssh_key_item.py
@@ -4,148 +4,119 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
-import pytest
-
 # make imports for type-hinting disappear at run-time to avoid
 # circular imports.
 # this also reduced exercising tested code simply by importing
 if TYPE_CHECKING:
-    from ..fixtures.expected_ssh_key_data import ExpectedSSHKey, ExpectedSSHKeyData
-    from pyonepassword import OP
+    from ..fixtures.expected_ssh_key_data import ExpectedSSHKeyData
+    from ..fixtures.valid_data import ValidData
 
 from pyonepassword.api.object_types import OPSSHKeyItem
 
-# ensure HOME env variable is set, and there's a valid op config present
-pytestmark = pytest.mark.usefixtures("valid_op_cli_config_homedir")
 
-
-def test_item_get_ssh_key_01(signed_in_op: OP, expected_ssh_key_data: ExpectedSSHKeyData):
+def test_item_get_ssh_key_010(valid_data: ValidData, expected_ssh_key_data: ExpectedSSHKeyData):
     item_name = "Example SSH Key"
-    expected: ExpectedSSHKey
-    result: OPSSHKeyItem
+    item_dict = valid_data.data_for_name("example-ssh-key")
 
     expected = expected_ssh_key_data.data_for_ssh_key(item_name)
-    result = signed_in_op.item_get(item_name)
-    assert isinstance(result, OPSSHKeyItem)
+    result = OPSSHKeyItem(item_dict)
     assert result.unique_id == expected.unique_id
 
 
-def test_item_get_ssh_key_02(signed_in_op: OP, expected_ssh_key_data: ExpectedSSHKeyData):
+def test_item_get_ssh_key_020(valid_data: ValidData, expected_ssh_key_data: ExpectedSSHKeyData):
     item_name = "Example SSH Key"
-    expected: ExpectedSSHKey
-    result: OPSSHKeyItem
+    item_dict = valid_data.data_for_name("example-ssh-key")
 
     expected = expected_ssh_key_data.data_for_ssh_key(item_name)
-    result = signed_in_op.item_get(item_name)
-    assert isinstance(result, OPSSHKeyItem)
+    result = OPSSHKeyItem(item_dict)
     assert result.title == expected.title
 
 
-def test_item_get_ssh_key_03(signed_in_op: OP, expected_ssh_key_data: ExpectedSSHKeyData):
+def test_item_get_ssh_key_030(valid_data: ValidData, expected_ssh_key_data: ExpectedSSHKeyData):
     item_name = "Example SSH Key"
-    expected: ExpectedSSHKey
-    result: OPSSHKeyItem
+    item_dict = valid_data.data_for_name("example-ssh-key")
 
     expected = expected_ssh_key_data.data_for_ssh_key(item_name)
-    result = signed_in_op.item_get(item_name)
-    assert isinstance(result, OPSSHKeyItem)
+    result = OPSSHKeyItem(item_dict)
     assert result.category == expected.category
 
 
-def test_item_get_ssh_key_04(signed_in_op: OP, expected_ssh_key_data: ExpectedSSHKeyData):
+def test_item_get_ssh_key_040(valid_data: ValidData, expected_ssh_key_data: ExpectedSSHKeyData):
     item_name = "Example SSH Key"
-    expected: ExpectedSSHKey
-    result: OPSSHKeyItem
+    item_dict = valid_data.data_for_name("example-ssh-key")
 
     expected = expected_ssh_key_data.data_for_ssh_key(item_name)
-    result = signed_in_op.item_get(item_name)
-    assert isinstance(result, OPSSHKeyItem)
+    result = OPSSHKeyItem(item_dict)
     assert result.last_edited_by == expected.last_edited_by
 
 
-def test_item_get_ssh_key_05(signed_in_op: OP, expected_ssh_key_data: ExpectedSSHKeyData):
+def test_item_get_ssh_key_050(valid_data: ValidData, expected_ssh_key_data: ExpectedSSHKeyData):
     item_name = "Example SSH Key"
-    expected: ExpectedSSHKey
-    result: OPSSHKeyItem
+    item_dict = valid_data.data_for_name("example-ssh-key")
 
     expected = expected_ssh_key_data.data_for_ssh_key(item_name)
-    result = signed_in_op.item_get(item_name)
-    assert isinstance(result, OPSSHKeyItem)
+    result = OPSSHKeyItem(item_dict)
     assert result.public_key == expected.public_key
 
 
-def test_item_get_ssh_key_06(signed_in_op: OP, expected_ssh_key_data: ExpectedSSHKeyData):
+def test_item_get_ssh_key_060(valid_data: ValidData, expected_ssh_key_data: ExpectedSSHKeyData):
     item_name = "Example SSH Key"
-    expected: ExpectedSSHKey
-    result: OPSSHKeyItem
+    item_dict = valid_data.data_for_name("example-ssh-key")
 
     expected = expected_ssh_key_data.data_for_ssh_key(item_name)
-    result = signed_in_op.item_get(item_name)
-    assert isinstance(result, OPSSHKeyItem)
+    result = OPSSHKeyItem(item_dict)
     assert result.public_key_reference == expected.public_key_ref
 
 
-def test_item_get_ssh_key_07(signed_in_op: OP, expected_ssh_key_data: ExpectedSSHKeyData):
+def test_item_get_ssh_key_070(valid_data: ValidData, expected_ssh_key_data: ExpectedSSHKeyData):
     item_name = "Example SSH Key"
-    expected: ExpectedSSHKey
-    result: OPSSHKeyItem
+    item_dict = valid_data.data_for_name("example-ssh-key")
 
     expected = expected_ssh_key_data.data_for_ssh_key(item_name)
-    result = signed_in_op.item_get(item_name)
-    assert isinstance(result, OPSSHKeyItem)
+    result = OPSSHKeyItem(item_dict)
     assert result.fingerprint == expected.fingerprint
 
 
-def test_item_get_ssh_key_08(signed_in_op: OP, expected_ssh_key_data: ExpectedSSHKeyData):
+def test_item_get_ssh_key_080(valid_data: ValidData, expected_ssh_key_data: ExpectedSSHKeyData):
     item_name = "Example SSH Key"
-    expected: ExpectedSSHKey
-    result: OPSSHKeyItem
+    item_dict = valid_data.data_for_name("example-ssh-key")
 
     expected = expected_ssh_key_data.data_for_ssh_key(item_name)
-    result = signed_in_op.item_get(item_name)
-    assert isinstance(result, OPSSHKeyItem)
+    result = OPSSHKeyItem(item_dict)
     assert result.fingerprint_reference == expected.fingerprint_ref
 
 
-def test_item_get_ssh_key_09(signed_in_op: OP, expected_ssh_key_data: ExpectedSSHKeyData):
+def test_item_get_ssh_key_090(valid_data: ValidData, expected_ssh_key_data: ExpectedSSHKeyData):
     item_name = "Example SSH Key"
-    expected: ExpectedSSHKey
-    result: OPSSHKeyItem
+    item_dict = valid_data.data_for_name("example-ssh-key")
 
     expected = expected_ssh_key_data.data_for_ssh_key(item_name)
-    result = signed_in_op.item_get(item_name)
-    assert isinstance(result, OPSSHKeyItem)
+    result = OPSSHKeyItem(item_dict)
     assert result.private_key == expected.private_key
 
 
-def test_item_get_ssh_key_10(signed_in_op: OP, expected_ssh_key_data: ExpectedSSHKeyData):
+def test_item_get_ssh_key_100(valid_data: ValidData, expected_ssh_key_data: ExpectedSSHKeyData):
     item_name = "Example SSH Key"
-    expected: ExpectedSSHKey
-    result: OPSSHKeyItem
+    item_dict = valid_data.data_for_name("example-ssh-key")
 
     expected = expected_ssh_key_data.data_for_ssh_key(item_name)
-    result = signed_in_op.item_get(item_name)
-    assert isinstance(result, OPSSHKeyItem)
+    result = OPSSHKeyItem(item_dict)
     assert result.private_key_reference == expected.private_key_ref
 
 
-def test_item_get_ssh_key_11(signed_in_op: OP, expected_ssh_key_data: ExpectedSSHKeyData):
+def test_item_get_ssh_key_110(valid_data: ValidData, expected_ssh_key_data: ExpectedSSHKeyData):
     item_name = "Example SSH Key"
-    expected: ExpectedSSHKey
-    result: OPSSHKeyItem
+    item_dict = valid_data.data_for_name("example-ssh-key")
 
     expected = expected_ssh_key_data.data_for_ssh_key(item_name)
-    result = signed_in_op.item_get(item_name)
-    assert isinstance(result, OPSSHKeyItem)
+    result = OPSSHKeyItem(item_dict)
     assert result.key_type == expected.key_type
 
 
-def test_item_get_ssh_key_12(signed_in_op: OP, expected_ssh_key_data: ExpectedSSHKeyData):
+def test_item_get_ssh_key_120(valid_data: ValidData, expected_ssh_key_data: ExpectedSSHKeyData):
     item_name = "Example SSH Key"
-    expected: ExpectedSSHKey
-    result: OPSSHKeyItem
+    item_dict = valid_data.data_for_name("example-ssh-key")
 
     expected = expected_ssh_key_data.data_for_ssh_key(item_name)
-    result = signed_in_op.item_get(item_name)
-    assert isinstance(result, OPSSHKeyItem)
+    result = OPSSHKeyItem(item_dict)
     assert result.key_type_reference == expected.key_type_ref

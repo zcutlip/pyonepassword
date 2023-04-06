11ef28a07042c2df7c495f59c32b2f9b19471223
Zachary Cutlip <uid000@gmail.com>
2023-04-06 15:46:29 -0700

    gh-115: add additional convenience properties to server item
     all fields under "hosting provider" section

--- pyonepassword/op_items/item_types/server.py
+++ pyonepassword/op_items/item_types/server.py
@@ -1,4 +1,4 @@
-from typing import Union
+from typing import Optional, Union
 
 from .._item_descriptor_registry import op_register_item_descriptor_type
 from .._item_type_registry import op_register_item_type
@@ -56,6 +56,60 @@ class OPServerItem(OPAbstractItem):
         url = self.field_value_by_section_title(
             "Admin Console", "admin console URL")
         return url
+
+    @property
+    def hosting_provider_name(self) -> Optional[str]:
+        """
+        Convenience property to retrieve the hosting provider name
+
+        Returns
+        -------
+        str
+            name of the hosting provider
+        """
+        # Note: the actual field ID and label are "name"
+        # so we look it up by section to be sure
+        return self.field_value_by_section_title("Hosting Provider", "name")
+
+    @property
+    def hosting_provider_website(self) -> Optional[str]:
+        """
+        Convenience property to retrieve the hosting provider website
+
+        Returns
+        -------
+        str
+            Value of the hosting provider website field
+        """
+        # Note: the actual field ID and label are "name"
+        # so we look it up by section to be sure
+        return self.field_value_by_section_title("Hosting Provider", "website")
+
+    @property
+    def support_contact_url(self) -> Optional[str]:
+        """
+        Convenience property to retrieve the hosting provider support URL
+
+        Returns
+        -------
+        str
+            Value of the hosting provider support URL field
+        """
+        url = self.field_value_by_id("support_contact_url")
+        return url
+
+    @property
+    def support_contact_phone(self) -> Optional[str]:
+        """
+        Convenience property to retrieve the hosting provider support phone
+
+        Returns
+        -------
+        str
+            Value of the hosting provider support phone field
+        """
+        phone = self.field_value_by_id("support_contact_phone")
+        return phone
 
 
 @op_register_item_type

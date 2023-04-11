a3ea742e443ab43f0340fa2ba0fe4c7e2249fef7
Zachary Cutlip <uid000@gmail.com>
2023-04-10 19:32:59 -0700

    add a test_group_get_01 back to test_group_get

--- tests/test_op_api/test_group_get.py
+++ tests/test_op_api/test_group_get.py
@@ -8,9 +8,18 @@ if TYPE_CHECKING:
     from pyonepassword import OP
 
 from pyonepassword.api.exceptions import OPGroupGetException
+from pyonepassword.api.object_types import OPGroup
 
 # ensure HOME env variable is set, and there's a valid op config present
 pytestmark = pytest.mark.usefixtures("valid_op_cli_config_homedir")
+
+
+def test_group_get_01(signed_in_op: OP, expected_group_data):
+    group_identifier = "Team Members"
+    expected = expected_group_data.data_for_group(group_identifier)
+    result = signed_in_op.group_get(group_identifier)
+    assert isinstance(result, OPGroup)
+    assert result.unique_id == expected.unique_id
 
 
 def test_group_get_invalid_01(signed_in_op: OP, expected_group_data):

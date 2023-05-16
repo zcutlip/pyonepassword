bfebc18080f4fbf10be5ada5312579d675a4e2da
Zachary Cutlip <uid000@gmail.com>
2023-05-16 13:55:02 -0700

    add SVC_ACCT_RESP_DIRECTORY_PATH to test fixture paths

--- tests/fixtures/paths.py
+++ tests/fixtures/paths.py
@@ -18,6 +18,11 @@ ITEM_DELETE_MULTIPLE_TITLE_GLOB_STATE_CONFIG_PATH = Path(
 
 UNAUTH_RESP_DIRECTORY_PATH = Path(
     MOCK_OP_CONFIG_PATH, "unauth-response-directory.json")
+
+SVC_ACCT_RESP_DIRECTORY_PATH = Path(
+    MOCK_OP_CONFIG_PATH, "svc-acct-response-directory.json"
+)
+
 VALID_DATA_REGISTRY_PATH = Path(
     TEST_INPUT_DATA_PATH, "valid-data-registry.json")
 VALID_DATA_PATH = Path(TEST_INPUT_DATA_PATH, "valid-data")

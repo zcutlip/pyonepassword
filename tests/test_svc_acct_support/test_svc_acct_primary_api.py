082fd4b4414629b16ad9b121c5efd404f1d63326
Zachary Cutlip <uid000@gmail.com>
2023-05-17 08:44:28 -0700

    add svc account tests for commands with prohibited options

--- tests/test_svc_acct_support/test_svc_acct_primary_api.py
+++ tests/test_svc_acct_support/test_svc_acct_primary_api.py
@@ -13,6 +13,7 @@ import pytest
 if TYPE_CHECKING:
     from pyonepassword import OP
 
+from pyonepassword.api.descriptor_types import OPVaultDescriptorList
 from pyonepassword.api.exceptions import (
     OPSvcAccountCommandNotSupportedException
 )
@@ -52,3 +53,31 @@ def test_svc_acct_item_get_02(signed_in_op: OP):
     item_name = "Example Login 1"
     with pytest.raises(OPSvcAccountCommandNotSupportedException):
         signed_in_op.item_get(item_name)
+
+
+@pytest.mark.usefixtures("valid_op_cli_config_homedir")
+@pytest.mark.usefixtures("setup_svc_acct_env")
+def test_svc_acct_item_get_03(signed_in_op: OP):
+    """
+    """
+    result = signed_in_op.vault_list()
+    assert isinstance(result, OPVaultDescriptorList)
+
+
+@pytest.mark.usefixtures("valid_op_cli_config_homedir")
+@pytest.mark.usefixtures("setup_svc_acct_env")
+def test_svc_acct_item_get_04(signed_in_op: OP):
+    """
+    Service account command test: "op vault list --group 'Team Members'"
+
+    This excercises the supported commands with prohibited arguments code paths
+
+    Set service account token via fixture
+    call OP.vault_list() with the prohibited group argument
+
+    Verify the call fails with OPSvcAccountCommandNotSupportedException
+    """
+    group = "Team Members"
+
+    with pytest.raises(OPSvcAccountCommandNotSupportedException):
+        signed_in_op.vault_list(group_name_or_id=group)

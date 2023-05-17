2defc9744a5c013964adfbf9256982ec93756dea
Zachary Cutlip <uid000@gmail.com>
2023-05-16 20:07:03 -0700

    add data_location_as_path() convenience function

--- pyonepassword/pkg_resources.py
+++ pyonepassword/pkg_resources.py
@@ -1,4 +1,21 @@
+from pathlib import Path
+
 try:
     from importlib.resources import files as pkgfiles  # noqa: F401
 except ImportError:  # pragma: no coverage
     from importlib_resources import files as pkgfiles  # noqa: F401
+
+
+def data_location_as_path(package, sub_path) -> Path:
+    """
+    Convenience function to get a Path object from a package and a subpath
+    within the package
+    """
+    # joinpath() returns a Traversable
+    # to turn it into Path we first turn it into a string
+    if not isinstance(sub_path, str):
+        sub_path = str(sub_path)
+    _data_path = pkgfiles(package).joinpath(sub_path)
+    _data_path_str = str(_data_path)
+    data_path = Path(_data_path_str)
+    return data_path

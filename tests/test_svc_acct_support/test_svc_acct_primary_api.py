85890fbfa63cf72169f2507c097efaca34e89ade
Zachary Cutlip <uid000@gmail.com>
2023-05-22 14:39:11 -0700

    test case to ensure all primary API methods have service account documentation

--- tests/test_svc_acct_support/test_svc_acct_primary_api.py
+++ tests/test_svc_acct_support/test_svc_acct_primary_api.py
@@ -6,13 +6,13 @@ and some prohibit specific options.
 """
 from __future__ import annotations
 
-from typing import TYPE_CHECKING
+# inspect, re required for checking docstrings
+import inspect
+import re
 
 import pytest
 
-if TYPE_CHECKING:
-    from pyonepassword import OP
-
+from pyonepassword import OP
 from pyonepassword.api.descriptor_types import OPVaultDescriptorList
 from pyonepassword.api.exceptions import (
     OPSvcAccountCommandNotSupportedException
@@ -83,3 +83,41 @@ def test_svc_acct_item_get_04(signed_in_op: OP):
 
     with pytest.raises(OPSvcAccountCommandNotSupportedException):
         signed_in_op.vault_list(group_name_or_id=group)
+
+
+def test_svc_account_primary_api_docstrings():
+    """
+    Inspect each non-private instance or class method in the OP class
+
+    Verify:
+        Each method's docstring contains at least the following section documenting service accounts:
+        Service Account Support
+        -----------------------
+    """
+    # regex where '.' will match newlines
+    svc_account_string_re = re.compile(
+        r"Service Account Support.\s+-----------------------", re.DOTALL)
+
+    # get all class and instance methods
+    api_method_dict = {fn_name: attr for fn_name, attr in vars(
+        OP).items() if inspect.isfunction(attr) or isinstance(attr, classmethod)}
+
+    # filter out "private" methods
+    api_method_dict = {fn_name: attr for fn_name,
+                       attr in api_method_dict.items() if not fn_name.startswith("_")}
+
+    failures = []
+    for meth_name, method_obj in api_method_dict.items():
+        matches = re.search(svc_account_string_re, method_obj.__doc__)
+        if not matches:
+            # we didn't find service account support in the docstring
+            failures.append(f"OP.{meth_name}()")
+        else:
+            # we found mention of service account support but it was incomplete or not formatted properly
+            if len(matches[0].splitlines()) != 2:
+                failures.append(f"OP.{meth_name}()")
+
+    # len(failures) should == 0
+    # if not generate failure message from list of failures
+    assert len(
+        failures) == 0, f"API methods without documented service account support: {failures}"

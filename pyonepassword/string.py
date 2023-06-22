4f3664f2a521a160222f2e8b844f8e9171a8aba5
Zachary Cutlip <uid000@gmail.com>
2023-06-22 10:58:27 -0700

    make string unmasking configurable

--- pyonepassword/string.py
+++ pyonepassword/string.py
@@ -6,17 +6,29 @@ class RedactableString(str):
     """
     # how much to leave unmasked at the end
     UNMASK_LEN = 5
+    UNMASK_LEN_MAX_FRACTION = 0.2
+    MIN_STR_LEN = UNMASK_LEN
 
     _orig_string: str
     _redacted_string: str
 
-    def __new__(cls, string):
+    def __new__(cls, string, *args, unmask_len=-1, **kwargs):
+        if len(string) <= cls.MIN_STR_LEN:
+            unmask_len = 0
+        elif unmask_len < 0:
+            unmask_len = cls.UNMASK_LEN
+
+        max_fraction = cls.UNMASK_LEN_MAX_FRACTION * len(string)
+
+        if unmask_len > max_fraction:
+            unmask_len = int(max_fraction)
+
         orig_string = string
         if isinstance(string, cls):
             orig_string = string._orig_string
         obj = str.__new__(cls, orig_string)
         obj._orig_string = orig_string
-        obj._redacted_string = cls._redacted(orig_string)
+        obj._redacted_string = cls._redacted(orig_string, unmask_len)
         return obj
 
     def __str__(self):
@@ -41,23 +53,19 @@ class RedactableString(str):
         return self._orig_string
 
     @classmethod
-    def _redacted(cls, orig_string) -> str:
+    def _redacted(cls, orig_string, unmask_len) -> str:
         # This aims to turn:
         # 5GHHPJK5HZC5BAT7WDUXW57G44 into
         # *********************57G44
 
-        if len(orig_string) <= cls.UNMASK_LEN:
-            # the thing is too short, so replace everything
-            orig_string = "*" * len(orig_string)
-        else:
-            # get the length of the portion we're trying to mask
-            star_count = len(orig_string) - cls.UNMASK_LEN
+        # get the length of the portion we're trying to mask
+        star_count = len(orig_string) - unmask_len
 
-            # make the star mask
-            stars = "*" * star_count
-            # get the bit we want to leave unmasked
-            _last_five = orig_string[-(cls.UNMASK_LEN):]
+        # make the star mask
+        stars = "*" * star_count
+        # get the bit we want to leave unmasked
+        unmasked_end = orig_string[star_count:]
 
-            # join the mask with the end part we're leaving unmasked
-            orig_string = stars + _last_five
-        return orig_string
+        # join the mask with the end part we're leaving unmasked
+        redacted_string = stars + unmasked_end
+        return redacted_string

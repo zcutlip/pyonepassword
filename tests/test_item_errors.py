79910fa8ea90c160244e89f088f74798f492c5e1
Zachary Cutlip <uid000@gmail.com>
2023-08-20 13:47:28 -0700

    add docstrings to test_item_errors

--- tests/test_item_errors.py
+++ tests/test_item_errors.py
@@ -24,18 +24,36 @@ pytestmark = pytest.mark.usefixtures("valid_op_cli_config_homedir")
 
 
 def test_unknown_item_type_01(invalid_data):
+    """
+    Attempt to create an op item with invalid category "UNKNOWN"
+
+    Verify OPUnknownItemtypeException is raised
+    """
     invalid_item_json = invalid_data.data_for_name("invalid-item")
     with pytest.raises(OPUnknownItemTypeException):
         _ = OPItemFactory.op_item(invalid_item_json)
 
 
 def test_malformed_item_json_01(invalid_data):
+    """
+    Attempt to create an op item with malformed/corrupted JSON
+
+    Verify OPInvalidItemException is raised
+    """
     malformed_json = invalid_data.data_for_name("malformed-item-json")
     with pytest.raises(OPInvalidItemException):
         _ = OPItemFactory.op_item(malformed_json)
 
 
 def test_item_field_not_found_01(valid_data: ValidData):
+    """
+    Create:
+        - a valid OPLoginItem object
+
+    Attempt to look up a non-existent field via field_by_id()
+
+    Verify OPFieldNotFoundException is raised
+    """
     item_dict = valid_data.data_for_name("example-login-1")
     result = OPLoginItem(item_dict)
 
@@ -44,6 +62,14 @@ def test_item_field_not_found_01(valid_data: ValidData):
 
 
 def test_item_field_collision_01(invalid_data):
+    """
+    Test item creation with colliding sections
+
+    Create:
+        - op item object from an item dictionary with colliding Field IDs
+    Verify:
+        - OPFieldCollisionException is raised
+    """
     field_collision_json = invalid_data.data_for_name("field-collision")
     with pytest.raises(OPItemFieldCollisionException):
         OPItemFactory.op_item(field_collision_json)

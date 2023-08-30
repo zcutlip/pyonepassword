eaade9e2e7c290de18157cb68bc18b79d848a08b
Zachary Cutlip <uid000@gmail.com>
2023-08-29 19:28:13 -0700

    make fromisoformat_z more robust

--- tests/test_support/_datetime.py
+++ tests/test_support/_datetime.py
@@ -1,3 +1,4 @@
+import re
 from datetime import datetime
 
 """
@@ -13,11 +14,16 @@ def fromisoformat_z(date_string: str) -> datetime:
     And there's no way to do it without an external package like dateutil
     https://bugs.python.org/issue35829
     """
-    if not date_string.endswith('Z'):
+    if date_string.endswith('Z'):
+        _str = date_string.rstrip('Z')
+        _str += "+00:00"
+    elif re.match(r".*[+-]\d{2}:\d{2}$", date_string):
+        # We shouldn't ever get this, but we also shouldn't blow up on it,
+        # so work with it if it happens
+        _str = date_string
+    else:
         # Mirror the ValueError that datetime.fromisoformat() raises
         raise ValueError(
-            f"Invalid Z-terminated isoformat string: '{date_string}'")
-    _str = date_string.rstrip('Z')
-    _str += "+00:00"
+            f"Invalid TZ terminated isoformat string: '{date_string}'")
     datetime_obj = datetime.fromisoformat(_str)
     return datetime_obj

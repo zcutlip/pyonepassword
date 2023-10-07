3e23be8e433e208b074b1282104cfe53c2775049
Zachary Cutlip <uid000@gmail.com>
2023-10-07 13:31:31 -0700

    add test to verify a URL field's type
    - should not change when setting the field's value

--- tests/test_item_edit/test_item_edit_set_field_value.py
+++ tests/test_item_edit/test_item_edit_set_field_value.py
@@ -4,7 +4,11 @@ from typing import TYPE_CHECKING
 
 import pytest
 
-from pyonepassword.api.object_types import OPConcealedField, OPStringField
+from pyonepassword.api.object_types import (
+    OPConcealedField,
+    OPStringField,
+    OPURLField
+)
 from pyonepassword.py_op_exceptions import OPPasswordFieldDowngradeException
 
 if TYPE_CHECKING:
@@ -235,3 +239,55 @@ def test_item_edit_set_url_field_020(signed_in_op: OP):
 
     # ensure the field value for the newly retrieved item matches the intended new field value
     assert item_2_field_value == new_field_value
+
+
+@pytest.mark.usefixtures("setup_stateful_item_edit")
+def test_item_edit_set_url_field_030(signed_in_op: OP):
+    """
+    Test: OP.item_edit_set_url_field()
+        - Retrieve an item via OP.item_get()
+        - Look up the field to be changed based on section and field name
+        - Call item_edit_set_url_field(), saving returned object
+        - Retreive the same item a second time
+    Verify:
+        - The original item field is an instance of OPURLField
+        - The original item field's value is not equal to the desired new value
+        - The newly retrieved item field is an instance of OPURLField
+        - The newly retrieved item field's value is the same as the desired new value
+    """
+
+    item_name = "Example Login Item 13"
+    field_label = "URL Field 01"
+    section_label = "Section 01"
+    new_field_value = "https://new-url.com/login.html"
+    vault = "Test Data 2"
+
+    # stateful response directory
+    # state 1: responses-item-edit/response-directory-1.json
+    item = signed_in_op.item_get(item_name, vault=vault)
+    section = item.first_section_by_label(section_label)
+    field = section.first_field_by_label(field_label)
+
+    # ensure the field is a URL field
+    assert isinstance(field, OPURLField)
+
+    # ensure the original field value is not the value we're trying to set
+    assert field.value != new_field_value
+
+    signed_in_op.item_edit_set_url_field(item_name,
+                                         new_field_value,
+                                         field_label,
+                                         section_label=section_label,
+                                         vault=vault)
+
+    # state changed with item_edit above
+    # state 2: responses-item-edit/response-directory-2.json
+    item = signed_in_op.item_get(item_name, vault=vault)
+    section = item.first_section_by_label(section_label)
+    field = section.first_field_by_label(field_label)
+
+    # ensure the field is still a URL field
+    assert isinstance(field, OPURLField)
+
+    # ensure the field value for the newly retrieved item matches the intended new field value
+    assert field.value == new_field_value

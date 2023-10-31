322c1c04d2d8ea1092dced6fd3b8275802f942ff
Zachary Cutlip <uid000@gmail.com>
2023-10-31 14:13:51 -0700

    disable example test case for deprecating kwargs

--- tests/test_deprecation_warnings.py
+++ tests/test_deprecation_warnings.py
@@ -1,8 +1,7 @@
-import warnings
+# import warnings
 
-import pytest
+# import pytest
 
-from pyonepassword import OP
 
 # disabled but left as example
 # def example_test_import_deprecation_01():
@@ -17,16 +16,16 @@ from pyonepassword import OP
 #             OPNotSignedInException  # noqa: F401
 #         assert warning_list
 
-
-@pytest.mark.usefixtures("valid_op_cli_config_homedir")
-@pytest.mark.usefixtures("setup_op_sess_var_alt_env")
-def test_kwarg_deprecation_01():
-    """
-    Simulate a pyonepassword environment that:
-    - doesn't use biometric
-    - DOES have OP_SESSION_<user uuid> env variable set
-    Check that OP(use_existing_session=True) succeeds
-    """
-    with warnings.catch_warnings(record=True) as warning_list:
-        OP(op_path='mock-op', use_existing_session=True, password_prompt=False)
-        assert warning_list
+# disabled but left as example
+# @pytest.mark.usefixtures("valid_op_cli_config_homedir")
+# @pytest.mark.usefixtures("setup_op_sess_var_alt_env")
+# def example_test_kwarg_deprecation_01():
+#     """
+#     Simulate a pyonepassword environment that:
+#     - doesn't use biometric
+#     - DOES have OP_SESSION_<user uuid> env variable set
+#     Check that OP(use_existing_session=True) succeeds
+#     """
+#     with warnings.catch_warnings(record=True) as warning_list:
+#         OP(op_path='mock-op', use_existing_session=True, password_prompt=False)
+#         assert warning_list

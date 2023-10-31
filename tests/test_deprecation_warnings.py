bf31f030a2bd7b01aeada595c04caeff66dbcaf9
Zachary Cutlip <uid000@gmail.com>
2023-10-31 14:09:55 -0700

    disable test_api_deprecation_01()
    leave commented test case as an example

--- tests/test_deprecation_warnings.py
+++ tests/test_deprecation_warnings.py
@@ -1,17 +1,17 @@
 import warnings
 
+# Commented but left as example
+# def test_api_deprecation_01():
+#     """
+#     Import deprecated OPNotSignedInException class from
+#     pyonepassword.api.exceptions
 
-def test_api_deprecation_01():
-    """
-    Import deprecated OPNotSignedInException class from
-    pyonepassword.api.exceptions
-
-    Ensure there is a deprecation warning
-    """
-    with warnings.catch_warnings(record=True) as warning_list:
-        from pyonepassword.api.exceptions import \
-            OPNotSignedInException  # noqa: F401
-        assert warning_list
+#     Ensure there is a deprecation warning
+#     """
+#     with warnings.catch_warnings(record=True) as warning_list:
+#         from pyonepassword.api.exceptions import \
+#             OPNotSignedInException  # noqa: F401
+#         assert warning_list
 
 
 def test_api_deprecation_02():

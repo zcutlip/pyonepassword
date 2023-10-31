405b6d7ffa7b3778bd9091c52eb302103dba4efc
Zachary Cutlip <uid000@gmail.com>
2023-10-31 14:42:53 -0700

    add test case for deprecated method

--- tests/test_deprecation_warnings.py
+++ tests/test_deprecation_warnings.py
@@ -1,8 +1,12 @@
-# import warnings
+from __future__ import annotations
 
-# import pytest
+import warnings
+from typing import TYPE_CHECKING
 
+from pyonepassword.api.object_types import OPLoginItem
 
+if TYPE_CHECKING:
+    from .fixtures.valid_data import ValidData
 # disabled but left as example
 # def example_test_import_deprecation_01():
 #     """
@@ -29,3 +33,23 @@
 #     with warnings.catch_warnings(record=True) as warning_list:
 #         OP(op_path='mock-op', use_existing_session=True, password_prompt=False)
 #         assert warning_list
+#         print(f"warning_list: {len(warning_list)}")
+
+
+def test_deprecated_method_01(valid_data: ValidData, expected_login_item_data):
+    """
+    OPAbstractItem.field_value_by_section_title() has been replaced with
+    field_value_by_section_label()
+
+    Verify a deprecation warning is generated
+    """
+    field_label = "Example Field"
+    section_label = "Example Section 1"
+
+    valid_item_dict = valid_data.data_for_name("example-login-with-fields")
+    result_login_item = OPLoginItem(valid_item_dict)
+
+    with warnings.catch_warnings(record=True) as warnings_list:
+        result_login_item.field_value_by_section_title(
+            section_label, field_label)
+        assert len(warnings_list) > 0

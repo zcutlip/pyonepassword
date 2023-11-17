0d1766bb3ace4fde026e39dfd5ed0e826bd70b1d
Zachary Cutlip <uid000@gmail.com>
2023-11-17 12:00:09 -0800

    add test case for:
    - editing a document item by providing the literal bytes
    - vs providing path to a new file

--- tests/test_op_api/test_document_edit/test_document_edit.py
+++ tests/test_op_api/test_document_edit/test_document_edit.py
@@ -51,3 +51,41 @@ def test_document_edit_01(signed_in_op: OP, binary_image_data: BinaryImageData):
     assert filename_2 == filename_1
     # The updated document digest should now match the digest of the input file
     assert digest_2 == input_data_digest
+
+
+@pytest.mark.usefixtures("setup_stateful_document_edit")
+def test_document_edit_02(signed_in_op: OP, binary_image_data: BinaryImageData):
+    """
+    Test: OP.document_edit() with the actual bytes of a replacement file
+        - Retrieve document bytes and filename via OP.document_get()
+        - Call document_edit(), providing the actual bytes of a new file to replace the document
+        - Retreive the same item a second time
+    Verify:
+        - The digest of the original document bytes does not match the digest of the replacement document bytes
+        - The the original document filename remains unchanged after the edit
+        - The edited document's digest matches the digest of the replacement document bytes
+    """
+    item_name = "example document 01"
+    vault = "Test Data 2"
+
+    input_data = binary_image_data.data_for_name("replacement-image-01")
+    input_data_digest = digest(input_data)
+
+    filename_1, data_1 = signed_in_op.document_get(item_name, vault=vault)
+    digest_1 = digest(data_1)
+
+    # these should be different, else we've messed up
+    # and the document has already been edited
+    assert digest_1 != input_data_digest
+
+    # Provide the literal bytes of the input file
+    # we'll test providingthe path to the input file separately
+    signed_in_op.document_edit(item_name, input_data, vault=vault)
+
+    filename_2, data_2 = signed_in_op.document_get(item_name, vault=vault)
+    digest_2 = digest(data_2)
+
+    # filename shouldn't change because we didn't explicitly set it
+    assert filename_2 == filename_1
+    # The updated document digest should now match the digest of the input file
+    assert digest_2 == input_data_digest

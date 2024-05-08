76ac7bcf31597e63dbd10857e428cbba80db37d6
Zachary Cutlip <uid000@gmail.com>
2024-05-07 19:09:52 -0700

    add script to regenerate README from template

---
+++ scripts/update_readme.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+
+from pyonepassword._op_cli_version import OPVersionSupport
+
+# isort: split
+parent_path = os.path.dirname(
+    os.path.dirname(
+        os.path.abspath(__file__)
+    )
+)
+if parent_path not in sys.path:
+    sys.path.insert(0, parent_path)
+
+MIN_VER_PLACEHOLDER = "MINIMUM_CLI_VERSION__"
+SUPPORTED_VER_PLACEHOLDER = "SUPPORTED_CLI_VERSION__"
+README_TEMPLATE = "_readme_template.md"
+README = "README.md"
+
+
+def main():
+    version_support = OPVersionSupport()
+    min_ver = str(version_support.minimum_version)
+    supported_ver = str(version_support.supported_version)
+    readme_text = open(README_TEMPLATE, "r").read()
+    readme_text = readme_text.replace(MIN_VER_PLACEHOLDER, min_ver)
+    readme_text = readme_text.replace(SUPPORTED_VER_PLACEHOLDER, supported_ver)
+    with open(README, "w") as f:
+        f.write(readme_text)
+
+
+if __name__ == "__main__":
+    exit(main())

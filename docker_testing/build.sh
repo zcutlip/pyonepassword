751b95136bf71db34cb277744ac41f489575d33e
Zachary Cutlip <uid000@gmail.com>
2024-07-25 20:47:31 -0700

    print some status from docker testing scripts

--- docker_testing/build.sh
+++ docker_testing/build.sh
@@ -2,7 +2,12 @@
 
 SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 
+echo "Building..."
+
 docker buildx build "$SCRIPT_DIR"/docker/ -f "$SCRIPT_DIR"/docker/py39.Dockerfile -t docker_py39 || exit
 docker buildx build "$SCRIPT_DIR"/docker/ -f "$SCRIPT_DIR"/docker/py310.Dockerfile -t docker_py310 || exit
 docker buildx build "$SCRIPT_DIR"/docker/ -f "$SCRIPT_DIR"/docker/py311.Dockerfile -t docker_py311 || exit
 docker buildx build "$SCRIPT_DIR"/docker/ -f "$SCRIPT_DIR"/docker/py312.Dockerfile -t docker_py312 || exit
+
+echo "...done"
+echo ""
--- docker_testing/clean.sh
+++ docker_testing/clean.sh
@@ -2,5 +2,10 @@
 
 # keep unused docker image names so we can ensure they get removed
 # this should not be an error
+echo "Cleaning..."
+echo "removing docker images"
 docker image rm docker_py38 docker_py39 docker_py310 docker_py311 docker_py312
+echo "removing .tox-docker"
 rm -rf .tox-docker
+echo "...done"
+echo ""

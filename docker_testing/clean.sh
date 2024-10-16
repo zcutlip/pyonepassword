872a2d079256cd497cec5c50b0dce5c81356b516
Zachary Cutlip <uid000@gmail.com>
2024-10-15 22:29:51 -0700

    update all docker testing shell scripts - remove python 3.9 support - add python 3.13 support

--- docker_testing/clean.sh
+++ docker_testing/clean.sh
@@ -4,7 +4,7 @@
 # this should not be an error
 echo "Cleaning..."
 echo "removing docker images"
-docker image rm docker_py38 docker_py39 docker_py310 docker_py311 docker_py312
+docker image rm docker_py39 docker_py310 docker_py311 docker_py312 docker_py313
 echo "removing .tox-docker"
 rm -rf .tox-docker
 echo "...done"

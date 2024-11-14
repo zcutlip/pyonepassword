9f859990f68e15047dbcacec34ac348fd85e4259
Zachary Cutlip <uid000@gmail.com>
2024-11-13 21:13:58 -0800

    update test_item_share_010 to check result against expected result

--- tests/test_op_api/item/share/test_item_share.py
+++ tests/test_op_api/item/share/test_item_share.py
@@ -7,8 +7,20 @@ from typing import TYPE_CHECKING
 if TYPE_CHECKING:
     from pyonepassword import OP
 
+    from ....fixtures.expected_item_share_data import (
+        ExpectedItemShare,
+        ExpectedItemShareData
+    )
 
-def test_item_share_010(signed_in_op: OP):
+
+def test_item_share_010(signed_in_op: OP, expected_item_share_data: ExpectedItemShareData):
+    item_key = "item-share-example-login-22-1"
+    expected_item_share: ExpectedItemShare
+    expected_item_share = expected_item_share_data.data_for_key(item_key)
+
     item_name = "Example Login Item 22"
+    assert expected_item_share.item_identifier == item_name
+
     share_url = signed_in_op.item_share(item_name)
-    print(share_url)
+
+    assert share_url == expected_item_share.url

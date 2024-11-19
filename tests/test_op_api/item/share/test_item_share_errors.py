9c8cfa894116c74aa22bad9ee7ae450430964f86
Zachary Cutlip <uid000@gmail.com>
2024-11-18 19:09:37 -0800

    add test_item_share_errors test module
    - test_item_share_invalid_expires_010()

---
+++ tests/test_op_api/item/share/test_item_share_errors.py
@@ -0,0 +1,28 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+import pytest
+
+from pyonepassword.api.exceptions import OPItemShareException
+
+if TYPE_CHECKING:
+    from pyonepassword import OP
+
+
+def test_item_share_invalid_expires_010(signed_in_op: OP):
+    """
+    Test: OP.item_share() with an invalid "expires-in" string
+        - Invalid "5q" string as expires_in kwarg
+        - Valid values for item_identifier, emails, & vault
+    Verify:
+        - The item share URL matches the expected URL
+    """
+
+    item_name = "Example Login Item 22"
+    expires_in = "5q"
+    email = "user_1@example.com"
+    vault = "Test Data 1"
+    with pytest.raises(OPItemShareException):
+        signed_in_op.item_share(item_name, emails=email,
+                                expires_in=expires_in, vault=vault)

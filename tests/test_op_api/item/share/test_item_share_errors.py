e8106688901a53f97774fe893fc1c330890dfb42
Zachary Cutlip <uid000@gmail.com>
2024-11-18 19:16:47 -0800

    add "item share" test case for invalid "expires_in" value

--- tests/test_op_api/item/share/test_item_share_errors.py
+++ tests/test_op_api/item/share/test_item_share_errors.py
@@ -26,3 +26,23 @@ def test_item_share_invalid_expires_010(signed_in_op: OP):
     with pytest.raises(OPItemShareException):
         signed_in_op.item_share(item_name, emails=email,
                                 expires_in=expires_in, vault=vault)
+
+
+def test_item_share_invalid_expires_020(signed_in_op: OP):
+    """
+    Test: OP.item_share() with an invalid "expires-in" string
+        - Invalid "banana" string as expires_in kwarg
+        - Valid values for item_identifier, emails, & vault
+    Verify:
+        - The item share URL matches the expected URL
+
+    NOTE: The error message for a completely invalid string such as
+        this is different than the error for a number+leter like "5q"
+    """
+    item_name = "Example Login Item 22"
+    expires_in = "banana"
+    email = "user_1@example.com"
+    vault = "Test Data 1"
+    with pytest.raises(OPItemShareException):
+        signed_in_op.item_share(item_name, emails=email,
+                                expires_in=expires_in, vault=vault)

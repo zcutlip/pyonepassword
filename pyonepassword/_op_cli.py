af3009d0aadf40fa297d4fc4583700608044f431
Zachary Cutlip <uid000@gmail.com>
2025-01-15 21:43:39 -0800

    handle `OPDesktopAppException` in `_OPCLIExcute._run_raw()`

--- pyonepassword/_op_cli.py
+++ pyonepassword/_op_cli.py
@@ -5,6 +5,7 @@ from os import environ
 from .py_op_exceptions import (
     OPCLIPanicException,
     OPCmdFailedException,
+    OPDesktopAppException,
     OPNotFoundException,
     OPRevokedSvcAcctTokenException
 )
@@ -25,6 +26,7 @@ class _OPCLIExecute:
     MOCK_OP_RESP_ERR_MSG = "Error looking up response"
     GO_RUNTIME_PANIC_MSG = "panic: runtime error:"
     SVC_ACCT_REVOKED_MSG = "The Service Account used in this integration has been deleted"
+    DESKTOP_APP_ERR_MSG = "connecting to desktop app:"
     logger = logging.getLogger("_OPCLIExecute")
     logger.setLevel(logging.INFO)
 
@@ -90,6 +92,8 @@ class _OPCLIExecute:
                     # in case caller is accidentally running with OP_SERVICE_ACCOUNT_TOKEN
                     raise OPRevokedSvcAcctTokenException(
                         stderr_output, returncode)
+                elif cls.DESKTOP_APP_ERR_MSG in stderr_output:
+                    raise OPDesktopAppException(stderr_output, returncode)
 
                 raise OPCmdFailedException(stderr_output, returncode) from err
 

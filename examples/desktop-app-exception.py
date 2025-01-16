9583d13e7f8f1ca43223e87f3e774d4937e17a7c
Zachary Cutlip <uid000@gmail.com>
2025-01-15 22:12:05 -0800

    add example handling `OPDesktopAppException`

---
+++ examples/desktop-app-exception.py
@@ -0,0 +1,72 @@
+import os
+import shlex
+import sys
+import time
+
+parent_path = os.path.dirname(
+    os.path.dirname(
+        os.path.abspath(__file__)
+    )
+)
+if parent_path not in sys.path:
+    sys.path.insert(0, parent_path)
+
+# isort: split
+from pyonepassword import OP  # noqa: E402
+from pyonepassword._op_cli import _OPCLIExecute as OPCLI  # noqa: E402
+from pyonepassword.api.exceptions import OPDesktopAppException  # noqa: E402
+from pyonepassword.logging import console_debug_logger  # noqa: E402
+from pyonepassword.py_op_exceptions import OPCmdFailedException  # noqa: E402
+
+
+class OPARGV(list):
+    """
+    stand-in for _OPCLIArgv class
+    """
+
+    def cmd_str(self):
+        """
+        return a shell-escaped command string from this argv
+        """
+
+        # ensure we get redacted versions of any strings where
+        # applicable
+        args = [str(arg) for arg in self]
+        cmd_str = shlex.join(args)
+        return cmd_str
+
+
+def kill_pgrep(match_pattern):
+    # this should be equivalent to:
+    # kill $(pgrep "1Password")
+    argv = OPARGV(["pgrep", match_pattern])
+    # we're lazy so we're going to use _OPCLIExecute.run() here
+    # since it already handles all the subprocess stuff
+    matches = OPCLI._run(argv, capture_stdout=True, decode="utf-8")
+
+    pid_list = matches.splitlines()
+    print(f"matching pids:\n{matches}")
+    args = ["kill"]
+    args.extend(pid_list)
+    argv = OPARGV(args)
+    OPCLI._run(argv)
+
+
+if __name__ == "__main__":
+    logger = console_debug_logger("pyonepassword")
+
+    try:
+        # kill all 1Password-related processes
+        kill_pgrep("1Pass")
+    except OPCmdFailedException:
+        pass
+    attempts = 0
+    while attempts < 3:
+        # It can fail twice in a row but with slightly different
+        # error each time. Give it three tries to be sure
+        attempts += 1
+        try:
+            op = OP(logger=logger)
+        except OPDesktopAppException as e:
+            print(e.err_output)
+            time.sleep(1)

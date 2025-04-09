0b9686f9724ffab3109d92a417127cbb6d3805e7
Zachary Cutlip <uid000@gmail.com>
2025-04-08 20:17:58 -0700

    fix mypy error

--- pyonepassword/py_op_exceptions.py
+++ pyonepassword/py_op_exceptions.py
@@ -142,8 +142,9 @@ class OPItemDeleteMultipleException(OPItemDeleteException):
         super().__init__(stderr_out, returncode)
 
     @classmethod
-    # type: ignore[override]
-    def from_opexception(cls, ope: OPCmdFailedException, deleted_items: OPItemList):
+    def from_opexception(cls,  # type: ignore[override]
+                         ope: OPCmdFailedException,
+                         deleted_items: OPItemList):
         return cls(deleted_items, ope.err_output, ope.returncode)
 
 
